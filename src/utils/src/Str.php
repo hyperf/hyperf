0f0671e57273f81b09c77d6296c60f75b5f3dcaa
李铭昕 <715557344@qq.com>
2018-12-29 10:26:13 +0800

    Fixed str namespace

--- src/utils/src/Str.php
+++ src/utils/src/Str.php
@@ -9,7 +9,7 @@ declare(strict_types=1);
  * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
  */
 
-namespace Hyperf\Support;
+namespace Hyperf\Utils;
 
 use Hyperf\Utils\Traits\Macroable;
 
