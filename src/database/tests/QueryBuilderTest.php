4eaf730a15cfd4edbc3b8437aa7ac528c6711f57
huangzhhui <huangzhwork@gmail.com>
2019-01-17 21:15:38 +0800

    Added QueryBuilder test

--- src/database/tests/QueryBuilderTest.php
+++ src/database/tests/QueryBuilderTest.php
@@ -3,19 +3,37 @@
 namespace HyperfTest\Database;
 
 
+use Hyperf\Database\ConnectionInterface;
+use Hyperf\Database\Query\Builder;
+use Hyperf\Database\Query\Grammars\Grammar;
+use Hyperf\Database\Query\Processors\Processor;
 use PHPUnit\Framework\TestCase;
-use Psr\Container\ContainerInterface;
 
+/**
+ * @property ConnectionInterface $connection
+ * @property Builder $queryBuilder
+ */
 class QueryBuilderTest extends TestCase
 {
+
     protected function setUp()
     {
-        $this->container = $this->prophesize(ContainerInterface::class);
+        $this->connection = $this->prophesize(ConnectionInterface::class);
+        $this->queryBuilder = new Builder($this->connection->reveal(), new Grammar(), new Processor());
     }
 
 
     public function testQueryBuilder()
     {
+        $this->assertInstanceOf(Builder::class, $this->queryBuilder);
+    }
+
+    public function testSelect()
+    {
+        $this->queryBuilder->select(['*']);
+        $this->assertSame('select *', $this->queryBuilder->toSql());
+        $this->queryBuilder->select(['id', 'name']);
+        $this->assertSame('select "id", "name"', $this->queryBuilder->toSql());
     }
 
 }
