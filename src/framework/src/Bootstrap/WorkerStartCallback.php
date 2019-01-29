1894ce6d76316c7326f7e1fb1b659b2a9996ec5e
huangzhhui <huangzhwork@gmail.com>
2019-01-29 16:35:41 +0800

    Added MainWorkerStart event

--- src/framework/src/Bootstrap/WorkerStartCallback.php
+++ src/framework/src/Bootstrap/WorkerStartCallback.php
@@ -14,10 +14,12 @@ namespace Hyperf\Framework\Bootstrap;
 
 use Hyperf\Di\Container;
 use Hyperf\Framework\Contract\StdoutLoggerInterface;
+use Hyperf\Framework\Event\MainWorkerStart;
 use Hyperf\Framework\SwooleEvent;
 use Hyperf\Memory\AtomicManager;
 use Hyperf\Memory\LockManager;
 use Psr\Container\ContainerInterface;
+use Psr\EventDispatcher\EventDispatcherInterface;
 use RuntimeException;
 use Swoole\Server as SwooleServer;
 
@@ -33,10 +35,16 @@ class WorkerStartCallback
      */
     private $logger;
 
+    /**
+     * @var EventDispatcherInterface
+     */
+    private $eventDispatcher;
+
     public function __construct(ContainerInterface $container, StdoutLoggerInterface $logger)
     {
         $this->container = $container;
         $this->logger = $logger;
+        $this->eventDispatcher = $container->get(EventDispatcherInterface::class);
     }
 
     /**
@@ -53,19 +61,18 @@ class WorkerStartCallback
             if ($lock->trylock()) {
                 $lockedWorkerId = $workerId;
                 // Only running in one worker.
-                $this->logger->debug("Worker ${lockedWorkerId} got the lock.");
-                // @TODO Do something that you want only one worker do.
-                sleep(1);
+                $this->logger->debug("Worker#${lockedWorkerId} got the lock.");
+                $this->eventDispatcher->dispatch(new MainWorkerStart($server, $lockedWorkerId));
                 $lock->unlock();
                 $atomic->wakeup($server->setting['worker_num'] - 1);
             } else {
-                $this->logger->debug("Worker ${workerId} wating ...");
+                $this->logger->debug("Worker#${workerId} wating ...");
                 $atomic->wait();
             }
             if (! $isScan || $workerId !== $lockedWorkerId) {
                 // @TODO Do something that the workers who does not got the lock should do.
             }
-            $this->logger->info("Worker ${workerId} started.");
+            $this->logger->info("Worker#${workerId} started.");
         } catch (RuntimeException $e) {
             $this->logger->warning('Worker atomic and lock initialize fail.');
         } finally {
