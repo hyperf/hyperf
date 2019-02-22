bd334ecb56fdd6719da71714d02f501f30743658
huangzhhui <huangzhwork@gmail.com>
2019-02-23 00:26:33 +0800

    Format

--- src/consul/src/Exception/ServerException.php
+++ src/consul/src/Exception/ServerException.php
@@ -1,9 +1,17 @@
 <?php
 
-namespace Hyperf\Consul;
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://hyperf.org
+ * @document https://wiki.hyperf.org
+ * @contact  group@hyperf.org
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
 
+namespace Hyperf\Consul;
 
 class ServerException extends \RuntimeException implements ConsulException
 {
-
 }
--- src/consul/src/HealthInterface.php
+++ src/consul/src/HealthInterface.php
@@ -1,18 +1,24 @@
 <?php
 
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://hyperf.org
+ * @document https://wiki.hyperf.org
+ * @contact  group@hyperf.org
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
 
 namespace Hyperf\Consul;
 
-
 interface HealthInterface
 {
+    public function node($node, array $options = []);
 
-    public function node($node, array $options = array());
+    public function checks($service, array $options = []);
 
-    public function checks($service, array $options = array());
+    public function service($service, array $options = []);
 
-    public function service($service, array $options = array());
-
-    public function state($state, array $options = array());
-
+    public function state($state, array $options = []);
 }
