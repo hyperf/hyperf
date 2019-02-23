5a70f8ba3ddd3ca611cd6df6385053cca1b1feb1
李铭昕 <715557344@qq.com>
2019-02-23 09:12:30 +0800

    Let GuzzleClientFactory support non-coroutine environment.

--- src/guzzle/src/ClientFactory.php
+++ src/guzzle/src/ClientFactory.php
@@ -12,14 +12,21 @@ declare(strict_types=1);
 
 namespace Hyperf\Guzzle;
 
+use Swoole\Coroutine;
 use GuzzleHttp\Client;
 use GuzzleHttp\HandlerStack;
+use function GuzzleHttp\choose_handler;
 
 class ClientFactory
 {
     public static function createClient(array $options = []): Client
     {
-        $stack = HandlerStack::create(new CoroutineHandler());
+        if (Coroutine::getCid() > 0) {
+            $stack = HandlerStack::create(new CoroutineHandler());
+        } else {
+            $stack = HandlerStack::create(choose_handler());
+        }
+
         $config = array_replace(['handler' => $stack], $options);
         return make(Client::class, compact('config'));
     }
