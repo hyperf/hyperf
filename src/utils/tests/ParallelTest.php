608a18ef46a4f548992fb9f7c78e13df615bdc78
huangzhhui <huangzhwork@gmail.com>
2019-02-25 18:04:11 +0800

    Added Parallel and the unit test

---
+++ src/utils/tests/ParallelTest.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://hyperf.org
+ * @document https://wiki.hyperf.org
+ * @contact  group@hyperf.org
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+namespace HyperfTest\Utils;
+
+use Hyperf\Utils\Coroutine;
+use Hyperf\Utils\Parallel;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ * @covers \Hyperf\Utils\Parallel
+ */
+class ParallelTest extends TestCase
+{
+
+    public function testParallel()
+    {
+        // Closure
+        $parallel = new Parallel();
+        for ($i = 0; $i < 3; ++$i) {
+            $parallel->add(function () {
+                return Coroutine::id();
+            });
+        }
+        $result = $parallel->wait();
+        $this->assertSame([2, 3, 4], $result);
+
+        // Array
+        $parallel = new Parallel();
+        for ($i = 0; $i < 3; ++$i) {
+            $parallel->add([$this, 'returnCoId']);
+        }
+        $result = $parallel->wait();
+        $this->assertSame([5, 6, 7], $result);
+    }
+
+    public function returnCoId()
+    {
+        return Coroutine::id();
+    }
+
+}
