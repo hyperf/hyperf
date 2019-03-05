0db05ae4b3e19b5c94404909a58e9f5677162d4f
李铭昕 <715557344@qq.com>
2019-03-05 12:58:31 +0800

    Added some generator commands.

---
+++ src/devtool/src/Generator/JobCommand.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://hyperf.org
+ * @document https://wiki.hyperf.org
+ * @contact  group@hyperf.org
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+namespace Hyperf\Devtool\Generator;
+
+use Hyperf\Framework\Annotation\Command;
+
+/**
+ * @Command
+ */
+class JobCommand extends GeneratorCommand
+{
+    public function __construct()
+    {
+        parent::__construct('make:job');
+    }
+
+    protected function getStub(): string
+    {
+        return __DIR__ . '/stubs/job.stub';
+    }
+
+    protected function getDefaultNamespace()
+    {
+        return 'App\\Jobs';
+    }
+}
