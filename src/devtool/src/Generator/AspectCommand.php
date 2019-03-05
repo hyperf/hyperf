0024bca0e4f6947b45e81a9733c78ee2c778e1c7
李铭昕 <715557344@qq.com>
2019-03-05 14:09:15 +0800

    Added make:aspect command.

---
+++ src/devtool/src/Generator/AspectCommand.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://hyperf.org
+ * @document https://wiki.hyperf.org
+ * @contact  group@hyperf.org
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+namespace Hyperf\Devtool\Generator;
+
+use Hyperf\Framework\Annotation\Command;
+
+/**
+ * @Command
+ */
+class AspectCommand extends GeneratorCommand
+{
+    public function __construct()
+    {
+        parent::__construct('make:aspect');
+    }
+
+    protected function getStub(): string
+    {
+        return __DIR__ . '/stubs/aspect.stub';
+    }
+
+    protected function getDefaultNamespace()
+    {
+        return 'App\\Aspects';
+    }
+}
