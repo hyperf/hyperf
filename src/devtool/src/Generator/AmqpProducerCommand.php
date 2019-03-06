9b8b6af7c7120bf06702b933761e306a5bba1ec0
李铭昕 <715557344@qq.com>
2019-03-06 11:45:04 +0800

    Added amqp consumer and producer generator command.

---
+++ src/devtool/src/Generator/AmqpProducerCommand.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://hyperf.org
+ * @document https://wiki.hyperf.org
+ * @contact  group@hyperf.org
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+namespace Hyperf\Devtool\Generator;
+
+use Hyperf\Framework\Annotation\Command;
+
+/**
+ * @Command
+ */
+class AmqpProducerCommand extends GeneratorCommand
+{
+    public function __construct()
+    {
+        parent::__construct('gen:amqp-producer');
+        $this->setDescription('Create a new amqp producer class');
+    }
+
+    protected function getStub(): string
+    {
+        return __DIR__ . '/stubs/amqp-producer.stub';
+    }
+
+    protected function getDefaultNamespace(): string
+    {
+        return 'App\\Amqp\\Producers';
+    }
+}
