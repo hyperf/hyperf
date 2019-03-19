0685875af6a2f367c5b65cd420527d19cecf65bd
李铭昕 <715557344@qq.com>
2019-03-19 15:54:46 +0800

    Added guzzle retry middleware.

---
+++ src/guzzle/src/RetryMiddleware.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://hyperf.org
+ * @document https://wiki.hyperf.org
+ * @contact  group@hyperf.org
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+namespace Hyperf\Guzzle;
+
+use GuzzleHttp\Middleware;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+class RetryMiddleware implements MiddlewareInterface
+{
+    protected $retries = 1;
+
+    protected $delay = 0;
+
+    public function __construct(int $retries = 1, int $delay = 0)
+    {
+        $this->retries = $retries;
+        $this->delay = $delay;
+    }
+
+    public function getMiddleware(): callable
+    {
+        return Middleware::retry(function ($retries, RequestInterface $request, ResponseInterface $response = null) {
+            if ($response->getStatusCode() !== 200 && $retries < $this->retries) {
+                return true;
+            }
+            return false;
+        }, function () {
+            return $this->delay;
+        });
+    }
+}
