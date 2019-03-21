afc7c3fe1b24bd616ad2c7b634885a0ad0d6918d
李铭昕 <715557344@qq.com>
2019-03-21 15:53:57 +0800

    Simplified sub handler logic for circuit-breaker.

--- src/circuit-breaker/src/Exception/TimeoutException.php
+++ src/circuit-breaker/src/Exception/TimeoutException.php
@@ -14,4 +14,9 @@ namespace Hyperf\CircuitBreaker\Exception;
 
 class TimeoutException extends CircuitBreakerException
 {
+    public function __construct(string $message = '', $result)
+    {
+        parent::__construct($message);
+        $this->result = $result;
+    }
 }
