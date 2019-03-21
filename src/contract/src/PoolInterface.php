5e4441da8386d420e5504a996a67df2c1753ad66
huangzhhui <huangzhwork@gmail.com>
2019-03-21 19:52:02 +0800

    Added comment

--- src/contract/src/PoolInterface.php
+++ src/contract/src/PoolInterface.php
@@ -14,9 +14,19 @@ namespace Hyperf\Contract;
 
 interface PoolInterface
 {
+
+    /**
+     * Get a connection from the connection pool.
+     */
     public function get(): ConnectionInterface;
 
+    /**
+     * Release a connection back to the connection pool.
+     */
     public function release(ConnectionInterface $connection): void;
 
+    /**
+     * Close and clear the connection pool.
+     */
     public function flush(): void;
 }
