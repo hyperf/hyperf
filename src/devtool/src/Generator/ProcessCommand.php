e5ea184c5bc54151a8c5fb052079fc5b2b6a5e1c
李铭昕 <715557344@qq.com>
2019-03-23 15:23:33 +0800

    Added process generator.

---
+++ src/devtool/src/Generator/ProcessCommand.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://hyperf.org
+ * @document https://wiki.hyperf.org
+ * @contact  group@hyperf.org
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+namespace Hyperf\Devtool\Generator;
+
+use Hyperf\Framework\Annotation\Command;
+
+/**
+ * @Command
+ */
+class ProcessCommand extends GeneratorCommand
+{
+    public function __construct()
+    {
+        parent::__construct('gen:process');
+        $this->setDescription('Create a new process class');
+    }
+
+    protected function getStub(): string
+    {
+        return __DIR__ . '/stubs/process.stub';
+    }
+
+    protected function getDefaultNamespace(): string
+    {
+        return 'App\\Processes';
+    }
+}
