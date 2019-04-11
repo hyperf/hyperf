8f93d46ff6e25f5813697206000b92b944001c21
huangzhhui <huangzhwork@gmail.com>
2019-04-12 00:52:25 +0800

    Create ChannelPool.php

---
+++ src/utils/src/ChannelPool.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://hyperf.org
+ * @document https://wiki.hyperf.org
+ * @contact  group@hyperf.org
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+namespace Hyperf\Utils;
+
+use Swoole\Coroutine\Channel;
+
+class ChannelPool extends \SplQueue
+{
+    /**
+     * @var ChannelPool
+     */
+    private static $instance;
+
+    public static function getInstance(): self
+    {
+        return static::$instance ?? (static::$instance = new self());
+    }
+
+    public function get(): Channel
+    {
+        return $this->isEmpty() ? new Channel(1) : $this->pop();
+    }
+
+    public function release(Channel $channel)
+    {
+        $channel->errCode = 0;
+        $this->push($channel);
+    }
+}
