6be479cb31a6157e4157eb96a5d7b27b342d2538
huangzhhui <huangzhwork@gmail.com>
2019-05-03 23:29:57 +0800

    Adjusted comment and return tyoe

--- src/http-message/src/Base/Response.php
+++ src/http-message/src/Base/Response.php
@@ -34,71 +34,77 @@ class Response implements ResponseInterface
     protected $charset = 'utf-8';
 
     /** @var array Map of standard HTTP status code/reason phrases */
-    private static $phrases = [
-        100 => 'Continue',
-        101 => 'Switching Protocols',
-        102 => 'Processing',
-        200 => 'OK',
-        201 => 'Created',
-        202 => 'Accepted',
-        203 => 'Non-Authoritative Information',
-        204 => 'No Content',
-        205 => 'Reset Content',
-        206 => 'Partial Content',
-        207 => 'Multi-status',
-        208 => 'Already Reported',
-        300 => 'Multiple Choices',
-        301 => 'Moved Permanently',
-        302 => 'Found',
-        303 => 'See Other',
-        304 => 'Not Modified',
-        305 => 'Use Proxy',
-        306 => 'Switch Proxy',
-        307 => 'Temporary Redirect',
-        400 => 'Bad Request',
-        401 => 'Unauthorized',
-        402 => 'Payment Required',
-        403 => 'Forbidden',
-        404 => 'Not Found',
-        405 => 'Method Not Allowed',
-        406 => 'Not Acceptable',
-        407 => 'Proxy Authentication Required',
-        408 => 'Request Time-out',
-        409 => 'Conflict',
-        410 => 'Gone',
-        411 => 'Length Required',
-        412 => 'Precondition Failed',
-        413 => 'Request Entity Too Large',
-        414 => 'Request-URI Too Large',
-        415 => 'Unsupported Media Type',
-        416 => 'Requested range not satisfiable',
-        417 => 'Expectation Failed',
-        418 => 'I\'m a teapot',
-        422 => 'Unprocessable Entity',
-        423 => 'Locked',
-        424 => 'Failed Dependency',
-        425 => 'Unordered Collection',
-        426 => 'Upgrade Required',
-        428 => 'Precondition Required',
-        429 => 'Too Many Requests',
-        431 => 'Request Header Fields Too Large',
-        451 => 'Unavailable For Legal Reasons',
-        500 => 'Internal Server Error',
-        501 => 'Not Implemented',
-        502 => 'Bad Gateway',
-        503 => 'Service Unavailable',
-        504 => 'Gateway Time-out',
-        505 => 'HTTP Version not supported',
-        506 => 'Variant Also Negotiates',
-        507 => 'Insufficient Storage',
-        508 => 'Loop Detected',
-        511 => 'Network Authentication Required',
-    ];
+    private static $phrases
+        = [
+            100 => 'Continue',
+            101 => 'Switching Protocols',
+            102 => 'Processing',
+            200 => 'OK',
+            201 => 'Created',
+            202 => 'Accepted',
+            203 => 'Non-Authoritative Information',
+            204 => 'No Content',
+            205 => 'Reset Content',
+            206 => 'Partial Content',
+            207 => 'Multi-status',
+            208 => 'Already Reported',
+            300 => 'Multiple Choices',
+            301 => 'Moved Permanently',
+            302 => 'Found',
+            303 => 'See Other',
+            304 => 'Not Modified',
+            305 => 'Use Proxy',
+            306 => 'Switch Proxy',
+            307 => 'Temporary Redirect',
+            400 => 'Bad Request',
+            401 => 'Unauthorized',
+            402 => 'Payment Required',
+            403 => 'Forbidden',
+            404 => 'Not Found',
+            405 => 'Method Not Allowed',
+            406 => 'Not Acceptable',
+            407 => 'Proxy Authentication Required',
+            408 => 'Request Time-out',
+            409 => 'Conflict',
+            410 => 'Gone',
+            411 => 'Length Required',
+            412 => 'Precondition Failed',
+            413 => 'Request Entity Too Large',
+            414 => 'Request-URI Too Large',
+            415 => 'Unsupported Media Type',
+            416 => 'Requested range not satisfiable',
+            417 => 'Expectation Failed',
+            418 => 'I\'m a teapot',
+            422 => 'Unprocessable Entity',
+            423 => 'Locked',
+            424 => 'Failed Dependency',
+            425 => 'Unordered Collection',
+            426 => 'Upgrade Required',
+            428 => 'Precondition Required',
+            429 => 'Too Many Requests',
+            431 => 'Request Header Fields Too Large',
+            451 => 'Unavailable For Legal Reasons',
+            500 => 'Internal Server Error',
+            501 => 'Not Implemented',
+            502 => 'Bad Gateway',
+            503 => 'Service Unavailable',
+            504 => 'Gateway Time-out',
+            505 => 'HTTP Version not supported',
+            506 => 'Variant Also Negotiates',
+            507 => 'Insufficient Storage',
+            508 => 'Loop Detected',
+            511 => 'Network Authentication Required',
+        ];
 
     /**
      * @var array
      */
     private $attributes = [];
+
+    public function __toString()
+    {
+        return $this->getBody()->getContents();
+    }
 
     /**
      * Retrieve attributes derived from the request.
@@ -123,10 +129,10 @@ class Response implements ResponseInterface
      * This method obviates the need for a hasAttribute() method, as it allows
      * specifying a default value to return if the attribute is not found.
      *
-     * @see getAttributes()
      * @param string $name the attribute name
      * @param mixed $default default value to return if the attribute does not exist
      * @return mixed
+     * @see getAttributes()
      */
     public function getAttribute($name, $default = null)
     {
@@ -141,10 +147,10 @@ class Response implements ResponseInterface
      * immutability of the message, and MUST return an instance that has the
      * updated attribute.
      *
-     * @see getAttributes()
      * @param string $name the attribute name
      * @param mixed $value the value of the attribute
      * @return static
+     * @see getAttributes()
      */
     public function withAttribute($name, $value)
     {
@@ -196,11 +202,8 @@ class Response implements ResponseInterface
 
     /**
      * Return the reason phrase by code.
-     *
-     * @param $code
-     * @return string
      */
-    public static function getReasonPhraseByCode($code): string
+    public static function getReasonPhraseByCode(int $code): string
     {
         return self::$phrases[$code] ?? '';
     }
@@ -208,11 +211,10 @@ class Response implements ResponseInterface
     /**
      * Return an instance with the specified charset content type.
      *
-     * @param $charset
      * @throws \InvalidArgumentException
      * @return static
      */
-    public function withCharset($charset): self
+    public function withCharset(string $charset): self
     {
         return $this->withAddedHeader('Content-Type', sprintf('charset=%s', $charset));
     }
@@ -234,18 +236,11 @@ class Response implements ResponseInterface
         return $this->reasonPhrase;
     }
 
-    /**
-     * @return string
-     */
     public function getCharset(): string
     {
         return $this->charset;
     }
 
-    /**
-     * @param string $charset
-     * @return Response
-     */
     public function setCharset(string $charset): Response
     {
         $this->charset = $charset;
@@ -255,135 +250,96 @@ class Response implements ResponseInterface
     /**
      * Is response invalid?
      *
-     * @return bool
-     *
      * @see http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
-     *
-     * @final since version 3.2
      */
-    public function isInvalid()
+    public function isInvalid(): bool
     {
         return $this->statusCode < 100 || $this->statusCode >= 600;
     }
 
     /**
      * Is response informative?
-     *
-     * @return bool
-     *
-     * @final since version 3.3
      */
-    public function isInformational()
+    public function isInformational(): bool
     {
         return $this->statusCode >= 100 && $this->statusCode < 200;
     }
 
     /**
      * Is response successful?
-     *
-     * @return bool
-     *
-     * @final since version 3.2
      */
-    public function isSuccessful()
+    public function isSuccessful(): bool
     {
         return $this->statusCode >= 200 && $this->statusCode < 300;
     }
 
     /**
      * Is the response a redirect?
-     *
-     * @return bool
-     *
-     * @final since version 3.2
      */
-    public function isRedirection()
+    public function isRedirection(): bool
     {
         return $this->statusCode >= 300 && $this->statusCode < 400;
     }
 
     /**
      * Is there a client error?
-     *
-     * @return bool
-     *
-     * @final since version 3.2
      */
-    public function isClientError()
+    public function isClientError(): bool
     {
         return $this->statusCode >= 400 && $this->statusCode < 500;
     }
 
     /**
      * Was there a server side error?
-     *
-     * @return bool
-     *
-     * @final since version 3.3
      */
-    public function isServerError()
+    public function isServerError(): bool
     {
         return $this->statusCode >= 500 && $this->statusCode < 600;
     }
 
     /**
      * Is the response OK?
-     *
-     * @return bool
-     *
-     * @final since version 3.2
      */
-    public function isOk()
+    public function isOk(): bool
     {
         return $this->statusCode === 200;
     }
 
     /**
      * Is the response forbidden?
-     *
-     * @return bool
-     *
-     * @final since version 3.2
      */
-    public function isForbidden()
+    public function isForbidden(): bool
     {
         return $this->statusCode === 403;
     }
 
     /**
      * Is the response a not found error?
-     *
-     * @return bool
-     *
-     * @final since version 3.2
      */
-    public function isNotFound()
+    public function isNotFound(): bool
     {
         return $this->statusCode === 404;
     }
 
     /**
      * Is the response a redirect of some form?
-     *
-     * @param string $location
-     *
-     * @return bool
-     *
-     * @final since version 3.2
      */
-    public function isRedirect($location = null)
+    public function isRedirect(string $location = null): bool
     {
-        return in_array($this->statusCode, [201, 301, 302, 303, 307, 308]) && ($location === null ?: $location == $this->getHeaderLine('Location'));
+        return in_array($this->statusCode, [
+            201,
+            301,
+            302,
+            303,
+            307,
+            308,
+        ]) && ($location === null ?: $location == $this->getHeaderLine('Location'));
     }
 
     /**
      * Is the response empty?
-     *
-     * @return bool
-     *
-     * @final since version 3.2
      */
-    public function isEmpty()
+    public function isEmpty(): bool
     {
         return in_array($this->statusCode, [204, 304]);
     }
