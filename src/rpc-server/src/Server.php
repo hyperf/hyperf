df3c21ab71473c25ea381b63c5b85272466738b1
huangzhhui <huangzhwork@gmail.com>
2019-05-03 23:35:59 +0800

    Becomes a abstract server

--- src/rpc-server/src/Server.php
+++ src/rpc-server/src/Server.php
@@ -16,85 +16,72 @@ use Hyperf\Contract\ConfigInterface;
 use Hyperf\Contract\DispatcherInterface;
 use Hyperf\Contract\MiddlewareInitializerInterface;
 use Hyperf\Contract\OnReceiveInterface;
-use Hyperf\Contract\PackerInterface;
 use Hyperf\Framework\ExceptionHandlerDispatcher;
-use Hyperf\HttpMessage\Server\Request as Psr7Request;
-use Hyperf\HttpMessage\Uri\Uri;
 use Hyperf\HttpServer\Exception\Handler\HttpExceptionHandler;
-use Hyperf\HttpServer\Exception\ServerException;
-use Hyperf\Rpc\Contract\EofInterface;
-use Hyperf\Rpc\Contract\ResponseInterface;
-use Hyperf\Rpc\Response as Psr7Response;
+use Hyperf\Server\Exception\ServerException;
 use Hyperf\Server\ServerManager;
 use Hyperf\Utils\Context;
-use InvalidArgumentException;
 use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Log\LoggerInterface;
 use Swoole\Server as SwooleServer;
 use Throwable;
 
-class Server implements OnReceiveInterface, MiddlewareInitializerInterface
+abstract class Server implements OnReceiveInterface, MiddlewareInitializerInterface
 {
     /**
      * @var array
      */
-    private $middlewares;
+    protected $middlewares;
 
     /**
      * @var string
      */
-    private $coreHandler;
+    protected $coreHandler;
 
     /**
      * @var MiddlewareInterface
      */
-    private $coreMiddleware;
+    protected $coreMiddleware;
 
     /**
      * @var array
      */
-    private $exceptionHandlers;
+    protected $exceptionHandlers;
 
     /**
      * @var ContainerInterface
      */
-    private $container;
+    protected $container;
 
     /**
      * @var DispatcherInterface
      */
-    private $dispatcher;
+    protected $dispatcher;
 
     /**
      * @var string
      */
-    private $serverName;
+    protected $serverName;
 
     /**
      * @var LoggerInterface
      */
-    private $logger;
-
-    /**
-     * @var PackerInterface
-     */
-    private $packer;
+    protected $logger;
 
     public function __construct(
         string $serverName,
         string $coreHandler,
         ContainerInterface $container,
         $dispatcher,
-        PackerInterface $packer,
         LoggerInterface $logger
     ) {
         $this->serverName = $serverName;
         $this->coreHandler = $coreHandler;
         $this->container = $container;
         $this->dispatcher = $dispatcher;
-        $this->packer = $packer;
         $this->logger = $logger;
     }
 
@@ -116,7 +103,7 @@ class Server implements OnReceiveInterface, MiddlewareInitializerInterface
         try {
             // Initialize PSR-7 Request and Response objects.
             Context::set(ServerRequestInterface::class, $request = $this->buildRequest($fd, $fromId, $data));
-            Context::set(\Psr\Http\Message\ResponseInterface::class, $this->buildResponse($fd, $server));
+            Context::set(ResponseInterface::class, $this->buildResponse($fd, $server));
 
             // $middlewares = array_merge($this->middlewares, MiddlewareManager::get());
             $middlewares = $this->middlewares;
@@ -132,73 +119,27 @@ class Server implements OnReceiveInterface, MiddlewareInitializerInterface
             $response = $exceptionHandlerDispatcher->dispatch($throwable, $this->exceptionHandlers);
         } finally {
             if (! $response instanceof ResponseInterface) {
-                $response = Context::get(ResponseInterface::class);
+                $response = $this->morphToResponse($response);
             }
             // Send the Response to client.
-            $response->send();
+            $server->send($fd, (string) $response);
         }
     }
 
     public function onConnect(SwooleServer $server)
     {
         // $server is the main server object, not the server object that this callback on.
-        /*
-         * @var \Swoole\Server\Port
-         */
+        /* @var \Swoole\Server\Port */
         [$type, $port] = ServerManager::get($this->serverName);
         $this->logger->debug(sprintf('Connect to %s:%d', $port->host, $port->port));
     }
 
-    private function buildRequest(int $fd, int $fromId, string $data): ServerRequestInterface
-    {
-        $data = $this->packer->unpack($data);
-        if (isset($data['jsonrpc'])) {
-            return $this->buildJsonRpcRequest($fd, $fromId, $data);
-        }
-        throw new InvalidArgumentException('Doesn\'t match any protocol.');
-    }
+    abstract protected function buildRequest(int $fd, int $fromId, string $data): ServerRequestInterface;
 
-    private function buildResponse(int $fd, SwooleServer $server): ResponseInterface
-    {
-        $response = new Psr7Response($fd, $server);
-        if ($response instanceof EofInterface) {
-            $eof = value(function () use ($server) {
-                /** @var \Swoole\Server\Port $port */
-                [$type, $port] = ServerManager::get($this->serverName);
-                if (isset($port->setting['package_eof'])) {
-                    return $port->setting['package_eof'];
-                }
-                if (isset($server->setting['package_eof'])) {
-                    return $server->setting['package_eof'];
-                }
-                return "\r\n";
-            });
-            $response->setEof($eof);
-        }
-        return $response;
-    }
+    abstract protected function buildResponse(int $fd, SwooleServer $server): ResponseInterface;
 
-    private function buildJsonRpcRequest(int $fd, int $fromId, array $data)
+    protected function morphToResponse($response)
     {
-        if (! isset($data['method'])) {
-            $data['method'] = '';
-        }
-        if (! isset($data['params'])) {
-            $data['params'] = [];
-        }
-        /** @var \Swoole\Server\Port $port */
-        [$type, $port] = ServerManager::get($this->serverName);
-
-        $uri = (new Uri())
-            ->withPath($data['method'])
-            ->withScheme('jsonrpc')
-            ->withHost($port->host)
-            ->withPort($port->port);
-        return (new Psr7Request('GET', $uri))
-            ->withAttribute('fd', $fd)
-            ->withAttribute('fromId', $fromId)
-            ->withAttribute('data', $data)
-            ->withProtocolVersion($data['jsonrpc'] ?? '2.0')
-            ->withParsedBody($data['params']);
+        return Context::get(ResponseInterface::class);
     }
 }
