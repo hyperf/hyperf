e4a930734dd58bd8e11f2c827a64c7ee2ea9ad57
huangzhhui <huangzhwork@gmail.com>
2019-05-03 22:47:45 +0800

    Added new components

--- bin/split.sh
+++ bin/split.sh
@@ -35,11 +35,14 @@ remote dispatcher git@github.com:hyperf-cloud/dispatcher.git
 remote elasticsearch git@github.com:hyperf-cloud/elasticsearch.git
 remote event git@github.com:hyperf-cloud/event.git
 remote framework git@github.com:hyperf-cloud/framework.git
+remote grpc git@github.com:hyperf-cloud/grpc.git
 remote grpc-client git@github.com:hyperf-cloud/grpc-client.git
 remote grpc-server git@github.com:hyperf-cloud/grpc-server.git
 remote guzzle git@github.com:hyperf-cloud/guzzle.git
 remote http-message git@github.com:hyperf-cloud/http-message.git
 remote http-server git@github.com:hyperf-cloud/http-server.git
+remote json-rpc git@github.com:hyperf-cloud/json-rpc.git
+remote load-balancer git@github.com:hyperf-cloud/load-balancer.git
 remote logger git@github.com:hyperf-cloud/logger.git
 remote memory git@github.com:hyperf-cloud/memory.git
 remote model-cache git@github.com:hyperf-cloud/model-cache.git
@@ -48,6 +51,12 @@ remote pool git@github.com:hyperf-cloud/pool.git
 remote process git@github.com:hyperf-cloud/process.git
 remote rate-limit git@github.com:hyperf-cloud/rate-limit.git
 remote redis git@github.com:hyperf-cloud/redis.git
+remote rpc git@github.com:hyperf-cloud/rpc.git
+remote rpc-client git@github.com:hyperf-cloud/rpc-client.git
+remote rpc-server git@github.com:hyperf-cloud/rpc-server.git
+remote server git@github.com:hyperf-cloud/server.git
+remote service-governance git@github.com:hyperf-cloud/service-governance.git
+remote swagger git@github.com:hyperf-cloud/swagger.git
 remote tracer git@github.com:hyperf-cloud/tracer.git
 remote utils git@github.com:hyperf-cloud/utils.git
 
@@ -68,11 +77,14 @@ split 'src/dispatcher' dispatcher
 split 'src/elasticsearch' elasticsearch
 split 'src/event' event
 split 'src/framework' framework
+split 'src/grpc' grpc
 split 'src/grpc-client' grpc-client
 split 'src/grpc-server' grpc-server
 split 'src/guzzle' guzzle
 split 'src/http-message' http-message
 split 'src/http-server' http-server
+split 'src/json-rpc' json-rpc
+split 'src/load-balancer' load-balancer
 split 'src/logger' logger
 split 'src/memory' memory
 split 'src/model-cache' model-cache
@@ -81,5 +93,11 @@ split 'src/pool' pool
 split 'src/process' process
 split 'src/rate-limit' rate-limit
 split 'src/redis' redis
+split 'src/rpc' rpc
+split 'src/rpc-client' rpc-client
+split 'src/rpc-server' rpc-server
+split 'src/server' server
+split 'src/service-governance' service-governance
 split 'src/tracer' tracer
+split 'src/swagger' swagger
 split 'src/utils' utils
