ad25471ebd3ddc528e7f3d95b1464e1cd69170ee
huangzhhui <huangzhwork@gmail.com>
2019-05-18 21:50:25 +0800

    Removed all helper methods

--- src/http-message/src/Server/Response.php
+++ src/http-message/src/Server/Response.php
@@ -12,21 +12,13 @@ declare(strict_types=1);
 
 namespace Hyperf\HttpMessage\Server;
 
-use Hyperf\Contract\Arrayable;
-use Hyperf\Helper\JsonHelper;
-use Hyperf\Helper\StringHelper;
 use Hyperf\HttpMessage\Cookie\Cookie;
 use Hyperf\HttpMessage\Stream\SwooleStream;
 
 class Response extends \Hyperf\HttpMessage\Base\Response
 {
     /**
-     * @var null|\Throwable
-     */
-    protected $exception;
-
-    /**
-     * @var \Swoole\Http\Response
+     * @var null|\Swoole\Http\Response
      */
     protected $swooleResponse;
 
@@ -36,114 +28,81 @@ class Response extends \Hyperf\HttpMessage\Base\Response
     protected $cookies = [];
 
     /**
-     * @param \Swoole\Http\Response $response
+     * @param null|\Swoole\Http\Response $response
      */
-    public function __construct(\Swoole\Http\Response $response)
+    public function __construct(\Swoole\Http\Response $response = null)
     {
         $this->swooleResponse = $response;
     }
 
     /**
-     * Redirect to a URL.
-     *
-     * @param string $url
-     * @param null|int $status
-     * @return static
-     */
-    public function redirect($url, $status = 302)
-    {
-        $response = $this;
-        return $response->withAddedHeader('Location', (string) $url)->withStatus($status);
-    }
-
-    /**
-     * return a Raw format response.
-     *
-     * @param string $data The data
-     * @param int $status the HTTP status code
-     * @return \Hyperf\HttpMessage\Server\Response when $data not jsonable
-     */
-    public function raw(string $data = '', int $status = 200): Response
-    {
-        $response = $this;
-
-        // Headers
-        $response = $response->withoutHeader('Content-Type')->withAddedHeader('Content-Type', 'text/plain');
-        $this->getCharset() && $response = $response->withCharset($this->getCharset());
-
-        // Content
-        $data && $response = $response->withContent($data);
-
-        // Status code
-        $status && $response = $response->withStatus($status);
-
-        return $response;
-    }
-
-    /**
-     * return a Json format response.
-     *
-     * @param array|Arrayable $data The data
-     * @param int $status the HTTP status code
-     * @param int $encodingOptions Json encoding options
-     * @throws \InvalidArgumentException
-     * @return static when $data not jsonable
-     */
-    public function json($data = [], int $status = 200, int $encodingOptions = JSON_UNESCAPED_UNICODE): Response
-    {
-        $response = $this;
-
-        // Headers
-        $response = $response->withoutHeader('Content-Type')->withAddedHeader('Content-Type', 'application/json');
-        $this->getCharset() && $response = $response->withCharset($this->getCharset());
-
-        // Content
-        if ($data && ($this->isArrayable($data) || is_string($data))) {
-            is_string($data) && $data = ['data' => $data];
-            $content = JsonHelper::encode($data, $encodingOptions);
-            $response = $response->withContent($content);
-        } else {
-            $response = $response->withContent('{}');
-        }
-
-        // Status code
-        $status && $response = $response->withStatus($status);
-
-        return $response;
-    }
-
-    /**
-     * 处理 Response 并发送数据.
+     * Handle response and send.
      */
     public function send()
     {
-        $response = $this;
+        if (! $this->getSwooleResponse()) {
+            return;
+        }
 
+        $this->buildSwooleResponse($this->swooleResponse, $this);
+
+        $this->swooleResponse->end($this->getBody()->getContents());
+    }
+
+    /**
+     * Returns an instance with body content.
+     */
+    public function withContent(string $content): self
+    {
+        $new = clone $this;
+        $new->stream = new SwooleStream($content);
+        return $new;
+    }
+
+    /**
+     * Return an instance with specified cookies.
+     */
+    public function withCookie(Cookie $cookie): self
+    {
+        $clone = clone $this;
+        $clone->cookies[$cookie->getDomain()][$cookie->getPath()][$cookie->getName()] = $cookie;
+        return $clone;
+    }
+
+    public function getSwooleResponse(): ?\Swoole\Http\Response
+    {
+        return $this->swooleResponse;
+    }
+
+    public function setSwooleResponse(\Swoole\Http\Response $swooleResponse): self
+    {
+        $this->swooleResponse = $swooleResponse;
+        return $this;
+    }
+
+    /**
+     * Keep this method at public level,
+     * allows the proxy class to override this method,
+     * or override the method that used this method.
+     */
+    public function buildSwooleResponse(\Swoole\Http\Response $swooleResponse, Response $response): void
+    {
         /*
          * Headers
          */
-        // Write Headers to swoole response
         foreach ($response->getHeaders() as $key => $value) {
-            $this->swooleResponse->header($key, implode(';', $value));
+            $swooleResponse->header($key, implode(';', $value));
         }
 
         /*
          * Cookies
          */
-        foreach ((array) $this->cookies as $domain => $paths) {
+        foreach ((array)$this->cookies as $domain => $paths) {
             foreach ($paths ?? [] as $path => $item) {
                 foreach ($item ?? [] as $name => $cookie) {
                     if ($cookie instanceof Cookie) {
                         $value = $cookie->isRaw() ? $cookie->getValue() : rawurlencode($cookie->getValue());
-                        $this->swooleResponse->rawcookie(
-                            $cookie->getName(),
-                            $value,
-                            $cookie->getExpiresTime(),
-                            $cookie->getPath(),
-                            $cookie->getDomain(),
-                            $cookie->isSecure(),
-                            $cookie->isHttpOnly()
-                        );
+                        $swooleResponse->rawcookie($cookie->getName(), $value, $cookie->getExpiresTime(), $cookie->getPath(), $cookie->getDomain(), $cookie->isSecure(), $cookie->isHttpOnly());
                     }
                 }
             }
@@ -152,96 +111,6 @@ class Response extends \Hyperf\HttpMessage\Base\Response
         /*
          * Status code
          */
-        $this->swooleResponse->status($response->getStatusCode());
-
-        /*
-         * Body
-         */
-        $this->swooleResponse->end($response->getBody()->getContents());
-    }
-
-    /**
-     * 设置Body内容，使用默认的Stream.
-     *
-     * @param string $content
-     * @return static
-     */
-    public function withContent($content): Response
-    {
-        if ($this->stream) {
-            return $this;
-        }
-
-        $new = clone $this;
-        $new->stream = new SwooleStream($content);
-        return $new;
-    }
-
-    /**
-     * Return an instance with specified cookies.
-     *
-     * @param Cookie $cookie
-     * @return static
-     */
-    public function withCookie(Cookie $cookie)
-    {
-        $clone = clone $this;
-        $clone->cookies[$cookie->getDomain()][$cookie->getPath()][$cookie->getName()] = $cookie;
-        return $clone;
-    }
-
-    /**
-     * @return null|\Throwable
-     */
-    public function getException()
-    {
-        return $this->exception;
-    }
-
-    /**
-     * @param \Throwable $exception
-     * @return $this
-     */
-    public function setException(\Throwable $exception)
-    {
-        $this->exception = $exception;
-        return $this;
-    }
-
-    /**
-     * @param mixed $value
-     * @return bool
-     */
-    public function isArrayable($value): bool
-    {
-        return is_array($value) || $value instanceof Arrayable;
-    }
-
-    /**
-     * @param string $accept
-     * @param string $keyword
-     * @return bool
-     */
-    public function isMatchAccept(string $accept, string $keyword): bool
-    {
-        return StringHelper::contains($accept, $keyword) === true;
-    }
-
-    /**
-     * @return \Swoole\Http\Response
-     */
-    public function getSwooleResponse(): \Swoole\Http\Response
-    {
-        return $this->swooleResponse;
-    }
-
-    /**
-     * @param \Swoole\Http\Response $swooleResponse
-     * @return $this
-     */
-    public function setSwooleResponse(\Swoole\Http\Response $swooleResponse)
-    {
-        $this->swooleResponse = $swooleResponse;
-        return $this;
+        $swooleResponse->status($response->getStatusCode());
     }
 }
