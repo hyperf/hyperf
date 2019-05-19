4151e6c30aa8d8ef355f594f1ba0d21c855ee013
huangzhhui <huangzhwork@gmail.com>
2019-05-19 14:28:52 +0800

    Added FailCache() annotation

---
+++ src/cache/src/Annotation/FailCache.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+namespace Hyperf\Cache\Annotation;
+
+use Hyperf\Cache\CacheListenerCollector;
+use Hyperf\Di\Annotation\AbstractAnnotation;
+use Hyperf\Di\Annotation\AnnotationCollector;
+
+/**
+ * @Annotation
+ * @Target({"METHOD"})
+ */
+class FailCache extends AbstractAnnotation
+{
+    public $prefix;
+
+    public $value;
+
+    public $ttl;
+
+    public $listener;
+
+    public $group = 'default';
+
+    public function collectMethod(string $className, ?string $target): void
+    {
+        if (isset($this->listener)) {
+            CacheListenerCollector::set($this->listener, [
+                'className' => $className,
+                'method' => $target,
+            ]);
+        }
+
+        AnnotationCollector::collectMethod($className, $target, static::class, $this);
+    }
+}
---
+++ src/cache/src/Aspect/FailCacheAspect.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+namespace Hyperf\Cache\Aspect;
+
+use Hyperf\Cache\Annotation\FailCache;
+use Hyperf\Cache\AnnotationManager;
+use Hyperf\Cache\CacheManager;
+use Hyperf\Contract\StdoutLoggerInterface;
+use Hyperf\Di\Annotation\Aspect;
+use Hyperf\Di\Aop\AbstractAspect;
+use Hyperf\Di\Aop\ProceedingJoinPoint;
+use Psr\Container\ContainerInterface;
+
+/**
+ * @Aspect
+ */
+class FailCacheAspect extends AbstractAspect
+{
+    public $classes = [];
+
+    public $annotations = [
+        FailCache::class,
+    ];
+
+    /**
+     * @var ContainerInterface
+     */
+    protected $manager;
+
+    /**
+     * @var AnnotationManager
+     */
+    protected $annotationManager;
+
+    /**
+     * @var StdoutLoggerInterface
+     */
+    private $logger;
+
+    public function __construct(CacheManager $manager, AnnotationManager $annotationManager, StdoutLoggerInterface $logger)
+    {
+        $this->manager = $manager;
+        $this->annotationManager = $annotationManager;
+        $this->logger = $logger;
+    }
+
+    public function process(ProceedingJoinPoint $proceedingJoinPoint)
+    {
+        $className = $proceedingJoinPoint->className;
+        $method = $proceedingJoinPoint->methodName;
+        $arguments = $proceedingJoinPoint->arguments['keys'];
+
+        [$key, $ttl, $group] = $this->annotationManager->getFailCacheValue($className, $method, $arguments);
+
+        $driver = $this->manager->getDriver($group);
+
+        try {
+            $result = $proceedingJoinPoint->process();
+            $driver->set($key, $result, $ttl);
+        } catch (\Throwable $throwable) {
+            [$has, $result] = $driver->fetch($key);
+            if (! $has) {
+                throw $throwable;
+            }
+            $this->logger->debug(sprintf('Returns fail cache [%s]', $key));
+        }
+
+        return $result;
+    }
+}
