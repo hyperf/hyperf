00a4c55085b0e437dc8eba37e88180ca1d678801
huangzhhui <huangzhwork@gmail.com>
2019-05-27 14:08:46 +0800

    Moved make() function to utils component from di component

--- src/utils/src/Functions.php
+++ src/utils/src/Functions.php
@@ -10,6 +10,7 @@ declare(strict_types=1);
  * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
  */
 
+use Hyperf\Utils\ApplicationContext;
 use Hyperf\Utils\Arr;
 use Hyperf\Utils\Collection;
 use Hyperf\Utils\HigherOrderTapProxy;
@@ -380,5 +381,25 @@ if (! function_exists('parallel')) {
             $parallel->add($callable, $key);
         }
         return $parallel->wait();
+    }
+}
+
+if (! function_exists('make')) {
+    /**
+     * Create a object instance, if the DI container exist in ApplicationContext,
+     * then the object will be create by DI container via `make()` method, if not,
+     * the object will create by `new` keyword.
+     */
+    function make(string $name, array $parameters = [])
+    {
+        if (ApplicationContext::hasContainer()) {
+            $container = ApplicationContext::getContainer();
+            if (! method_exists($container, 'make')) {
+                throw new \RuntimeException(sprintf('Make error, make() method does not exist in %s', get_class($container)));
+            }
+            return $container->make($name, $parameters);
+        }
+        $parameters = array_values($parameters);
+        return new $name(...$parameters);
     }
 }
