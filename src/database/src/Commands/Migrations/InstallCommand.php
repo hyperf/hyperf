0e72406bb9ff3c17d86aba4ae88ad2f89bfc443c
huangzhhui <huangzhwork@gmail.com>
2019-05-29 20:23:43 +0800

    Added migrate and migrate:install commands

--- src/database/src/Commands/Migrations/InstallCommand.php
+++ src/database/src/Commands/Migrations/InstallCommand.php
@@ -1,14 +1,21 @@
 <?php
 
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
 namespace Hyperf\Database\Commands\Migrations;
 
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Hyperf\Database\Migrations\MigrationRepositoryInterface;
-use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
 
-class InstallCommand extends Command
+class InstallCommand extends BaseCommand
 {
     /**
      * The console command name.
@@ -32,45 +39,15 @@ class InstallCommand extends Command
     protected $repository;
 
     /**
-     * The input interface implementation.
-     *
-     * @var InputInterface
-     */
-    protected $input;
-
-    /**
-     * The output interface implementation.
-     *
-     * @var OutputInterface
-     */
-    protected $output;
-
-    /**
      * Create a new migration install command instance.
      *
-     * @param  \Hyperf\Database\Migrations\MigrationRepositoryInterface $repository
-     * @return void
+     * @param MigrationRepositoryInterface $repository
      */
     public function __construct(MigrationRepositoryInterface $repository)
     {
         parent::__construct();
-        $this->specifyParameters();
 
         $this->repository = $repository;
-    }
-
-    /**
-     * Execute the console command.
-     *
-     * @return void
-     */
-    public function execute(InputInterface $input, OutputInterface $output)
-    {
-        $this->repository->setSource($input->getOption('database'));
-
-        $this->repository->createRepository();
-
-        $output->writeln('<info>Migration table created successfully.</info>');
     }
 
     /**
@@ -86,23 +63,14 @@ class InstallCommand extends Command
     }
 
     /**
-     * Specify the arguments and options on the command.
+     * Handle the current command.
      */
-    protected function specifyParameters(): void
+    public function handle()
     {
-        // We will loop through all of the arguments and options for the command and
-        // set them all on the base command instance. This specifies what can get
-        // passed into these commands as "parameters" to control the execution.
-        if (method_exists($this, 'getArguments')) {
-            foreach ($this->getArguments() ?? [] as $arguments) {
-                call_user_func_array([$this, 'addArgument'], $arguments);
-            }
-        }
+        $this->repository->setSource($this->input->getOption('database'));
 
-        if (method_exists($this, 'getOptions')) {
-            foreach ($this->getOptions() ?? [] as $options) {
-                call_user_func_array([$this, 'addOption'], $options);
-            }
-        }
+        $this->repository->createRepository();
+
+        $this->output->writeln('<info>[INFO] Migration table created successfully.</info>');
     }
 }
---
+++ src/database/src/Commands/Migrations/MigrateCommand.php
@@ -0,0 +1,105 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+namespace Hyperf\Database\Commands\Migrations;
+
+use Hyperf\Command\ConfirmableTrait;
+use Hyperf\Database\Migrations\Migrator;
+use Symfony\Component\Console\Input\InputOption;
+
+class MigrateCommand extends BaseCommand
+{
+    use ConfirmableTrait;
+
+    protected $name = 'migrate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Run the database migrations';
+
+    /**
+     * The migrator instance.
+     *
+     * @var \Hyperf\Database\Migrations\Migrator
+     */
+    protected $migrator;
+
+    /**
+     * Create a new migration command instance.
+     *
+     * @param \Hyperf\Database\Migrations\Migrator $migrator
+     */
+    public function __construct(Migrator $migrator)
+    {
+        parent::__construct();
+
+        $this->migrator = $migrator;
+    }
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        if (! $this->confirmToProceed()) {
+            return;
+        }
+
+        $this->prepareDatabase();
+
+        // Next, we will check to see if a path option has been defined. If it has
+        // we will use the path relative to the root of this installation folder
+        // so that migrations may be run for any path within the applications.
+        $this->migrator->setOutput($this->output)
+            ->run($this->getMigrationPaths(), [
+                'pretend' => $this->input->getOption('pretend'),
+                'step' => $this->input->getOption('step'),
+            ]);
+
+        // Finally, if the "seed" option has been given, we will re-run the database
+        // seed task to re-populate the database, which is convenient when adding
+        // a migration and a seed at the same time, as it is only this command.
+        if ($this->input->getOption('seed') && ! $this->input->getOption('pretend')) {
+            $this->call('db:seed', ['--force' => true]);
+        }
+    }
+
+    protected function getOptions(): array
+    {
+        return [
+            ['database', null, InputOption::VALUE_OPTIONAL, 'The database connection to use'],
+            ['force', null, InputOption::VALUE_NONE, 'Force the operation to run when in production'],
+            ['path', null, InputOption::VALUE_OPTIONAL, 'The path to the migrations files to be executed'],
+            ['realpath', null, InputOption::VALUE_NONE, 'Indicate any provided migration file paths are pre-resolved absolute paths'],
+            ['pretend', null, InputOption::VALUE_NONE, 'Dump the SQL queries that would be run'],
+            ['seed', null, InputOption::VALUE_NONE, 'Indicates if the seed task should be re-run'],
+            ['step', null, InputOption::VALUE_NONE, 'Force the migrations to be run so they can be rolled back individually'],
+        ];
+    }
+
+    /**
+     * Prepare the migration database for running.
+     */
+    protected function prepareDatabase()
+    {
+        $this->migrator->setConnection($this->input->getOption('database') ?? 'default');
+
+        if (! $this->migrator->repositoryExists()) {
+            $this->call('migrate:install', array_filter([
+                '--database' => $this->input->getOption('database'),
+            ]));
+        }
+    }
+}
