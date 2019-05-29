819a410138579367b82e652435e8534aa61c9221
huangzhhui <huangzhwork@gmail.com>
2019-05-29 20:26:39 +0800

    Added migrate:status command

---
+++ src/database/src/Commands/Migrations/StatusCommand.php
@@ -0,0 +1,118 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+namespace Hyperf\Database\Commands\Migrations;
+
+use Hyperf\Database\Migrations\Migrator;
+use Hyperf\Utils\Collection;
+use Symfony\Component\Console\Input\InputOption;
+
+class StatusCommand extends BaseCommand
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'migrate:status';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Show the status of each migration';
+
+    /**
+     * The migrator instance.
+     *
+     * @var \Hyperf\Database\Migrations\Migrator
+     */
+    protected $migrator;
+
+    /**
+     * Create a new migration rollback command instance.
+     */
+    public function __construct(Migrator $migrator)
+    {
+        parent::__construct();
+
+        $this->migrator = $migrator;
+    }
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->migrator->setConnection($this->input->getOption('database') ?? 'default');
+
+        if (! $this->migrator->repositoryExists()) {
+            return $this->error('Migration table not found.');
+        }
+
+        $ran = $this->migrator->getRepository()->getRan();
+
+        $batches = $this->migrator->getRepository()->getMigrationBatches();
+
+        if (count($migrations = $this->getStatusFor($ran, $batches)) > 0) {
+            $this->table(['Ran?', 'Migration', 'Batch'], $migrations);
+        } else {
+            $this->error('No migrations found');
+        }
+    }
+
+    /**
+     * Get the status for the given ran migrations.
+     *
+     * @param array $ran
+     * @param array $batches
+     * @return \Hyperf\Support\Collection
+     */
+    protected function getStatusFor(array $ran, array $batches)
+    {
+        return Collection::make($this->getAllMigrationFiles())
+            ->map(function ($migration) use ($ran, $batches) {
+                        $migrationName = $this->migrator->getMigrationName($migration);
+
+                        return in_array($migrationName, $ran)
+                                ? ['<info>Yes</info>', $migrationName, $batches[$migrationName]]
+                                : ['<fg=red>No</fg=red>', $migrationName];
+                    });
+    }
+
+    /**
+     * Get an array of all of the migration files.
+     *
+     * @return array
+     */
+    protected function getAllMigrationFiles()
+    {
+        return $this->migrator->getMigrationFiles($this->getMigrationPaths());
+    }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return [
+            ['database', null, InputOption::VALUE_OPTIONAL, 'The database connection to use'],
+
+            ['path', null, InputOption::VALUE_OPTIONAL, 'The path to the migrations files to use'],
+
+            ['realpath', null, InputOption::VALUE_NONE, 'Indicate any provided migration file paths are pre-resolved absolute paths'],
+        ];
+    }
+}
--- src/db-connection/src/ConfigProvider.php
+++ src/db-connection/src/ConfigProvider.php
@@ -19,6 +19,7 @@ use Hyperf\Database\Commands\Migrations\MigrateCommand;
 use Hyperf\Database\Commands\Migrations\RefreshCommand;
 use Hyperf\Database\Commands\Migrations\ResetCommand;
 use Hyperf\Database\Commands\Migrations\RollbackCommand;
+use Hyperf\Database\Commands\Migrations\StatusCommand;
 use Hyperf\Database\Commands\ModelCommand;
 use Hyperf\Database\ConnectionResolverInterface;
 use Hyperf\Database\Connectors\ConnectionFactory;
@@ -47,6 +48,7 @@ class ConfigProvider
                 RefreshCommand::class,
                 ResetCommand::class,
                 RollbackCommand::class,
+                StatusCommand::class,
             ],
             'scan' => [
                 'paths' => [
