8f626f08e701bd29c34c8110834d98f47fcef681
huangzhhui <huangzhwork@gmail.com>
2019-05-29 20:21:45 +0800

    Fixed non-exist method

--- src/database/src/Commands/Migrations/BaseCommand.php
+++ src/database/src/Commands/Migrations/BaseCommand.php
@@ -45,7 +45,7 @@ abstract class BaseCommand extends Command
      */
     protected function usingRealPath()
     {
-        return $this->input->hasOption('realpath') && $this->option('realpath');
+        return $this->input->hasOption('realpath') && $this->input->getOption('realpath');
     }
 
     /**
