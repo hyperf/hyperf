8bdf6a1a04ed1ba1041f041aa380e0c5972123a5
huangzhhui <huangzhwork@gmail.com>
2019-05-31 16:49:02 +0800

    Added unit test of migrate:rollback command

---
+++ src/database/tests/DatabaseMigrationRollbackCommandTest.php
@@ -0,0 +1,89 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+namespace HyperfTest\Database;
+
+use Hyperf\Database\Commands\Migrations\RollbackCommand;
+use Hyperf\Database\Migrations\Migrator;
+use Mockery;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\NullOutput;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+class DatabaseMigrationRollbackCommandTest extends TestCase
+{
+    use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        ! defined('BASE_PATH') && define('BASE_PATH', __DIR__);
+    }
+
+    protected function tearDown(): void
+    {
+        Mockery::close();
+    }
+
+    public function testRollbackCommandCallsMigratorWithProperArguments()
+    {
+        $command = new RollbackCommand($migrator = Mockery::mock(Migrator::class));
+        $migrator->shouldReceive('paths')->once()->andReturn([]);
+        $migrator->shouldReceive('setConnection')->once()->with('default');
+        $migrator->shouldReceive('setOutput')->once()->andReturn($migrator);
+        $migrator->shouldReceive('rollback')->once()->with([__DIR__ . DIRECTORY_SEPARATOR . 'migrations'], ['pretend' => false, 'step' => 0]);
+
+        $this->runCommand($command);
+    }
+
+    public function testRollbackCommandCallsMigratorWithStepOption()
+    {
+        $command = new RollbackCommand($migrator = Mockery::mock(Migrator::class));
+        $migrator->shouldReceive('paths')->once()->andReturn([]);
+        $migrator->shouldReceive('setConnection')->once()->with('default');
+        $migrator->shouldReceive('setOutput')->once()->andReturn($migrator);
+        $migrator->shouldReceive('rollback')->once()->with([__DIR__ . DIRECTORY_SEPARATOR . 'migrations'], ['pretend' => false, 'step' => 2]);
+
+        $this->runCommand($command, ['--step' => 2]);
+    }
+
+    public function testRollbackCommandCanBePretended()
+    {
+        $command = new RollbackCommand($migrator = Mockery::mock(Migrator::class));
+        $migrator->shouldReceive('paths')->once()->andReturn([]);
+        $migrator->shouldReceive('setConnection')->once()->with('foo');
+        $migrator->shouldReceive('setOutput')->once()->andReturn($migrator);
+        $migrator->shouldReceive('rollback')->once()->with([__DIR__ . DIRECTORY_SEPARATOR . 'migrations'], true);
+
+        $this->runCommand($command, ['--pretend' => true, '--database' => 'foo']);
+    }
+
+    public function testRollbackCommandCanBePretendedWithStepOption()
+    {
+        $command = new RollbackCommand($migrator = Mockery::mock(Migrator::class));
+        $migrator->shouldReceive('paths')->once()->andReturn([]);
+        $migrator->shouldReceive('setConnection')->once()->with('foo');
+        $migrator->shouldReceive('setOutput')->once()->andReturn($migrator);
+        $migrator->shouldReceive('rollback')->once()->with([__DIR__ . DIRECTORY_SEPARATOR . 'migrations'], ['pretend' => true, 'step' => 2]);
+
+        $this->runCommand($command, ['--pretend' => true, '--database' => 'foo', '--step' => 2]);
+    }
+
+    protected function runCommand($command, $input = [])
+    {
+        return $command->run(new ArrayInput($input), new NullOutput());
+    }
+}
