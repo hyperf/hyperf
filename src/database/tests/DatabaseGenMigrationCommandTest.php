d831d6e2f6b95a5cc6cd726b75cf4769460f96e8
huangzhhui <huangzhwork@gmail.com>
2019-06-01 19:30:41 +0800

    Added unit test

---
+++ src/database/tests/DatabaseGenMigrationCommandTest.php
@@ -0,0 +1,94 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+namespace HyperfTest\Database;
+
+use Hyperf\Database\Commands\Migrations\GenMigrateCommand;
+use Hyperf\Database\Migrations\MigrationCreator;
+use Mockery;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\NullOutput;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+class DatabaseGenMigrationCommandTest extends TestCase
+{
+    use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        ! defined('BASE_PATH') && define('BASE_PATH', __DIR__);
+    }
+
+    protected function tearDown(): void
+    {
+        Mockery::close();
+    }
+
+    public function testBasicCreateGivesCreatorProperArguments()
+    {
+        $command = new GenMigrateCommand(
+            $creator = Mockery::mock(MigrationCreator::class)
+        );
+        $creator->shouldReceive('create')->once()->with('create_foo', __DIR__ . DIRECTORY_SEPARATOR . 'migrations', 'foo', true)->andReturn('');
+
+        $this->runCommand($command, ['name' => 'create_foo']);
+    }
+
+    public function testBasicCreateGivesCreatorProperArgumentsWhenNameIsStudlyCase()
+    {
+        $command = new GenMigrateCommand(
+            $creator = Mockery::mock(MigrationCreator::class)
+        );
+        $creator->shouldReceive('create')->once()->with('create_foo', __DIR__ . DIRECTORY_SEPARATOR . 'migrations', 'foo', true)->andReturn('');
+
+        $this->runCommand($command, ['name' => 'CreateFoo']);
+    }
+
+    public function testBasicCreateGivesCreatorProperArgumentsWhenTableIsSet()
+    {
+        $command = new GenMigrateCommand(
+            $creator = Mockery::mock(MigrationCreator::class)
+        );
+        $creator->shouldReceive('create')->once()->with('create_foo', __DIR__ . DIRECTORY_SEPARATOR . 'migrations', 'users', true)->andReturn('');
+
+        $this->runCommand($command, ['name' => 'create_foo', '--create' => 'users']);
+    }
+
+    public function testBasicCreateGivesCreatorProperArgumentsWhenCreateTablePatternIsFound()
+    {
+        $command = new GenMigrateCommand(
+            $creator = Mockery::mock(MigrationCreator::class)
+        );
+        $creator->shouldReceive('create')->once()->with('create_users_table', __DIR__ . DIRECTORY_SEPARATOR . 'migrations', 'users', true)->andReturn('');
+
+        $this->runCommand($command, ['name' => 'create_users_table']);
+    }
+
+    public function testCanSpecifyPathToCreateMigrationsIn()
+    {
+        $command = new GenMigrateCommand(
+            $creator = Mockery::mock(MigrationCreator::class)
+        );
+        $creator->shouldReceive('create')->once()->with('create_foo', BASE_PATH . '/vendor/hyperf/migrations', 'users', true)->andReturn('');
+        $this->runCommand($command, ['name' => 'create_foo', '--path' => 'vendor/hyperf/migrations', '--create' => 'users']);
+    }
+
+    protected function runCommand($command, $input = [])
+    {
+        return $command->run(new ArrayInput($input), new NullOutput());
+    }
+}
---
+++ src/database/tests/DatabaseMigrationRepositoryTest.php
@@ -0,0 +1,137 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+namespace HyperfTest\Database;
+
+use Closure;
+use Hyperf\Database\Connection;
+use Hyperf\Database\ConnectionResolverInterface;
+use Hyperf\Database\Migrations\DatabaseMigrationRepository;
+use Hyperf\Database\Query\Builder;
+use Hyperf\Utils\Collection;
+use Mockery;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+class DatabaseMigrationRepositoryTest extends TestCase
+{
+    use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+
+    protected function tearDown(): void
+    {
+        Mockery::close();
+    }
+
+    public function testGetRanMigrationsListMigrationsByPackage()
+    {
+        $repo = $this->getRepository();
+        $query = Mockery::mock(Builder::class);
+        $connectionMock = Mockery::mock(Connection::class);
+        $repo->getConnectionResolver()->shouldReceive('connection')->with(null)->andReturn($connectionMock);
+        $repo->getConnection()->shouldReceive('table')->once()->with('migrations')->andReturn($query);
+        $query->shouldReceive('orderBy')->once()->with('batch', 'asc')->andReturn($query);
+        $query->shouldReceive('orderBy')->once()->with('migration', 'asc')->andReturn($query);
+        $query->shouldReceive('pluck')->once()->with('migration')->andReturn(new Collection(['bar']));
+        $query->shouldReceive('useWritePdo')->once()->andReturn($query);
+
+        $this->assertEquals(['bar'], $repo->getRan());
+    }
+
+    public function testGetLastMigrationsGetsAllMigrationsWithTheLatestBatchNumber()
+    {
+        $repo = $this->getMockBuilder(DatabaseMigrationRepository::class)->setMethods(['getLastBatchNumber'])->setConstructorArgs([
+            $resolver = Mockery::mock(ConnectionResolverInterface::class), 'migrations',
+        ])->getMock();
+        $repo->expects($this->once())->method('getLastBatchNumber')->will($this->returnValue(1));
+        $query = Mockery::mock(Builder::class);
+        $connectionMock = Mockery::mock(Connection::class);
+        $repo->getConnectionResolver()->shouldReceive('connection')->with(null)->andReturn($connectionMock);
+        $repo->getConnection()->shouldReceive('table')->once()->with('migrations')->andReturn($query);
+        $query->shouldReceive('where')->once()->with('batch', 1)->andReturn($query);
+        $query->shouldReceive('orderBy')->once()->with('migration', 'desc')->andReturn($query);
+        $query->shouldReceive('get')->once()->andReturn(new Collection(['foo']));
+        $query->shouldReceive('useWritePdo')->once()->andReturn($query);
+
+        $this->assertEquals(['foo'], $repo->getLast());
+    }
+
+    public function testLogMethodInsertsRecordIntoMigrationTable()
+    {
+        $repo = $this->getRepository();
+        $query = Mockery::mock(Builder::class);
+        $connectionMock = Mockery::mock(Connection::class);
+        $repo->getConnectionResolver()->shouldReceive('connection')->with(null)->andReturn($connectionMock);
+        $repo->getConnection()->shouldReceive('table')->once()->with('migrations')->andReturn($query);
+        $query->shouldReceive('insert')->once()->with(['migration' => 'bar', 'batch' => 1]);
+        $query->shouldReceive('useWritePdo')->once()->andReturn($query);
+
+        $repo->log('bar', 1);
+    }
+
+    public function testDeleteMethodRemovesAMigrationFromTheTable()
+    {
+        $repo = $this->getRepository();
+        $query = Mockery::mock(Builder::class);
+        $connectionMock = Mockery::mock(Connection::class);
+        $repo->getConnectionResolver()->shouldReceive('connection')->with(null)->andReturn($connectionMock);
+        $repo->getConnection()->shouldReceive('table')->once()->with('migrations')->andReturn($query);
+        $query->shouldReceive('where')->once()->with('migration', 'foo')->andReturn($query);
+        $query->shouldReceive('delete')->once();
+        $query->shouldReceive('useWritePdo')->once()->andReturn($query);
+        $migration = (object) ['migration' => 'foo'];
+
+        $repo->delete($migration);
+    }
+
+    public function testGetNextBatchNumberReturnsLastBatchNumberPlusOne()
+    {
+        $repo = $this->getMockBuilder(DatabaseMigrationRepository::class)->setMethods(['getLastBatchNumber'])->setConstructorArgs([
+            Mockery::mock(ConnectionResolverInterface::class), 'migrations',
+        ])->getMock();
+        $repo->expects($this->once())->method('getLastBatchNumber')->will($this->returnValue(1));
+
+        $this->assertEquals(2, $repo->getNextBatchNumber());
+    }
+
+    public function testGetLastBatchNumberReturnsMaxBatch()
+    {
+        $repo = $this->getRepository();
+        $query = Mockery::mock(Builder::class);
+        $connectionMock = Mockery::mock(Connection::class);
+        $repo->getConnectionResolver()->shouldReceive('connection')->with(null)->andReturn($connectionMock);
+        $repo->getConnection()->shouldReceive('table')->once()->with('migrations')->andReturn($query);
+        $query->shouldReceive('max')->once()->andReturn(1);
+        $query->shouldReceive('useWritePdo')->once()->andReturn($query);
+
+        $this->assertEquals(1, $repo->getLastBatchNumber());
+    }
+
+    public function testCreateRepositoryCreatesProperDatabaseTable()
+    {
+        $repo = $this->getRepository();
+        $schema = Mockery::mock(Builder::class);
+        $connectionMock = Mockery::mock(Connection::class);
+        $repo->getConnectionResolver()->shouldReceive('connection')->with(null)->andReturn($connectionMock);
+        $repo->getConnection()->shouldReceive('getSchemaBuilder')->once()->andReturn($schema);
+        $schema->shouldReceive('create')->once()->with('migrations', Mockery::type(Closure::class));
+
+        $repo->createRepository();
+    }
+
+    protected function getRepository()
+    {
+        return new DatabaseMigrationRepository(Mockery::mock(ConnectionResolverInterface::class), 'migrations');
+    }
+}
