1a2360b796806a18616af4b6a090d4435c9fcaa8
huangzhhui <huangzhwork@gmail.com>
2019-06-04 00:46:55 +0800

    Added migrator integration unit test

---
+++ src/database/tests/DatabaseMigratorIntegrationTest.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace HyperfTest\Database;
+
+use Hyperf\Container\Container;
+use Hyperf\Database\ConnectionResolver;
+use Hyperf\Database\ConnectionResolverInterface;
+use Hyperf\Database\Connectors\ConnectionFactory;
+use Hyperf\Database\Connectors\MySqlConnector;
+use Hyperf\Database\Migrations\DatabaseMigrationRepository;
+use Hyperf\Database\Migrations\Migrator;
+use Hyperf\Database\Schema\Schema;
+use Hyperf\Utils\ApplicationContext;
+use Hyperf\Utils\Filesystem\Filesystem;
+use Hyperf\Utils\Str;
+use Mockery as m;
+use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerInterface;
+use Symfony\Component\Console\Style\OutputStyle;
+
+class DatabaseMigratorIntegrationTest extends TestCase
+{
+    protected $migrator;
+
+    protected function setUp(): void
+    {
+        $container = m::mock(ContainerInterface::class);
+        $container->shouldReceive('has')->andReturn(true);
+        $container->shouldReceive('get')->with('db.connector.mysql')->andReturn(new MySqlConnector());
+        $connector = new ConnectionFactory($container);
+
+        $dbConfig = [
+            'driver' => 'mysql',
+            'host' => 'localhost',
+            'database' => 'hyperf',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => '',
+        ];
+
+        $connection = $connector->make($dbConfig);
+
+        $resolver = new ConnectionResolver(['default' => $connection]);
+
+        $container->shouldReceive('get')->with(ConnectionResolverInterface::class)->andReturn($resolver);
+
+        ApplicationContext::setContainer($container);
+
+        $this->migrator = new Migrator(
+            $repository = new DatabaseMigrationRepository($resolver, 'migrations'),
+            $resolver,
+            new Filesystem
+        );
+
+        $output = m::mock(OutputStyle::class);
+        $output->shouldReceive('writeln');
+
+        $this->migrator->setOutput($output);
+
+        if (! $repository->repositoryExists()) {
+            $repository->createRepository();
+        }
+    }
+
+    public function testBasicMigrationOfSingleFolder()
+    {
+        $schema = new Schema();
+        $ran = $this->migrator->run([__DIR__.'/migrations/one']);
+
+        $this->assertTrue($schema->hasTable('users'));
+        $this->assertTrue($schema->hasTable('password_resets'));
+
+        $this->assertTrue(Str::contains($ran[0], 'users'));
+        $this->assertTrue(Str::contains($ran[1], 'password_resets'));
+    }
+
+    public function testMigrationsCanBeRolledBack()
+    {
+        $schema = new Schema();
+        $this->migrator->run([__DIR__.'/migrations/one']);
+        $this->assertTrue($schema->hasTable('users'));
+        $this->assertTrue($schema->hasTable('password_resets'));
+        $rolledBack = $this->migrator->rollback([__DIR__.'/migrations/one']);
+        $this->assertFalse($schema->hasTable('users'));
+        $this->assertFalse($schema->hasTable('password_resets'));
+
+        $this->assertTrue(Str::contains($rolledBack[0], 'password_resets'));
+        $this->assertTrue(Str::contains($rolledBack[1], 'users'));
+    }
+
+    public function testMigrationsCanBeReset()
+    {
+        $schema = new Schema();
+        $this->migrator->run([__DIR__.'/migrations/one']);
+        $this->assertTrue($schema->hasTable('users'));
+        $this->assertTrue($schema->hasTable('password_resets'));
+        $rolledBack = $this->migrator->reset([__DIR__.'/migrations/one']);
+        $this->assertFalse($schema->hasTable('users'));
+        $this->assertFalse($schema->hasTable('password_resets'));
+
+        $this->assertTrue(Str::contains($rolledBack[0], 'password_resets'));
+        $this->assertTrue(Str::contains($rolledBack[1], 'users'));
+    }
+
+    public function testNoErrorIsThrownWhenNoOutstandingMigrationsExist()
+    {
+        $schema = new Schema();
+        $this->migrator->run([__DIR__.'/migrations/one']);
+        $this->assertTrue($schema->hasTable('users'));
+        $this->assertTrue($schema->hasTable('password_resets'));
+        $this->migrator->run([__DIR__.'/migrations/one']);
+    }
+
+    public function testNoErrorIsThrownWhenNothingToRollback()
+    {
+        $schema = new Schema();
+        $this->migrator->run([__DIR__.'/migrations/one']);
+        $this->assertTrue($schema->hasTable('users'));
+        $this->assertTrue($schema->hasTable('password_resets'));
+        $this->migrator->rollback([__DIR__.'/migrations/one']);
+        $this->assertFalse($schema->hasTable('users'));
+        $this->assertFalse($schema->hasTable('password_resets'));
+        $this->migrator->rollback([__DIR__.'/migrations/one']);
+    }
+
+    public function testMigrationsCanRunAcrossMultiplePaths()
+    {
+        $schema = new Schema();
+        $this->migrator->run([__DIR__.'/migrations/one', __DIR__.'/migrations/two']);
+        $this->assertTrue($schema->hasTable('users'));
+        $this->assertTrue($schema->hasTable('password_resets'));
+        $this->assertTrue($schema->hasTable('flights'));
+    }
+
+    public function testMigrationsCanBeRolledBackAcrossMultiplePaths()
+    {
+        $schema = new Schema();
+        $this->migrator->run([__DIR__.'/migrations/one', __DIR__.'/migrations/two']);
+        $this->assertTrue($schema->hasTable('users'));
+        $this->assertTrue($schema->hasTable('password_resets'));
+        $this->assertTrue($schema->hasTable('flights'));
+        $this->migrator->rollback([__DIR__.'/migrations/one', __DIR__.'/migrations/two']);
+        $this->assertFalse($schema->hasTable('users'));
+        $this->assertFalse($schema->hasTable('password_resets'));
+        $this->assertFalse($schema->hasTable('flights'));
+    }
+
+    public function testMigrationsCanBeResetAcrossMultiplePaths()
+    {
+        $schema = new Schema();
+        $this->migrator->run([__DIR__.'/migrations/one', __DIR__.'/migrations/two']);
+        $this->assertTrue($schema->hasTable('users'));
+        $this->assertTrue($schema->hasTable('password_resets'));
+        $this->assertTrue($schema->hasTable('flights'));
+        $this->migrator->reset([__DIR__.'/migrations/one', __DIR__.'/migrations/two']);
+        $this->assertFalse($schema->hasTable('users'));
+        $this->assertFalse($schema->hasTable('password_resets'));
+        $this->assertFalse($schema->hasTable('flights'));
+    }
+}
---
+++ src/database/tests/migrations/two/2016_01_01_200000_create_flights_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Hyperf\Database\Schema\Schema;
+use Hyperf\Database\Schema\Blueprint;
+use Hyperf\Database\Migrations\Migration;
+
+class CreateFlightsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('flights', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('flights');
+    }
+}
