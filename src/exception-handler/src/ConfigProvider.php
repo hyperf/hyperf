2ec7231ba8cfe166e43766e8384e388cea381d7b
李铭昕 <715557344@qq.com>
2019-06-06 15:03:40 +0800

    Added logger when throw exception.

---
+++ src/exception-handler/src/ConfigProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+namespace Hyperf\Logger;
+
+use Hyperf\ExceptionHandler\Formatter\DefaultFormatter;
+use Hyperf\ExceptionHandler\Formatter\FormatterInterface;
+
+class ConfigProvider
+{
+    public function __invoke(): array
+    {
+        return [
+            'dependencies' => [
+                FormatterInterface::class => DefaultFormatter::class
+            ],
+            'commands' => [
+            ],
+            'scan' => [
+                'paths' => [
+                    __DIR__,
+                ],
+            ],
+        ];
+    }
+}
---
+++ src/exception-handler/src/Formatter/DefaultFormatter.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+namespace Hyperf\ExceptionHandler\Formatter;
+
+use Throwable;
+
+class DefaultFormatter implements FormatterInterface
+{
+    public function format(Throwable $throwable): string
+    {
+        return sprintf(
+            "%s:%s(%s) in %s:%s\nStack trace:\n%s",
+            get_class($throwable),
+            $throwable->getMessage(),
+            $throwable->getCode(),
+            $throwable->getFile(),
+            $throwable->getLine(),
+            $throwable->getTraceAsString()
+        );
+    }
+}
