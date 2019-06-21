305de48c84a3f6c5859fac3b4780125274225fb6
李铭昕 <715557344@qq.com>
2019-06-21 16:53:52 +0800

    Optimized code.

--- src/websocket-server/src/Server.php
+++ src/websocket-server/src/Server.php
@@ -16,46 +16,12 @@ use Hyperf\Contract\ConfigInterface;
 use Hyperf\Contract\MiddlewareInitializerInterface;
 use Hyperf\Contract\OnRequestInterface;
 use Hyperf\Dispatcher\HttpDispatcher;
-use Hyperf\ExceptionHandler\ExceptionHandlerDispatcher;
-use Hyperf\HttpMessage\Server\Request as Psr7Request;
-use Hyperf\HttpMessage\Server\Response as Psr7Response;
 use Hyperf\HttpServer\Exception\Handler\HttpExceptionHandler;
-use Hyperf\HttpServer\MiddlewareManager;
-use Hyperf\Utils\Context;
+use Hyperf\HttpServer\Server as HttpServer;
 use Psr\Container\ContainerInterface;
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
-use Swoole\Http\Request as SwooleRequest;
-use Swoole\Http\Response as SwooleResponse;
-use Throwable;
 
-class Server implements MiddlewareInitializerInterface, OnRequestInterface
+class Server extends HttpServer implements MiddlewareInitializerInterface, OnRequestInterface
 {
-    /**
-     * @var ContainerInterface
-     */
-    protected $container;
-
-    /**
-     * @var CoreMiddleware
-     */
-    protected $coreMiddleware;
-
-    /**
-     * @var array
-     */
-    protected $middlewares = [];
-
-    /**
-     * @var string
-     */
-    protected $serverName = 'websocket';
-
-    /**
-     * @var HttpDispatcher
-     */
-    protected $dispatcher;
-
     public function __construct(ContainerInterface $container, string $serverName)
     {
         $this->container = $container;
@@ -71,40 +37,7 @@ class Server implements MiddlewareInitializerInterface, OnRequestInterface
         $config = $this->container->get(ConfigInterface::class);
         $this->middlewares = $config->get('middlewares.' . $serverName, []);
         $this->exceptionHandlers = $config->get('exceptions.handler.' . $serverName, [
-            // HttpExceptionHandler::class,
+            HttpExceptionHandler::class,
         ]);
-    }
-
-    public function onRequest(SwooleRequest $request, SwooleResponse $response): void
-    {
-        try {
-            /**
-             * @var ServerRequestInterface
-             * @var ResponseInterface $psr7Response
-             */
-            [$psr7Request, $psr7Response] = $this->initRequestAndResponse($request, $response);
-
-            $middlewares = array_merge($this->middlewares, MiddlewareManager::get($this->serverName, $psr7Request->getUri()->getPath(), $psr7Request->getMethod()));
-
-            $psr7Response = $this->dispatcher->dispatch($psr7Request, $middlewares, $this->coreMiddleware);
-        } catch (Throwable $throwable) {
-            // Delegate the exception to exception handler.
-            $exceptionHandlerDispatcher = $this->container->get(ExceptionHandlerDispatcher::class);
-            $psr7Response = $exceptionHandlerDispatcher->dispatch($throwable, $this->exceptionHandlers);
-        } finally {
-            // Send the Response to client.
-            if (! $psr7Response || ! $psr7Response instanceof Psr7Response) {
-                return;
-            }
-            $psr7Response->send();
-        }
-    }
-
-    protected function initRequestAndResponse(SwooleRequest $request, SwooleResponse $response): array
-    {
-        // Initialize PSR-7 Request and Response objects.
-        Context::set(ServerRequestInterface::class, $psr7Request = Psr7Request::loadFromSwooleRequest($request));
-        Context::set(ResponseInterface::class, $psr7Response = new Psr7Response($response));
-        return [$psr7Request, $psr7Response];
     }
 }
