08fc3aba8a8c9860a702ee1b150700646501b01d
xuchunlong <574331590@qq.com>
2019-06-24 15:23:28 +0800

    no message

--- src/db-connection/publish/databases.php
+++ src/db-connection/publish/databases.php
@@ -14,6 +14,7 @@ return [
     'default' => [
         'driver' => env('DB_DRIVER', 'mysql'),
         'host' => env('DB_HOST', 'localhost'),
+        'port' => env('DB_PORT', '3306'),
         'database' => env('DB_DATABASE', 'hyperf'),
         'username' => env('DB_USERNAME', 'root'),
         'password' => env('DB_PASSWORD', ''),
