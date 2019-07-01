71c5246b655f2cb30c489175fe705aacbc72556f
李铭昕 <715557344@qq.com>
2019-07-01 18:13:49 +0800

    Format code.

--- src/http-server/tests/Router/DispatcherFactoryTest.php
+++ src/http-server/tests/Router/DispatcherFactoryTest.php
@@ -1,11 +1,24 @@
 <?php
 
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
 
 namespace HyperfTest\HttpServer\Router;
 
 use HyperfTest\HttpServer\Stub\DispatcherFactory;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * @internal
+ * @coversNothing
+ */
 class DispatcherFactoryTest extends TestCase
 {
     public function testGetPrefix()
