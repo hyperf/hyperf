38b3d7ec6aa398fa916d22ce34e23c3df829022c
黄朝晖 <huangzhwork@gmail.com>
2019-07-05 13:50:16 +0800

    Update RouteCollector.php

--- src/http-server/src/Router/RouteCollector.php
+++ src/http-server/src/Router/RouteCollector.php
@@ -64,9 +64,6 @@ class RouteCollector
      * The syntax used in the $route string depends on the used route parser.
      *
      * @param string|string[] $httpMethod
-     * @param string $route
-     * @param mixed $handler
-     * @param array $options
      */
     public function addRoute($httpMethod, string $route, $handler, array $options = [])
     {
@@ -86,11 +83,8 @@ class RouteCollector
      * Create a route group with a common prefix.
      *
      * All routes created in the passed callback will have the given group prefix prepended.
-     *
-     * @param string $prefix
-     * @param callable $callback
      */
-    public function addGroup($prefix, callable $callback, array $options = [])
+    public function addGroup(string $prefix, callable $callback, array $options = [])
     {
         $previousGroupPrefix = $this->currentGroupPrefix;
         $currentGroupOptions = $this->currentGroupOptions;
@@ -107,11 +101,8 @@ class RouteCollector
      * Adds a GET route to the collection.
      *
      * This is simply an alias of $this->addRoute('GET', $route, $handler)
-     *
-     * @param string $route
-     * @param mixed $handler
      */
-    public function get($route, $handler, array $options = [])
+    public function get(string $route, $handler, array $options = [])
     {
         $this->addRoute('GET', $route, $handler, $options);
     }
@@ -120,11 +111,8 @@ class RouteCollector
      * Adds a POST route to the collection.
      *
      * This is simply an alias of $this->addRoute('POST', $route, $handler)
-     *
-     * @param string $route
-     * @param mixed $handler
      */
-    public function post($route, $handler, array $options = [])
+    public function post(string $route, $handler, array $options = [])
     {
         $this->addRoute('POST', $route, $handler, $options);
     }
@@ -133,11 +121,8 @@ class RouteCollector
      * Adds a PUT route to the collection.
      *
      * This is simply an alias of $this->addRoute('PUT', $route, $handler)
-     *
-     * @param string $route
-     * @param mixed $handler
      */
-    public function put($route, $handler, array $options = [])
+    public function put(string $route, $handler, array $options = [])
     {
         $this->addRoute('PUT', $route, $handler, $options);
     }
@@ -146,11 +131,8 @@ class RouteCollector
      * Adds a DELETE route to the collection.
      *
      * This is simply an alias of $this->addRoute('DELETE', $route, $handler)
-     *
-     * @param string $route
-     * @param mixed $handler
      */
-    public function delete($route, $handler, array $options = [])
+    public function delete(string $route, $handler, array $options = [])
     {
         $this->addRoute('DELETE', $route, $handler, $options);
     }
@@ -159,11 +141,8 @@ class RouteCollector
      * Adds a PATCH route to the collection.
      *
      * This is simply an alias of $this->addRoute('PATCH', $route, $handler)
-     *
-     * @param string $route
-     * @param mixed $handler
      */
-    public function patch($route, $handler, array $options = [])
+    public function patch(string $route, $handler, array $options = [])
     {
         $this->addRoute('PATCH', $route, $handler, $options);
     }
@@ -172,29 +151,21 @@ class RouteCollector
      * Adds a HEAD route to the collection.
      *
      * This is simply an alias of $this->addRoute('HEAD', $route, $handler)
-     *
-     * @param string $route
-     * @param mixed $handler
      */
-    public function head($route, $handler, array $options = [])
+    public function head(string $route, $handler, array $options = [])
     {
         $this->addRoute('HEAD', $route, $handler, $options);
     }
 
     /**
      * Returns the collected route data, as provided by the data generator.
-     *
-     * @return array
      */
-    public function getData()
+    public function getData(): array
     {
         return $this->dataGenerator->getData();
     }
 
-    /**
-     * @return array
-     */
-    protected function mergeOptions(array $origin, array $options)
+    protected function mergeOptions(array $origin, array $options): array
     {
         return array_merge_recursive($origin, $options);
     }
