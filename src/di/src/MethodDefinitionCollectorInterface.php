954b5b0922f95c2716e79f6c4658ca5080bc5be4
黄朝晖 <huangzhwork@gmail.com>
2019-08-02 16:32:55 +0800

    Update MethodDefinitionCollectorInterface.php

--- src/di/src/MethodDefinitionCollectorInterface.php
+++ src/di/src/MethodDefinitionCollectorInterface.php
@@ -16,16 +16,12 @@ interface MethodDefinitionCollectorInterface
 {
     /**
      * Retrieve the metadata for the parameters of the method.
-     * @param string $class
-     * @param string $method
      * @return ReflectionType[]
      */
     public function getParameters(string $class, string $method): array;
 
     /**
      * Retrieve the metadata for the return value of the method.
-     * @param string $class
-     * @param string $method
      * @return ReflectionType
      */
     public function getReturnType(string $class, string $method): ReflectionType;
