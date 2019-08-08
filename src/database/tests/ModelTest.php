835f47bc3037f54a745e6f688bdf85392ca17340
李铭昕 <715557344@qq.com>
2019-08-08 13:16:13 +0800

    Fixed bug.

--- src/database/tests/ModelTest.php
+++ src/database/tests/ModelTest.php
@@ -1885,7 +1885,7 @@ class ModelTest extends TestCase
             'host' => 'localhost',
             'database' => 'hyperf',
             'username' => 'root',
-            'password' => '910123',
+            'password' => '',
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
