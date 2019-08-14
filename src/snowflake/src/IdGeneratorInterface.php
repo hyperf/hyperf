871644ac962f3733cf3350cf57a09aee078ad721
李铭昕 <715557344@qq.com>
2019-08-14 13:37:00 +0800

    Removed IdGeneratorInterface::generate return type.

--- src/snowflake/src/IdGeneratorInterface.php
+++ src/snowflake/src/IdGeneratorInterface.php
@@ -12,7 +12,7 @@ declare(strict_types=1);
 
 namespace Hyperf\Snowflake;
 
-interface IdGeneratorInterface
+interface IdGeneratorInterface extends \Hyperf\Contract\IdGeneratorInterface
 {
     const LEVEL_SECOND = 1;
 
