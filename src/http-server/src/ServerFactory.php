647a9bb99030d32273ba9be4c71ecf6e32164a7f
李铭昕 <715557344@qq.com>
2019-08-15 10:36:12 +0800

    Deprecated some class.

---
+++ src/http-server/src/ServerFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+namespace Hyperf\HttpServer;
+
+use Psr\Container\ContainerInterface;
+
+/**
+ * @deprecated v1.2
+ */
+class ServerFactory
+{
+    public function __invoke(ContainerInterface $container)
+    {
+        return new Server($container);
+    }
+}
