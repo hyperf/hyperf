5e6316b92b58147020c88830735a0bab6207c978
李铭昕 <715557344@qq.com>
2019-08-16 14:49:25 +0800

    Support multi handlers for logger.

--- src/logger/publish/logger.php
+++ src/logger/publish/logger.php
@@ -12,15 +12,30 @@ declare(strict_types=1);
 
 return [
     'default' => [
-        'handler' => [
-            'class' => \Monolog\Handler\StreamHandler::class,
-            'constructor' => [
-                'stream' => BASE_PATH . '/runtime/logs/hyperf.log',
-                'level' => \Monolog\Logger::DEBUG,
+        'handlers' => [
+            [
+                'class' => Monolog\Handler\StreamHandler::class,
+                'constructor' => [
+                    'stream' => BASE_PATH . '/runtime/logs/hyperf.log',
+                    'level' => Monolog\Logger::DEBUG,
+                ],
+                'formatter' => [
+                    'class' => Monolog\Formatter\LineFormatter::class,
+                    'constructor' => [],
+                ],
             ],
         ],
+        // Default Handler
+        'handler' => [
+            'class' => Monolog\Handler\StreamHandler::class,
+            'constructor' => [
+                'stream' => BASE_PATH . '/runtime/logs/hyperf.log',
+                'level' => Monolog\Logger::DEBUG,
+            ],
+        ],
+        // Default Formatter
         'formatter' => [
-            'class' => \Monolog\Formatter\LineFormatter::class,
+            'class' => Monolog\Formatter\LineFormatter::class,
             'constructor' => [],
         ],
     ],
