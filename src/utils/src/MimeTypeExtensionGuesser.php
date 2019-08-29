6ba729f5701a5a9d9d7ef3dbc8795ae171db6536
huangzhhui <huangzhwork@gmail.com>
2019-08-29 18:45:41 +0800

    Moved MimeTypeExtensionGuesser to Utils

--- src/utils/src/MimeTypeExtensionGuesser.php
+++ src/utils/src/MimeTypeExtensionGuesser.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
  * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
  */
 
-namespace Hyperf\HttpServer\MimeType;
+namespace Hyperf\Utils;
 
 class MimeTypeExtensionGuesser
 {
