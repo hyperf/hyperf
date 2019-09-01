f88cd803a5819076ec775e972c9e97370810a160
sy-records <52o@qq52o.cn>
2019-09-01 11:10:37 +0800

    add swoole-tracker new file:   swoole-tracker/LICENSE.md new file:   swoole-tracker/README.md new file:   swoole-tracker/composer.json new file:   swoole-tracker/src/ConfigProvider.php new file:   swoole-tracker/src/Middleware/HttpServerMiddleware.php

---
+++ src/swoole-tracker/src/ConfigProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+namespace Hyperf\SwooleTracker;
+
+class ConfigProvider
+{
+    public function __invoke(): array
+    {
+        return [
+            'dependencies' => [
+            ],
+            'commands' => [
+            ],
+            'scan' => [
+                'paths' => [
+                    __DIR__,
+                ],
+            ],
+        ];
+    }
+}
