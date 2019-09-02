8402cc466334fbb055bb28032a37e7502bbda773
NiZerin98 <nzl199851@gmail.com>
2019-09-02 13:35:41 +0800

    Update DispatcherFactory.php

--- src/http-server/src/Router/DispatcherFactory.php
+++ src/http-server/src/Router/DispatcherFactory.php
@@ -249,7 +249,7 @@ class DispatcherFactory
         return [$middleware->middleware];
     }
 
-    protected function getAllRoutes()
+    protected function getAllRoutes() :array
     {
         $routeDir = BASE_PATH . '/routes/';
         $routeFiles = scandir($routeDir);
