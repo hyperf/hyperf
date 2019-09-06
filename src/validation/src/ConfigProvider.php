1aeb9dad6a09fd065f21752007ade04fbe578ddb
wangyi <chunhei2008@qq.com>
2019-09-06 16:38:13 +0800

    fix: 🐛 ConfigProvider
    * Remove Validatorinterface|* Modify languages director in publish

--- src/validation/src/ConfigProvider.php
+++ src/validation/src/ConfigProvider.php
@@ -13,7 +13,6 @@ declare(strict_types=1);
 namespace Hyperf\Validation;
 
 use Hyperf\Validation\Contracts\Validation\Factory as FactoryInterface;
-use Hyperf\Contract\ValidatorInterface;
 
 class ConfigProvider
 {
@@ -21,7 +20,6 @@ class ConfigProvider
     {
         return [
             'dependencies' => [
-                ValidatorInterface::class => ValidatorFactory::class,
                 PresenceVerifierInterface::class => DatabasePresenceVerifierFactory::class,
                 FactoryInterface::class => Factory::class,
             ],
@@ -35,13 +33,13 @@ class ConfigProvider
                     'id' => 'zh_CN',
                     'description' => 'The message bag for validation.',
                     'source' => __DIR__ . '/../publish/zh_CN/validation.php',
-                    'destination' => BASE_PATH . '/resources/lang/zh_CN/validation.php',
+                    'destination' => BASE_PATH . '/storage/languages/zh_CN/validation.php',
                 ],
                 [
                     'id' => 'en',
                     'description' => 'The message bag for validation.',
                     'source' => __DIR__ . '/../publish/en/validation.php',
-                    'destination' => BASE_PATH . '/resources/lang/en/validation.php',
+                    'destination' => BASE_PATH . '/storage/languages/en/validation.php',
                 ],
             ],
         ];
