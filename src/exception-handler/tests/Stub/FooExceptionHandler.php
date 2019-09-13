e2b8d2ef565279e26af308138f50ec3a28e55abc
李铭昕 <715557344@qq.com>
2019-09-13 14:55:38 +0800

    Added testing.

---
+++ src/exception-handler/tests/Stub/FooExceptionHandler.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+namespace HyperfTest\ExceptionHandler\Stub;
+
+use Hyperf\ExceptionHandler\ExceptionHandler;
+use Hyperf\Utils\Context;
+use Psr\Http\Message\ResponseInterface;
+use Throwable;
+
+class FooExceptionHandler extends ExceptionHandler
+{
+    public function handle(Throwable $throwable, ResponseInterface $response)
+    {
+        Context::set('test.exception-handler.latest-handler', static::class);
+        return $response;
+    }
+
+    public function isValid(Throwable $throwable): bool
+    {
+        return true;
+    }
+}
