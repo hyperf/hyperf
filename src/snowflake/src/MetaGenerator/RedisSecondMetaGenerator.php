2806160b4d595de0c91182a7fc71288941299664
李铭昕 <715557344@qq.com>
2019-09-16 16:25:44 +0800

    Optimized code.

--- src/snowflake/src/MetaGenerator/RedisSecondMetaGenerator.php
+++ src/snowflake/src/MetaGenerator/RedisSecondMetaGenerator.php
@@ -13,60 +13,13 @@ declare(strict_types=1);
 namespace Hyperf\Snowflake\MetaGenerator;
 
 use Hyperf\Contract\ConfigInterface;
-use Hyperf\Redis\RedisProxy;
 use Hyperf\Snowflake\ConfigurationInterface;
-use Hyperf\Snowflake\MetaGenerator;
 
-class RedisSecondMetaGenerator extends MetaGenerator
+class RedisSecondMetaGenerator extends RedisMetaGenerator
 {
-    const DEFAULT_REDIS_KEY = 'hyperf:snowflake:workerId';
-
-    /**
-     * @var ConfigInterface
-     */
-    protected $config;
-
-    protected $workerId;
-
-    protected $dataCenterId;
-
     public function __construct(ConfigurationInterface $configuration, int $beginTimestamp = self::DEFAULT_BEGIN_SECOND, ConfigInterface $config)
     {
-        parent::__construct($configuration, $beginTimestamp);
-
-        $this->config = $config;
-    }
-
-    public function init()
-    {
-        if (is_null($this->workerId) || is_null($this->dataCenterId)) {
-            $pool = $this->config->get(sprintf('snowflake.%s.pool', static::class), 'default');
-
-            /** @var \Redis $redis */
-            $redis = make(RedisProxy::class, [
-                'pool' => $pool,
-            ]);
-
-            $key = $this->config->get(sprintf('snowflake.%s.key', static::class), static::DEFAULT_REDIS_KEY);
-            $id = $redis->incr($key);
-
-            $this->workerId = $id % $this->configuration->maxWorkerId();
-            $this->dataCenterId = intval($id / $this->configuration->maxWorkerId()) % $this->configuration->maxDataCenterId();
-        }
-    }
-
-    public function getDataCenterId(): int
-    {
-        $this->init();
-
-        return $this->dataCenterId;
-    }
-
-    public function getWorkerId(): int
-    {
-        $this->init();
-
-        return $this->workerId;
+        parent::__construct($configuration, $beginTimestamp, $config);
     }
 
     public function getTimestamp(): int
