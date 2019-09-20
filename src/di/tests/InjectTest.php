7dc5d3bec2053524e2ad5c2da81a89cf825b1485
xiabin <xiabin@pp.cc>
2019-09-20 09:39:07 +0800

    fixed inject test error

--- src/di/tests/InjectTest.php
+++ src/di/tests/InjectTest.php
@@ -16,11 +16,12 @@ namespace HyperfTest\Di;
 use Hyperf\Di\Annotation\Scanner;
 use Hyperf\Di\Container;
 use Hyperf\Di\Definition\DefinitionSource;
+use Hyperf\Di\Definition\ScanConfig;
+use HyperfTest\Di\ExceptionStub\DemoInjectException;
 use HyperfTest\Di\Stub\AnnotationCollector;
 use HyperfTest\Di\Stub\AspectCollector;
 use HyperfTest\Di\Stub\Demo;
 use HyperfTest\Di\Stub\DemoInject;
-use HyperfTest\Di\Stub\DemoInjectException;
 use PhpDocReader\AnnotationException;
 use PHPUnit\Framework\TestCase;
 
@@ -28,7 +29,7 @@ class InjectTest extends TestCase
 {
     public function testInject()
     {
-        $container = new Container(new DefinitionSource([], ['./Stub'], new Scanner()));
+        $container = new Container(new DefinitionSource([], new ScanConfig(['./Stub'])));
         $demoInject = $container->get(DemoInject::class);
         $this->assertSame(Demo::class, get_class($demoInject->getDemo()));
         $this->assertSame(null, $demoInject->getDemo1());
@@ -38,7 +39,7 @@ class InjectTest extends TestCase
     {
 
         try {
-            $container = new Container(new DefinitionSource([], ['./Stub','./ExceptionStub'], new Scanner()));
+            $container = new Container(new DefinitionSource([], new ScanConfig(['./Stub','./ExceptionStub'])));
             $container->get(DemoInjectException::class);
         } catch (\Exception $e) {
             $this->assertSame(true, $e instanceof AnnotationException);
