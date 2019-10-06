d6fed2717c1e33a7978a5f28e61422e8e3a36677
Dacheng Gao <dickensgao@protonmail.ch>
2019-10-06 21:37:10 +0800

    Rename license

--- src/di/src/Functions.php
+++ src/di/src/Functions.php
@@ -7,5 +7,5 @@ declare(strict_types=1);
  * @link     https://www.hyperf.io
  * @document https://doc.hyperf.io
  * @contact  group@hyperf.io
- * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE.md
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
  */
--- src/websocket-server/src/Listener/OnPipeMessageListener.php
+++ src/websocket-server/src/Listener/OnPipeMessageListener.php
@@ -7,7 +7,7 @@ declare(strict_types=1);
  * @link     https://www.hyperf.io
  * @document https://doc.hyperf.io
  * @contact  group@hyperf.io
- * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE.md
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
  */
 
 namespace Hyperf\WebSocketServer\Listener;
