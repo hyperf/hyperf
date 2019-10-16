a4bbacee641c20d012cc436dc1ce24b106903593
李铭昕 <715557344@qq.com>
2019-10-16 14:29:29 +0800

    Renamed db:model to gen:model.

--- src/db-connection/publish/databases.php
+++ src/db-connection/publish/databases.php
@@ -36,7 +36,7 @@ return [
             'load_script' => true,
         ],
         'commands' => [
-            'db:model' => [
+            'gen:model' => [
                 'path' => 'app/Model',
                 'force_casts' => true,
                 'inheritance' => 'Model',
