83ba60a22bcd2ff951c9e43596731f2e3ff649aa
李铭昕 <715557344@qq.com>
2019-10-26 15:32:43 +0800

    Optimized code.

--- src/clickhouse/src/ClickhouseFactory.php
+++ src/clickhouse/src/ClickhouseFactory.php
@@ -7,12 +7,13 @@ declare(strict_types=1);
  * @link     https://www.hyperf.io
  * @document https://doc.hyperf.io
  * @contact  group@hyperf.io
- * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
 
 namespace Hyperf\Clickhouse;
 
 use ClickHouseDB\Client;
+use Hyperf\Clickhouse\Exception\NotHookException;
 use Hyperf\Contract\ConfigInterface;
 
 class ClickhouseFactory
@@ -31,6 +32,10 @@ class ClickhouseFactory
     {
         $config = $this->config->get('clickhouse.' . $pool, []);
         $settings = $config['settings'] ?? [];
+
+        // if ((swoole_hook_flags() & SWOOLE_HOOK_CURL) !== SWOOLE_HOOK_CURL) {
+        //     throw new NotHookException('The swoole hook flags not support CURL.');
+        // }
 
         return new Client($config, $settings);
     }
--- src/clickhouse/tests/ClickhouseFactoryTest.php
+++ src/clickhouse/tests/ClickhouseFactoryTest.php
@@ -7,13 +7,14 @@ declare(strict_types=1);
  * @link     https://www.hyperf.io
  * @document https://doc.hyperf.io
  * @contact  group@hyperf.io
- * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
 
 namespace HyperfTest\Clickhouse;
 
 use ClickHouseDB\Client;
 use Hyperf\Clickhouse\ClickhouseFactory;
+use Hyperf\Clickhouse\Exception\NotHookException;
 use Hyperf\Config\Config;
 use PHPUnit\Framework\TestCase;
 
@@ -23,7 +24,7 @@ use PHPUnit\Framework\TestCase;
  */
 class ClickhouseFactoryTest extends TestCase
 {
-    public function testCreate()
+    public function testNotHookCurl()
     {
         $config = new Config([
             'clickhouse' => [
@@ -41,8 +42,22 @@ class ClickhouseFactoryTest extends TestCase
 
         $factory = new ClickhouseFactory($config);
 
+        // $this->expectException(NotHookException::class);
+
         $db = $factory->create();
 
         $this->assertInstanceOf(Client::class, $db);
+    }
+
+    public function testEnable()
+    {
+        $flag = SWOOLE_HOOK_ALL;
+        $this->assertTrue(($flag & SWOOLE_HOOK_CURL) === 0);
+
+        $flag = SWOOLE_HOOK_ALL | SWOOLE_HOOK_CURL;
+        $this->assertTrue(($flag & SWOOLE_HOOK_CURL) !== 0);
+
+        $flag = SWOOLE_HOOK_ALL | SWOOLE_HOOK_BLOCKING_FUNCTION;
+        $this->assertTrue(($flag & SWOOLE_HOOK_CURL) === 0);
     }
 }
