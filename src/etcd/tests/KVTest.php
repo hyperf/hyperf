cbf498c3cae518c567cd571815e87e98b6874b2b
李铭昕 <715557344@qq.com>
2019-10-27 19:53:51 +0800

    Added testing.

--- src/etcd/tests/KVTest.php
+++ src/etcd/tests/KVTest.php
@@ -12,18 +12,25 @@ declare(strict_types=1);
 
 namespace HyperfTest\Etcd;
 
+use GuzzleHttp\Client;
 use Hyperf\Config\Config;
 use Hyperf\Contract\ConfigInterface;
 use Hyperf\Di\Container;
 use Hyperf\Etcd\KVFactory;
 use Hyperf\Etcd\KVInterface;
+use Hyperf\Etcd\V3\EtcdClient;
 use Hyperf\Etcd\V3\KV;
 use Hyperf\Guzzle\HandlerStackFactory;
 use Hyperf\Guzzle\PoolHandler;
+use Hyperf\Pool\Channel;
+use Hyperf\Pool\PoolOption;
+use Hyperf\Pool\SimplePool\Connection;
+use Hyperf\Pool\SimplePool\Pool;
+use Hyperf\Pool\SimplePool\PoolFactory;
 use Hyperf\Utils\ApplicationContext;
+use HyperfTest\Etcd\Stub\GuzzleClientStub;
 use Mockery;
 use PHPUnit\Framework\TestCase;
-use Psr\Container\ContainerInterface;
 
 /**
  * @internal
@@ -38,6 +45,29 @@ class KVTest extends TestCase
 
     public function testGetKyFromFactory()
     {
+        $kv = $this->getKVClient();
+
+        $this->assertInstanceOf(KVInterface::class, $kv);
+    }
+
+    public function testPutAndGet()
+    {
+        $kv = $this->getKVClient();
+
+        $res = $kv->put('/test/test2', 'Hello World!');
+
+        $this->assertArrayHasKey('header', $res);
+
+        $res = $kv->get('/test/test2');
+
+        $this->assertSame('Hello World!', $res['kvs'][0]['value']);
+    }
+
+    /**
+     * @return KVInterface
+     */
+    protected function getKVClient()
+    {
         $config = new Config([
             'etcd' => [
                 'uri' => 'http://127.0.0.1:2379',
@@ -51,8 +81,26 @@ class KVTest extends TestCase
         $container = Mockery::mock(Container::class);
         ApplicationContext::setContainer($container);
 
-        $container->shouldReceive('make')->with(PoolHandler::class, Mockery::any())->andReturnUsing(function ($class, $args) {
-            var_dump($class, $args);
+        $container->shouldReceive('make')->with(EtcdClient::class, Mockery::any())->andReturnUsing(function ($class, $args) {
+            return new EtcdClient($args['client']);
+        });
+        $container->shouldReceive('make')->with(Client::class, Mockery::any())->andReturnUsing(function ($class, $args) {
+            return new GuzzleClientStub($args['config']);
+        });
+        $container->shouldReceive('make')->with(PoolHandler::class, Mockery::any())->andReturnUsing(function ($class, $args) use ($container) {
+            return new PoolHandler(new PoolFactory($container), $args['option']);
+        });
+        $container->shouldReceive('make')->with(Channel::class, Mockery::any())->andReturnUsing(function ($class, $args) {
+            return new Channel($args['size']);
+        });
+        $container->shouldReceive('make')->with(Connection::class, Mockery::any())->andReturnUsing(function ($class, $args) use ($container) {
+            return new Connection($container, $args['pool'], $args['callback']);
+        });
+        $container->shouldReceive('make')->with(PoolOption::class, Mockery::any())->andReturnUsing(function ($class, $args) {
+            return new PoolOption(...array_values($args));
+        });
+        $container->shouldReceive('make')->with(Pool::class, Mockery::any())->andReturnUsing(function ($class, $args) use ($container) {
+            return new Pool($container, $args['callback'], $args['option']);
         });
         $container->shouldReceive('make')->with(KV::class, Mockery::any())->andReturnUsing(function ($class, $args) {
             return new KV(...array_values($args));
@@ -61,8 +109,6 @@ class KVTest extends TestCase
         $container->shouldReceive('get')->with(HandlerStackFactory::class)->andReturn(new HandlerStackFactory());
 
         $factory = new KVFactory();
-        $kv = $factory($container);
-
-        $this->assertInstanceOf(KVInterface::class, $kv);
+        return $factory($container);
     }
 }
