525e6ec4dbd1e1aaa892bf3f9b4f3fa031f1f9d6
谷溪 <guxi99@gmail.com>
2019-10-27 20:36:26 +0800

    Use channel to communicate, instead of sharing mem
    A saying from CSP:
    
    Do not communicate by sharing memory; instead, share memory by communicating.
    
    “不要以共享内存的方式来通信，相反，要通过通信来共享内存。”

--- src/process/src/AbstractProcess.php
+++ src/process/src/AbstractProcess.php
@@ -20,7 +20,7 @@ use Hyperf\Process\Event\BeforeProcessHandle;
 use Hyperf\Process\Event\PipeMessage;
 use Psr\Container\ContainerInterface;
 use Psr\EventDispatcher\EventDispatcherInterface;
-use Swoole\Coroutine as SwooleCoroutine;
+use Swoole\Coroutine\Channel;
 use Swoole\Event;
 use Swoole\Process as SwooleProcess;
 use Swoole\Server;
@@ -82,11 +82,6 @@ abstract class AbstractProcess implements ProcessInterface
      */
     protected $restartInterval = 5;
 
-    /**
-     * @var bool
-     */
-    protected $listening = true;
-
     public function __construct(ContainerInterface $container)
     {
         $this->container = $container;
@@ -110,13 +105,16 @@ abstract class AbstractProcess implements ProcessInterface
 
                     $this->process = $process;
                     if ($this->enableCoroutine) {
-                        $this->listen();
+                        $quit = new Channel(1);
+                        $this->listen($quit);
                     }
                     $this->handle();
 
                     $this->event && $this->event->dispatch(new AfterProcessHandle($this, $i));
                 } finally {
-                    $this->listening = false;
+                    if (isset($quit)) {
+                        $quit->push(true);
+                    }
                     sleep($this->restartInterval);
                 }
             }, $this->redirectStdinStdout, $this->pipeType, $this->enableCoroutine);
@@ -131,10 +129,10 @@ abstract class AbstractProcess implements ProcessInterface
     /**
      * Added event for listening data from worker/task.
      */
-    protected function listen()
+    protected function listen(Channel $quit)
     {
-        go(function () {
-            while ($this->listening) {
+        go(function () use ($quit) {
+            while ($quit->pop(0.001) !== true) {
                 try {
                     /** @var \Swoole\Coroutine\Socket $sock */
                     $sock = $this->process->exportSocket();
@@ -148,10 +146,9 @@ abstract class AbstractProcess implements ProcessInterface
                         $formatter = $this->container->get(FormatterInterface::class);
                         $logger->error($formatter->format($exception));
                     }
-                } finally {
-                    SwooleCoroutine::sleep(0.001);
                 }
             }
+            $quit->close();
         });
     }
 }
