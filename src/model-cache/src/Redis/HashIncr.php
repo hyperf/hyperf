9164a2c5d3a343186c4bd4daf6eb7718a4967da9
李铭昕 <715557344@qq.com>
2019-11-15 15:38:44 +0800

    Update HashIncr.php

--- src/model-cache/src/Redis/HashIncr.php
+++ src/model-cache/src/Redis/HashIncr.php
@@ -17,7 +17,7 @@ class HashIncr implements OperatorInterface
     public function getScript(): string
     {
         return <<<'LUA'
-    if(redis.call('type',KEYS[1]).ok == 'hash') then
+    if(redis.call('type', KEYS[1]).ok == 'hash') then
         return redis.call('HINCRBYFLOAT', KEYS[1], ARGV[1], ARGV[2]);
     end
     return false;
