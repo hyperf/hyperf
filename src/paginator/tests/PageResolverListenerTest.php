3bd4afb28018323aef4a34e70142211b5a090e07
李铭昕 <715557344@qq.com>
2019-11-16 19:48:48 +0800

    Fixed type error, when use paginator in non-worker process.

---
+++ src/paginator/tests/PageResolverListenerTest.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+
+namespace HyperfTest\Paginator;
+
+use Hyperf\Di\Container;
+use Hyperf\Framework\Event\BootApplication;
+use Hyperf\HttpServer\Contract\RequestInterface;
+use Hyperf\HttpServer\Request;
+use Hyperf\Paginator\LengthAwarePaginator;
+use Hyperf\Paginator\Listener\PageResolverListener;
+use Hyperf\Utils\ApplicationContext;
+use Hyperf\Utils\Context;
+use Mockery;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ServerRequestInterface as PsrServerRequestInterface;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+class PageResolverListenerTest extends TestCase
+{
+    protected function tearDown()
+    {
+        Mockery::close();
+        Context::set(PsrServerRequestInterface::class, null);
+        Context::set('http.request.parsedData', null);
+    }
+
+    public function testPageResolve()
+    {
+        $this->getContainer();
+        $paginator = new LengthAwarePaginator([1, 2], 10, 2, null);
+        $this->assertSame('/?page=2', $paginator->nextPageUrl());
+
+        $listener = new PageResolverListener();
+        $listener->process(new BootApplication());
+
+        $paginator = new LengthAwarePaginator([1, 2], 10, 2, null);
+        $this->assertSame('/?page=2', $paginator->nextPageUrl());
+
+        Context::set(PsrServerRequestInterface::class, value(function () {
+            $request = new \Hyperf\HttpMessage\Server\Request('GET', '/index');
+            return $request->withQueryParams(['page' => 2]);
+        }));
+        $paginator = new LengthAwarePaginator([1, 2], 10, 2, null);
+        $this->assertSame('/?page=3', $paginator->nextPageUrl());
+    }
+
+    protected function getContainer()
+    {
+        $container = Mockery::mock(Container::class);
+        $container->shouldReceive('get')->with(RequestInterface::class)->andReturn(new Request());
+
+        ApplicationContext::setContainer($container);
+        return $container;
+    }
+}
