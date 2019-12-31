7197589c6e5297e6e3ba73fdd96420fc24144af0
chunhei2008 <chunhei2008@qq.com>
2019-12-31 15:11:57 +0800

    style: 💄 cs fix validation

--- src/validation/src/Concerns/ValidatesAttributes.php
+++ src/validation/src/Concerns/ValidatesAttributes.php
@@ -1493,7 +1493,7 @@ trait ValidatesAttributes
             return $value->getSize() / 1024;
         }
 
-        return mb_strlen((string)$value);
+        return mb_strlen((string) $value);
     }
 
     /**
