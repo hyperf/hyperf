cbbe1c7ddd8121f5d5a80c48c550111cee815a64
李铭昕 <715557344@qq.com>
2020-01-22 10:56:37 +0800

    Added default pool config.

--- src/nsq/publish/nsq.php
+++ src/nsq/publish/nsq.php
@@ -14,5 +14,13 @@ return [
     'default' => [
         'host' => '127.0.0.1',
         'port' => 4150,
+        'pool' => [
+            'min_connections' => 1,
+            'max_connections' => 10,
+            'connect_timeout' => 10.0,
+            'wait_timeout' => 3.0,
+            'heartbeat' => -1,
+            'max_idle_time' => 60.0,
+        ],
     ],
 ];
