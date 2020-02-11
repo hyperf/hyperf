d1c8e8e858a71fd3d314e734ac2d171032142956
ixingjue <ixingjue@qq.com>
2020-02-11 23:50:24 +0800

    add route:list command

---
+++ src/devtool/src/RouteListCommand.php
@@ -0,0 +1,112 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+namespace Hyperf\Devtool;
+
+use Hyperf\Contract\ConfigInterface;
+use Hyperf\HttpServer\MiddlewareManager;
+use Psr\Container\ContainerInterface;
+use Hyperf\Command\Annotation\Command;
+use Symfony\Component\Console\Helper\Table;
+use Hyperf\HttpServer\Router\DispatcherFactory;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Helper\TableSeparator;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Command\Command as SymfonyCommand;
+
+/**
+ * @Command
+ */
+class RouteListCommand extends SymfonyCommand
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * @var ConfigInterface
+     */
+    private $config;
+
+    protected $routes = [BASE_PATH . '/config/routes.php'];
+
+    public function __construct(ContainerInterface $container, ConfigInterface $config)
+    {
+        parent::__construct('route:list');
+        $this->container = $container;
+        $this->config = $config;
+    }
+
+    protected function configure()
+    {
+        $this->setDescription('Get routes [{path}] to get the route info.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+        $data = [];
+
+        $factory = $this->container->get(DispatcherFactory::class);
+        $router = $factory->getRouter('http');
+        [$routers] = $router->getData();
+
+        foreach ($routers as $method => $items) {
+            foreach ($items as $item) {
+                $uri = $item->route;
+                if (is_array($item->callback)) {
+                    $action = $item->callback[0]. '@'. $item->callback[1];
+                } else {
+                    $action = $item->callback;
+                }
+                if(isset($data[$uri])) {
+                    $data[$uri]['method'][] = $method;
+                } else {
+                    // method,uri,name,action,middleware
+                    $serverName = 'http';
+                    $registedMiddlewares = MiddlewareManager::get('http', $uri, $method);
+                    $middlewares = $this->config->get('middlewares.' . $serverName, []);
+
+                    $middlewares = array_merge($middlewares, $registedMiddlewares);
+                    $data[$uri] = [
+                        'server' => $serverName ,
+                        'method' => [$method],
+                        'uri' => $uri,
+                        'action' => $action,
+                        'middleware' => implode(PHP_EOL, array_unique($middlewares))
+                    ];
+                }
+            }
+        }
+        $this->show($data, $output);
+        $io->success('success.');
+    }
+
+    private function show($data, $output)
+    {
+        $rows = [];
+        foreach ($data as $route) {
+            $route['method'] = implode('|', $route['method']);
+            $rows[] = $route;
+            $rows[] = new TableSeparator();
+        }
+        $rows = array_slice($rows, 0, count($rows) -1);
+        $table = new Table($output);
+        $table
+            ->setHeaders(array('Server', 'Method', 'URI', 'Action', 'Middleware'))
+            ->setRows($rows)
+        ;
+        $table->render();
+    }
+}
