3cc14493023f36135df7336d9852707beb9921c9
李铭昕 <715557344@qq.com>
2020-02-24 13:58:45 +0800

    Added processors for logger.

---
+++ src/logger/tests/Stub/FooHandler.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+
+namespace HyperfTest\Logger\Stub;
+
+use Hyperf\Utils\Context;
+use Monolog\Handler\StreamHandler;
+
+class FooHandler extends StreamHandler
+{
+    public function write(array $record)
+    {
+        Context::set('test.logger.foo_handler.record', $record);
+    }
+}
