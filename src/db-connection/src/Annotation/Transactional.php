bb84006cf8588c0d33df41a12b8c4dddc7c7b1f6
lidanyang <lidanyang@bilibili.com>
2020-03-09 21:00:23 +0800

    php cs fix

--- src/db-connection/src/Annotation/Transactional.php
+++ src/db-connection/src/Annotation/Transactional.php
@@ -1,10 +1,16 @@
 <?php
 
 declare(strict_types=1);
-
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
 
 namespace Hyperf\DbConnection\Annotation;
-
 
 use Hyperf\Di\Annotation\AbstractAnnotation;
 
@@ -14,5 +20,4 @@ use Hyperf\Di\Annotation\AbstractAnnotation;
  */
 class Transactional extends AbstractAnnotation
 {
-
 }
