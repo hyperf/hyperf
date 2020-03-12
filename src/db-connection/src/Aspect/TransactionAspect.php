bdfdbc2dfcbe21b6d6e66b0fe83c3290bbba45b8
李铭昕 <715557344@qq.com>
2020-03-12 10:18:25 +0800

    Update TransactionAspect.php

--- src/db-connection/src/Aspect/TransactionAspect.php
+++ src/db-connection/src/Aspect/TransactionAspect.php
@@ -12,14 +12,13 @@ declare(strict_types=1);
 
 namespace Hyperf\DbConnection\Aspect;
 
+use Hyperf\DbConnection\Annotation\Transactional;
 use Hyperf\DbConnection\Db;
 use Hyperf\Di\Annotation\Aspect;
 use Hyperf\Di\Aop\AbstractAspect;
 use Hyperf\Di\Aop\ProceedingJoinPoint;
-use Hyperf\DbConnection\Annotation\Transactional;
 
 /**
- * 自动事务注解
  * @Aspect
  */
 class TransactionAspect extends AbstractAspect
@@ -28,10 +27,6 @@ class TransactionAspect extends AbstractAspect
         Transactional::class,
     ];
 
-    /**
-     * @inheritDoc
-     * @throws \Throwable
-     */
     public function process(ProceedingJoinPoint $proceedingJoinPoint)
     {
         return Db::transaction(
