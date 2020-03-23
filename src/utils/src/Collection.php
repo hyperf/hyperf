f6555bd86423b12699e8c7293c44fc549d432da2
Donjan <Donjan@cqbaobao.cn>
2020-03-23 15:02:45 +0800

    强制int参数,默认INF并不会有用，且会报错

--- src/utils/src/Collection.php
+++ src/utils/src/Collection.php
@@ -558,7 +558,7 @@ class Collection implements ArrayAccess, Arrayable, Countable, IteratorAggregate
     /**
      * Get a flattened array of the items in the collection.
      */
-    public function flatten(int $depth = INF): self
+    public function flatten($depth = INF): self
     {
         return new static(Arr::flatten($this->items, $depth));
     }
