590951b7e833f3a34da5968ec12bffe882ddc103
李铭昕 <715557344@qq.com>
2020-05-04 15:03:19 +0800

    Update server.php

--- src/server/publish/server.php
+++ src/server/publish/server.php
@@ -31,16 +31,17 @@ return [
     'settings' => [
         'enable_coroutine' => true,
         'worker_num' => 4,
-        'pid_file' => 'runtime/hyperf.pid',
+        'pid_file' => BASE_PATH . '/runtime/hyperf.pid',
         'open_tcp_nodelay' => true,
         'max_coroutine' => 100000,
         'open_http2_protocol' => true,
-        'max_request' => 10000,
+        'max_request' => 0,
         'socket_buffer_size' => 2 * 1024 * 1024,
     ],
     'callbacks' => [
         SwooleEvent::ON_BEFORE_START => [Hyperf\Framework\Bootstrap\ServerStartCallback::class, 'beforeStart'],
         SwooleEvent::ON_WORKER_START => [Hyperf\Framework\Bootstrap\WorkerStartCallback::class, 'onWorkerStart'],
         SwooleEvent::ON_PIPE_MESSAGE => [Hyperf\Framework\Bootstrap\PipeMessageCallback::class, 'onPipeMessage'],
+        SwooleEvent::ON_WORKER_EXIT => [Hyperf\Framework\Bootstrap\WorkerExitCallback::class, 'onWorkerExit'],
     ],
 ];
