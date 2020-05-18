25ecbce31dbfd0ab2f0ef08b3ed11fea07a9cdc3
nfangxu <nfangxu@gmail.com>
2020-05-18 16:28:33 +0800

    add test

---
+++ src/database/tests/DatabaseSQLiteProcessorTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace HyperfTest\Database;
+
+use Hyperf\Database\Query\Processors\SQLiteProcessor;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+class DatabaseSQLiteProcessorTest extends TestCase
+{
+    public function testProcessColumnListing()
+    {
+        $processor = new SQLiteProcessor();
+
+        $listing = [['name' => 'id'], ['name' => 'name'], ['name' => 'email']];
+        $expected = ['id', 'name', 'email'];
+
+        $this->assertEquals($expected, $processor->processColumnListing($listing));
+
+        // convert listing to objects to simulate PDO::FETCH_CLASS
+        foreach ($listing as &$row) {
+            $row = (object) $row;
+        }
+
+        $this->assertEquals($expected, $processor->processColumnListing($listing));
+    }
+}
