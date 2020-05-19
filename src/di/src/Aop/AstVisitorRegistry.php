f28046850bab0c6da4247315212800cd33b5031f
huangzhhui <huangzhwork@gmail.com>
2020-05-19 19:56:49 +0800

    Use AstVisitorRegistry instead of AstVisitorCollector

---
+++ src/di/src/Aop/AstVisitorRegistry.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace Hyperf\Di\Aop;
+
+use Hyperf\Utils\Traits\Container;
+
+/**
+ * @mixin \SplPriorityQueue
+ */
+class AstVisitorRegistry
+{
+    /**
+     * @var \SplPriorityQueue
+     */
+    protected static $queue;
+
+    public static function getQueue(): \SplPriorityQueue
+    {
+        if (! static::$queue instanceof \SplPriorityQueue) {
+            static::$queue = new \SplPriorityQueue();
+        }
+        return static::$queue;
+    }
+
+    public static function __callStatic($name, $arguments)
+    {
+        $queue = static::getQueue();
+        if (method_exists($queue, $name)) {
+            return $queue->$name(...$arguments);
+        }
+        throw new \InvalidArgumentException('Invalid method for ' . __CLASS__);
+    }
+
+
+}
