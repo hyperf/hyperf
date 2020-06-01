a3cd909c645b2722f66e03ccf07978b676c7e5f2
李铭昕 <715557344@qq.com>
2020-06-01 12:54:49 +0800

    Fixed analyse for server.

--- src/rpc-server/src/RequestDispatcher.php
+++ src/rpc-server/src/RequestDispatcher.php
@@ -16,6 +16,7 @@ use Hyperf\Dispatcher\HttpRequestHandler;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
 
 class RequestDispatcher extends HttpDispatcher
 {
@@ -33,7 +34,7 @@ class RequestDispatcher extends HttpDispatcher
     {
         /** @var ServerRequestInterface $request */
         /** @var array $middlewares */
-        /** @var string $coreHandler */
+        /** @var MiddlewareInterface $coreHandler */
         [$request, $middlewares, $coreHandler] = $params;
         $requestHandler = new HttpRequestHandler($middlewares, $coreHandler, $this->container);
         return $requestHandler->handle($request);
