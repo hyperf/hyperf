2a3db07bc9dc46c498109f166128b881736d2717
李铭昕 <l@hyperf.io>
2020-06-04 14:13:37 +0800

    Added `hyperf/signal` component. (#1819)

---
+++ src/signal/src/Listener/SignalRegisterListener.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace Hyperf\Signal\Listener;
+
+use Hyperf\Event\Contract\ListenerInterface;
+use Hyperf\Framework\Event\BeforeWorkerStart;
+use Hyperf\Process\Event\BeforeProcessHandle;
+use Hyperf\Signal\SignalHandlerInterface as I;
+use Hyperf\Signal\SignalManager;
+use Psr\Container\ContainerInterface;
+
+class SignalRegisterListener implements ListenerInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function listen(): array
+    {
+        return [
+            BeforeWorkerStart::class,
+            BeforeProcessHandle::class,
+        ];
+    }
+
+    public function process(object $event)
+    {
+        $manager = $this->container->get(SignalManager::class);
+
+        $manager->init();
+        $manager->listen(value(function () use ($event) {
+            if ($event instanceof BeforeWorkerStart) {
+                return I::WORKER;
+            }
+
+            if ($event instanceof BeforeProcessHandle) {
+                return I::PROCESS;
+            }
+
+            return null;
+        }));
+    }
+}
