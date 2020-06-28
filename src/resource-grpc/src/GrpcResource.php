a408e37ec0e5d96279d6d3ef1628c23aba3e5f57
nfangxu <nfangxu@gmail.com>
2020-06-28 16:11:14 +0800

    add hyperf/resource-grpc

---
+++ src/resource-grpc/src/GrpcResource.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace Hyperf\ResourceGrpc;
+
+use Hyperf\Resource\Json\JsonResource;
+
+class GrpcResource extends JsonResource
+{
+    public function expect(): string
+    {
+        throw new UndefinedGrpcResourceExpectMessage($this);
+    }
+
+    public function toMessage()
+    {
+        return (new GrpcResponse($this))->toMessage();
+    }
+
+    /**
+     * Create new anonymous resource collection.
+     *
+     * @param mixed $resource
+     * @return AnonymousGrpcResourceCollection
+     */
+    public static function collection($resource)
+    {
+        return tap(
+            new AnonymousGrpcResourceCollection($resource, static::class),
+            function ($collection) {
+                if (property_exists(static::class, 'preserveKeys')) {
+                    $collection->preserveKeys = (new static([]))->preserveKeys === true;
+                }
+            }
+        );
+    }
+}
