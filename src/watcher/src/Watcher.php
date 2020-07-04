77e05418bb1e686153001736b5fa4eca9a1cec21
沈唁 <52o@qq52o.cn>
2020-07-04 10:06:38 +0800

    Replaced method `go` to `Coroutine::create`. (#2038)
    Co-authored-by: 李铭昕 <715557344@qq.com>

--- src/watcher/src/Watcher.php
+++ src/watcher/src/Watcher.php
@@ -17,6 +17,7 @@ use Hyperf\Di\Annotation\ScanConfig;
 use Hyperf\Di\Aop\Ast;
 use Hyperf\Di\ClassLoader;
 use Hyperf\Utils\Codec\Json;
+use Hyperf\Utils\Coroutine;
 use Hyperf\Utils\Filesystem\Filesystem;
 use Hyperf\Watcher\Ast\Metadata;
 use Hyperf\Watcher\Ast\RewriteClassNameVisitor;
@@ -127,7 +128,7 @@ class Watcher
         $this->restart(true);
 
         $channel = new Channel(999);
-        go(function () use ($channel) {
+        Coroutine::create(function () use ($channel) {
             $this->driver->watch($channel);
         });
 
@@ -202,7 +203,7 @@ class Watcher
             }
         }
 
-        go(function () {
+        Coroutine::create(function () {
             $this->channel->pop();
             $this->output->writeln('Start server ...');
             $ret = System::exec($this->option->getBin() . ' vendor/bin/watcher.php start');
