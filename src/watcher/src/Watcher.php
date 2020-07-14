c88949fc3eab7f1e64469883f867ba0041f94187
沈唁 <52o@qq52o.cn>
2020-07-14 13:42:37 +0800

    Don't kill `SIGTERM` if the process not exists. (#2110)
    * Added check if the process exists to `hyperf/watcher`
    
    * Format code.
    
    Co-authored-by: 李铭昕 <715557344@qq.com>

--- src/watcher/src/Watcher.php
+++ src/watcher/src/Watcher.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
  * This file is part of Hyperf.
  *
  * @link     https://www.hyperf.io
- * @document https://doc.hyperf.io
+ * @document https://hyperf.wiki
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
@@ -158,7 +158,9 @@ class Watcher
             $pid = $this->filesystem->get($file);
             try {
                 $this->output->writeln('Stop server...');
-                Process::kill((int) $pid, SIGTERM);
+                if (Process::kill((int) $pid, 0)) {
+                    Process::kill((int) $pid, SIGTERM);
+                }
             } catch (\Throwable $exception) {
                 $this->output->writeln('Stop server failed. Please execute `composer dump-autoload -o`');
             }
