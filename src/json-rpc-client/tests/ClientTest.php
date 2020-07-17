1a253b05a6871f693138de4c26bbc2329a531b24
李铭昕 <715557344@qq.com>
2020-07-17 10:22:06 +0800

    Deleted useless code.

--- src/json-rpc-client/tests/ClientTest.php
+++ src/json-rpc-client/tests/ClientTest.php
@@ -9,7 +9,7 @@ declare(strict_types=1);
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
-namespace HyperfTest\JsonRpcClient\Cases;
+namespace HyperfTest\JsonRpcClient;
 
 use Hyperf\JsonRpcClient\Exception\ServerException;
 use Hyperf\JsonRpcClient\Packer\JsonLengthPacker;
