f6309ba120729814808069ced7f50e1730b04304
pandaLIU <563883861@qq.com>
2020-07-21 12:03:34 +0800

    Added seeder call

--- src/database/src/Seeders/Seeder.php
+++ src/database/src/Seeders/Seeder.php
@@ -11,6 +11,12 @@ declare(strict_types=1);
  */
 namespace Hyperf\Database\Seeders;
 
+use Hyperf\Contract\ApplicationInterface;
+use Hyperf\Utils\ApplicationContext;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\NullOutput;
+
 abstract class Seeder
 {
     /**
@@ -35,5 +41,26 @@ abstract class Seeder
     public function getConnection()
     {
         return $this->connection;
+    }
+
+    protected function call(array $classes)
+    {
+        foreach ($classes as $class) {
+            $this->runCommand($class);
+        }
+    }
+
+    protected function runCommand($class)
+    {
+        $command = 'db:seed';
+        $params = ["command" => $command, "--path" =>  'seeders/' . $class . '.php'];
+
+        $input = new ArrayInput($params);
+        $output = new NullOutput();
+
+        /** @var Application $application */
+        $application = ApplicationContext::getContainer()->get(ApplicationInterface::class);
+        $application->setAutoExit(false);
+        $application->find($command)->run($input, $output);
     }
 }
