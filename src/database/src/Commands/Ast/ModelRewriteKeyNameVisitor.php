5167b64e9ebf8a196f03c0b49415611c537b8da3
李铭昕 <715557344@qq.com>
2020-07-24 10:57:57 +0800

    Added keytype and incrementing.

--- src/database/src/Commands/Ast/ModelRewriteKeyNameVisitor.php
+++ src/database/src/Commands/Ast/ModelRewriteKeyNameVisitor.php
@@ -15,6 +15,7 @@ use Hyperf\Database\Commands\ModelData;
 use Hyperf\Database\Commands\ModelOption;
 use Hyperf\Utils\Arr;
 use Hyperf\Utils\Collection;
+use Hyperf\Utils\Str;
 use PhpParser\Node;
 use PhpParser\NodeTraverser;
 use PhpParser\NodeVisitorAbstract;
@@ -36,6 +37,16 @@ class ModelRewriteKeyNameVisitor extends NodeVisitorAbstract
      */
     protected $hasPrimaryKey = false;
 
+    /**
+     * @var bool
+     */
+    protected $hasKeyType = false;
+
+    /**
+     * @var bool
+     */
+    protected $hasIncrementing = false;
+
     public function __construct(ModelOption $option, ModelData $data)
     {
         $this->option = $option;
@@ -48,7 +59,19 @@ class ModelRewriteKeyNameVisitor extends NodeVisitorAbstract
             case $node instanceof Node\Stmt\Property:
                 if ($node->props[0]->name->toLowerString() === 'primarykey') {
                     $this->hasPrimaryKey = true;
-                    if (! ($node = $this->rewritePrimaryKey($node))) {
+                    if (! ($node = $this->rewrite('primaryKey', $node))) {
+                        return NodeTraverser::REMOVE_NODE;
+                    }
+                }
+                if ($node->props[0]->name->toLowerString() === 'keytype') {
+                    $this->hasKeyType = true;
+                    if (! ($node = $this->rewrite('keyType', $node))) {
+                        return NodeTraverser::REMOVE_NODE;
+                    }
+                }
+                if ($node->props[0]->name->toLowerString() === 'incrementing') {
+                    $this->hasIncrementing = true;
+                    if (! ($node = $this->rewrite('incrementing', $node))) {
                         return NodeTraverser::REMOVE_NODE;
                     }
                 }
@@ -58,10 +81,6 @@ class ModelRewriteKeyNameVisitor extends NodeVisitorAbstract
 
     public function afterTraverse(array $nodes)
     {
-        if ($this->hasPrimaryKey || $this->shouldRemovedPrimaryKey()) {
-            return null;
-        }
-
         foreach ($nodes as $namespace) {
             if (! $namespace instanceof Node\Stmt\Namespace_) {
                 continue;
@@ -75,7 +94,13 @@ class ModelRewriteKeyNameVisitor extends NodeVisitorAbstract
                 foreach ($class->stmts as $key => $node) {
                     if (isset($node->props, $node->props[0], $node->props[0]->name)
                         && $node->props[0]->name->toLowerString() === 'table') {
-                        if ($newNode = $this->rewritePrimaryKey()) {
+                        if (! $this->hasKeyType && $newNode = $this->rewrite('keyType')) {
+                            array_splice($class->stmts, $key, 0, [$newNode]);
+                        }
+                        if (! $this->hasPrimaryKey && $newNode = $this->rewrite('primaryKey')) {
+                            array_splice($class->stmts, $key, 0, [$newNode]);
+                        }
+                        if (! $this->hasIncrementing && $newNode = $this->rewrite('incrementing')) {
                             array_splice($class->stmts, $key, 0, [$newNode]);
                         }
                         return null;
@@ -85,45 +110,68 @@ class ModelRewriteKeyNameVisitor extends NodeVisitorAbstract
         }
     }
 
-    protected function rewritePrimaryKey(?Node\Stmt\Property $node = null): ?Node\Stmt\Property
+    protected function rewrite($property = 'primaryKey', ?Node\Stmt\Property $node = null): ?Node\Stmt\Property
     {
-        if ($this->shouldRemovedPrimaryKey()) {
+        $data = $this->getKeyInfo();
+        if ($data === null) {
+            return $node;
+        }
+
+        [$primaryKey, $keyType, $incrementing] = $data;
+
+        if ($this->shouldRemoveProperty($property, ${$property})) {
             return null;
         }
 
-        $primaryKey = $this->getKeyName();
-        if (! $primaryKey) {
-            return $node;
-        }
         if ($node) {
-            $node->props[0]->default = new Node\Scalar\String_($primaryKey);
+            $node->props[0]->default = $this->getExpr($property, ${$property});
         } else {
-            $prop = new Node\Stmt\PropertyProperty('primaryKey', new Node\Scalar\String_($primaryKey));
-            $node = new Node\Stmt\Property(Node\Stmt\Class_::MODIFIER_PROTECTED, [$prop]);
+            $prop = new Node\Stmt\PropertyProperty($property, $this->getExpr($property, ${$property}));
+            $node = new Node\Stmt\Property(
+                $property == 'incrementing' ? Node\Stmt\Class_::MODIFIER_PUBLIC : Node\Stmt\Class_::MODIFIER_PROTECTED,
+                [$prop]
+            );
         }
 
         return $node;
     }
 
-    protected function getKeyName(): ?string
+    /**
+     * @param bool|string $value
+     * @return Node\Scalar
+     */
+    protected function getExpr(string $property, $value): Node\Expr
+    {
+        if ($property == 'incrementing') {
+            return new Node\Expr\ConstFetch(new Node\Name([$value ? 'true' : 'false']));
+        }
+
+        return new Node\Scalar\String_($value);
+    }
+
+    protected function getKeyInfo(): ?array
     {
         $columns = Collection::make($this->data->getColumns());
         $column = $columns->where('column_key', 'PRI')->first();
 
-        return Arr::get($column, 'column_name');
+        if ($column) {
+            $name = Arr::get($column, 'column_name');
+            $type = Str::endsWith(Arr::get($column, 'data_type', 'int'), 'int') ? 'int' : 'string';
+            $increment = Arr::get($column, 'extra') == 'auto_increment';
+            return [$name, $type, $increment];
+        }
+
+        return null;
     }
 
-    protected function shouldRemovedPrimaryKey(): bool
+    protected function shouldRemoveProperty($property, $value): bool
     {
-        $primaryKey = $this->getKeyName();
         $ref = new \ReflectionClass($this->data->getClass());
 
         if (! $ref->getParentClass()) {
             return false;
         }
 
-        $parentPrimaryKey = $ref->getParentClass()->getDefaultProperties()['primaryKey'] ?? null;
-
-        return $parentPrimaryKey === $primaryKey;
+        return $value == ($ref->getParentClass()->getDefaultProperties()[$property] ?? null);
     }
 }
