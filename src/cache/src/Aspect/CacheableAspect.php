633152fbec54f54ddf0708998ea76840e596f9a1
zhenguo.guan <guanzhenguo@3k.com>
2020-09-03 13:54:08 +0800

    增加缓存开关 & 异常处理

--- src/cache/src/Aspect/CacheableAspect.php
+++ src/cache/src/Aspect/CacheableAspect.php
@@ -15,9 +15,12 @@ use Hyperf\Cache\Annotation\Cacheable;
 use Hyperf\Cache\AnnotationManager;
 use Hyperf\Cache\CacheManager;
 use Hyperf\Cache\Driver\KeyCollectorInterface;
+use Hyperf\Contract\ConfigInterface;
+use Hyperf\Contract\StdoutLoggerInterface;
 use Hyperf\Di\Annotation\Aspect;
 use Hyperf\Di\Aop\AbstractAspect;
 use Hyperf\Di\Aop\ProceedingJoinPoint;
+use Hyperf\ExceptionHandler\Formatter\FormatterInterface;
 use Psr\Container\ContainerInterface;
 
 /**
@@ -41,25 +44,51 @@ class CacheableAspect extends AbstractAspect
      */
     protected $annotationManager;
 
-    public function __construct(CacheManager $manager, AnnotationManager $annotationManager)
+    /**
+     * @var StdoutLoggerInterface
+     */
+    protected $logger;
+
+    /**
+     * @var FormatterInterface
+     */
+    protected $formatter;
+
+    /**
+     * @var ConfigInterface
+     */
+    protected $config;
+
+    public function __construct(CacheManager $manager, AnnotationManager $annotationManager, StdoutLoggerInterface $logger, FormatterInterface $formatter, ConfigInterface $config)
     {
         $this->manager = $manager;
         $this->annotationManager = $annotationManager;
+        $this->logger = $logger;
+        $this->formatter = $formatter;
+        $this->config = $config;
     }
 
     public function process(ProceedingJoinPoint $proceedingJoinPoint)
     {
+        if($this->config->get('enable_cache', true) == false) {
+            return $proceedingJoinPoint->process();
+        }
+
         $className = $proceedingJoinPoint->className;
         $method = $proceedingJoinPoint->methodName;
         $arguments = $proceedingJoinPoint->arguments['keys'];
 
         [$key, $ttl, $group, $annotation] = $this->annotationManager->getCacheableValue($className, $method, $arguments);
 
-        $driver = $this->manager->getDriver($group);
-
-        [$has, $result] = $driver->fetch($key);
-        if ($has) {
-            return $result;
+        try {
+            $driver = $this->manager->getDriver($group);
+            [$has, $result] = $driver->fetch($key);
+            if ($has) {
+                return $result;
+            }
+        } catch (\Throwable $e) {
+            $this->logger->error($this->formatter->format($e), ['class' => $className, 'method' => $method, 'args' => $arguments]);
+            return $proceedingJoinPoint->process();
         }
 
         $result = $proceedingJoinPoint->process();
