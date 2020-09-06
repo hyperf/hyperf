e33477fc7308386a2761002b23026bdcf7a898e7
sy-records <52o@qq52o.cn>
2020-09-06 09:25:43 +0800

    add getTraceId and getSpanId method
    add ignoreErrors
    
    add function_exists
    
    add ignoreErrors

--- src/swoole-tracker/src/Aspect/CoroutineHandlerAspect.php
+++ src/swoole-tracker/src/Aspect/CoroutineHandlerAspect.php
@@ -17,6 +17,8 @@ use Hyperf\Guzzle\CoroutineHandler;
 use Psr\Container\ContainerInterface;
 use Swoole\Coroutine\Http\Client;
 use SwooleTracker\Stats;
+use function getSwooleTrackerSpanId;
+use function getSwooleTrackerTraceId;
 
 class CoroutineHandlerAspect extends AbstractAspect
 {
@@ -40,8 +42,8 @@ class CoroutineHandlerAspect extends AbstractAspect
             if ($client instanceof Client) {
                 $client->setHeaders(array_merge(
                     [
-                        'x-swoole-traceid' => '', // TODO: 获取生成的 TraceId
-                        'x-swoole-spanid' => '',
+                        'x-swoole-traceid' => function_exists('getSwooleTrackerTraceId') ? getSwooleTrackerTraceId() : '',
+                        'x-swoole-spanid' => function_exists('getSwooleTrackerSpanId') ? getSwooleTrackerSpanId() : '',
                     ],
                     $client->requestHeaders
                 ));
