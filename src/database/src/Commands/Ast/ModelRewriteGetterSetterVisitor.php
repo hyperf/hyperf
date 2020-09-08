1d2e9cdffc6254d5695e6c6c1e5d8b2dcf95eb2e
李铭昕 <715557344@qq.com>
2020-09-08 17:16:11 +0800

    Support getter and setter.

--- src/database/src/Commands/Ast/ModelRewriteGetterSetterVisitor.php
+++ src/database/src/Commands/Ast/ModelRewriteGetterSetterVisitor.php
@@ -13,11 +13,21 @@ namespace Hyperf\Database\Commands\Ast;
 
 use Hyperf\Database\Commands\ModelData;
 use Hyperf\Database\Commands\ModelOption;
+use Hyperf\Utils\Str;
+use PhpParser\Node;
 use Roave\BetterReflection\Reflection\ReflectionClass;
 
 class ModelRewriteGetterSetterVisitor extends AbstractVisitor
 {
-    protected $methods = [];
+    /**
+     * @var string[]
+     */
+    protected $getters = [];
+
+    /**
+     * @var string[]
+     */
+    protected $setters = [];
 
     public function __construct(ModelOption $option, ModelData $data)
     {
@@ -26,13 +36,97 @@ class ModelRewriteGetterSetterVisitor extends AbstractVisitor
         $this->collectMethods($data->getClass());
     }
 
+    public function afterTraverse(array $nodes)
+    {
+        foreach ($nodes as $namespace) {
+            if (! $namespace instanceof Node\Stmt\Namespace_) {
+                continue;
+            }
+
+            foreach ($namespace->stmts as $class) {
+                if (! $class instanceof Node\Stmt\Class_) {
+                    continue;
+                }
+
+                foreach ($class->stmts as $stmt) {
+                    var_dump($stmt);
+                }
+
+                array_push($class->stmts, ...$this->buildGetterAndSetter());
+            }
+        }
+
+        return $nodes;
+    }
+
+    /**
+     * @return Node\Stmt\ClassMethod[]
+     */
+    protected function buildGetterAndSetter(): array
+    {
+        $stmts = [];
+        foreach ($this->data->getColumns() as $column) {
+            if ($name = $column['column_name'] ?? null) {
+                $getter = getter($name);
+                if (! in_array($getter, $this->getters)) {
+                    $stmts[] = $this->createGetter($getter, $name);
+                }
+                $setter = setter($name);
+                if (! in_array($setter, $this->setters)) {
+                    $stmts[] = $this->createSetter($setter, $name);
+                }
+            }
+        }
+
+        return $stmts;
+    }
+
+    protected function createGetter(string $method, string $name): Node\Stmt\ClassMethod
+    {
+        $node = new Node\Stmt\ClassMethod($method, ['flags' => Node\Stmt\Class_::MODIFIER_PUBLIC]);
+        $node->stmts[] = new Node\Stmt\Return_(
+            new Node\Expr\PropertyFetch(
+                new Node\Expr\Variable('this'),
+                new Node\Identifier($name)
+            )
+        );
+
+        return $node;
+    }
+
+    protected function createSetter(string $method, string $name): Node\Stmt\ClassMethod
+    {
+        $node = new Node\Stmt\ClassMethod($method, [
+            'flags' => Node\Stmt\Class_::MODIFIER_PUBLIC,
+            'params' => [new Node\Param(new Node\Expr\Variable($name))]
+        ]);
+        $node->stmts[] = new Node\Stmt\Expression(
+            new Node\Expr\Assign(
+                new Node\Expr\PropertyFetch(
+                    new Node\Expr\Variable('this'),
+                    new Node\Identifier($name)
+                ),
+                new Node\Expr\Variable($name)
+            )
+        );
+        $node->stmts[] = new Node\Stmt\Return_(
+            new Node\Expr\Variable('this')
+        );
+
+        return $node;
+    }
+
     protected function collectMethods(string $class)
     {
-        // /** @var ReflectionClass $reflection */
-        // $reflection = self::getReflector()->reflect($class);
-        // $methods = $reflection->getImmediateMethods();
-        // foreach ($methods as $method) {
-        //     var_dump($method);
-        // }
+        /** @var ReflectionClass $reflection */
+        $reflection = BetterReflectionManager::getReflector()->reflect($class);
+        $methods = $reflection->getImmediateMethods();
+        foreach ($methods as $method) {
+            if (Str::startsWith($method->getName(), 'get')) {
+                $this->getters[] = $method->getName();
+            } elseif (Str::startsWith($method->getName(), 'set')) {
+                $this->setters[] = $method->getName();
+            }
+        }
     }
 }
