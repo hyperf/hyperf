54395387423814be7a18b61b5725b859b55418e2
yansongda <me@yansongda.cn>
2020-09-09 11:18:30 +0800

    add: 增加 container retry policy

---
+++ src/retry/src/Policy/FallbackContainerRetryPolicy.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace Hyperf\Retry\Policy;
+
+use Hyperf\Retry\RetryContext;
+use Hyperf\Utils\ApplicationContext;
+use Throwable;
+
+class FallbackContainerRetryPolicy extends BaseRetryPolicy implements RetryPolicyInterface
+{
+    /**
+     * @var string
+     */
+    public $fallbackClass = '';
+
+    /**
+     * @var string
+     */
+    public $fallbackMethod = '';
+
+    public function __construct(string $fallbackClass = '', string $fallbackMethod = '')
+    {
+        $this->fallbackClass = $fallbackClass;
+        $this->fallbackMethod = $fallbackMethod;
+    }
+
+    public function end(RetryContext &$retryContext): bool
+    {
+        if (!isset($retryContext['retryExhausted']) ||
+            empty($this->fallbackClass) || empty($this->fallbackMethod)) {
+            return false;
+        }
+
+        $retryContext['lastThrowable'] = $retryContext['lastResult'] = null;
+
+        $arguments = [];
+        if (isset($retryContext['proceedingJoinPoint'])) {
+            $arguments = $retryContext['proceedingJoinPoint']->getArguments();
+        }
+
+        try {
+            $retryContext['lastResult'] = ApplicationContext::getContainer()->get($this->fallbackClass)->{$this->fallbackMethod}(...$arguments);
+        } catch (Throwable $throwable) {
+            $retryContext['lastThrowable'] = $throwable;
+        }
+
+        return false;
+    }
+}
