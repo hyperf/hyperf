af1d0233d89f07b5bb2e659e53c52af56fdc2584
fangjw <fangjw@wecut.com>
2020-09-28 16:00:15 +0800

    CamelCase最近解决了驼峰字段写入的问题，但同时地，导致下划线字段写入出现了问题，没进行向下兼容

--- src/database/src/Model/Concerns/CamelCase.php
+++ src/database/src/Model/Concerns/CamelCase.php
@@ -38,6 +38,7 @@ trait CamelCase
     {
         $fillable = [];
         foreach (parent::getFillable() as $key) {
+            $fillable[] = $key;
             $fillable[] = $this->keyTransform($key);
         }
         return $fillable;
