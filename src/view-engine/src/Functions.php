8aaaa2352c0f2aabe72fef701f2a114cd5efc178
李铭昕 <715557344@qq.com>
2020-10-09 12:36:07 +0800

    Fixed `@env` and `@production` don't work.

--- src/view-engine/src/Functions.php
+++ src/view-engine/src/Functions.php
@@ -9,6 +9,8 @@ declare(strict_types=1);
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
+namespace Hyperf\ViewEngine;
+
 use Hyperf\Utils\ApplicationContext;
 use Hyperf\Utils\Context;
 use Hyperf\Utils\Contracts\Arrayable;
@@ -16,7 +18,7 @@ use Hyperf\ViewEngine\Contract\FactoryInterface;
 use Hyperf\ViewEngine\Contract\ViewInterface;
 use Psr\Http\Message\ResponseInterface;
 
-if (! function_exists('view')) {
+if (! function_exists('Hyperf\\ViewEngine\\view')) {
     /**
      * Get the evaluated view contents for the given view.
      *
