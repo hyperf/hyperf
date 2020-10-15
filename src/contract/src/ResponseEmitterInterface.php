11d821c2f5f65764cd3a6efeb8b6538ea6c5b739
李铭昕 <715557344@qq.com>
2020-10-15 11:01:33 +0800

    Update ResponseEmitterInterface.php

--- src/contract/src/ResponseEmitterInterface.php
+++ src/contract/src/ResponseEmitterInterface.php
@@ -15,5 +15,8 @@ use Psr\Http\Message\ResponseInterface;
 
 interface ResponseEmitterInterface
 {
+    /**
+     * @param mixed $connection swoole response or swow session
+     */
     public function emit(ResponseInterface $response, $connection, bool $withContent = true);
 }
