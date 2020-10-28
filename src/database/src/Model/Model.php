5a39a69ab4ff72592c9be3e89f77bcc7dbdf91c1
李铭昕 <715557344@qq.com>
2020-10-28 16:29:36 +0800

    Update Model.php

--- src/database/src/Model/Model.php
+++ src/database/src/Model/Model.php
@@ -29,7 +29,6 @@ use Psr\EventDispatcher\StoppableEventInterface;
 
 /**
  * @mixin Builder
- * @mixin QueryBuilder
  */
 abstract class Model implements ArrayAccess, Arrayable, Jsonable, JsonSerializable, CompressInterface
 {
