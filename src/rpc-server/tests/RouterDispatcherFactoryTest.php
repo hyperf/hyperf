7b2bf9e778bfc0ae43185572c747725da67c653a
李铭昕 <715557344@qq.com>
2020-10-29 18:06:43 +0800

    Added test case

--- src/rpc-server/tests/RouterDispatcherFactoryTest.php
+++ src/rpc-server/tests/RouterDispatcherFactoryTest.php
@@ -11,7 +11,16 @@ declare(strict_types=1);
  */
 namespace HyperfTest\RpcServer;
 
+use Hyperf\Rpc\Contract\PathGeneratorInterface;
+use Hyperf\Rpc\PathGenerator\PathGenerator;
+use Hyperf\RpcServer\Annotation\RpcService;
+use Hyperf\RpcServer\Event\AfterPathRegister;
+use Hyperf\RpcServer\Router\DispatcherFactory;
+use HyperfTest\RpcServer\Stub\ContainerStub;
+use HyperfTest\RpcServer\Stub\IdGeneratorStub;
+use Mockery;
 use PHPUnit\Framework\TestCase;
+use Psr\EventDispatcher\EventDispatcherInterface;
 
 /**
  * @internal
@@ -19,8 +28,31 @@ use PHPUnit\Framework\TestCase;
  */
 class RouterDispatcherFactoryTest extends TestCase
 {
+    protected function tearDown()
+    {
+        Mockery::close();
+    }
+
     public function testHandleRpcService()
     {
-        $this->assertTrue(true);
+        $container = ContainerStub::getContainer();
+        $container->shouldReceive('get')->with(EventDispatcherInterface::class)->andReturnUsing(function () {
+            $dispatcher = Mockery::mock(EventDispatcherInterface::class);
+            $dispatcher->shouldReceive('dispatch')->withAnyArgs()->once()->andReturnUsing(function ($object) {
+                $this->assertInstanceOf(AfterPathRegister::class, $object);
+                $this->assertSame('/id_generator_stub/generate', $object->path);
+                return $object;
+            });
+            return $dispatcher;
+        });
+        $container->shouldReceive('get')->with(PathGeneratorInterface::class)->andReturn(new PathGenerator());
+        $factory = new DispatcherFactory(
+            $container->get(EventDispatcherInterface::class),
+            $container->get(PathGeneratorInterface::class)
+        );
+        $ref = new \ReflectionClass($factory);
+        $m = $ref->getMethod('handleRpcService');
+        $m->setAccessible(true);
+        $m->invokeArgs($factory, [IdGeneratorStub::class, new RpcService('IdGenerator'), [], []]);
     }
 }
