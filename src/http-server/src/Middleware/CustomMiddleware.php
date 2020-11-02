3d0ea9b5428ec42f56cc3a00fc1b014961bee27b
李铭昕 <715557344@qq.com>
2020-11-02 22:21:22 +0800

    Added CustomMiddleware, you can reuse middleware by passing in parameters.

---
+++ src/http-server/src/Middleware/CustomMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace Hyperf\HttpServer\Middleware;
+
+use Hyperf\HttpServer\Router\Dispatched;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+abstract class CustomMiddleware implements MiddlewareInterface
+{
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        /** @var Dispatched $dispatched */
+        $dispatched = $request->getAttribute(Dispatched::class);
+        $params = $dispatched->handler->options[static::class] ?? [];
+        return $this->handle($request, $handler, ...$params);
+    }
+
+    abstract public function handle(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface;
+}
