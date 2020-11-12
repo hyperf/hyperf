6757e4cceb5539aa162387a49c1753eb30a73481
李铭昕 <715557344@qq.com>
2020-11-12 09:36:11 +0800

    Rename expire_time to expire_minutes

--- src/session/publish/session.php
+++ src/session/publish/session.php
@@ -19,6 +19,6 @@ return [
         'gc_maxlifetime' => 1200,
         'session_name' => 'HYPERF_SESSION_ID',
         'domain' => null,
-        'expire_time' => 5 * 60
+        'expire_minutes' => 5 * 60,
     ],
 ];
