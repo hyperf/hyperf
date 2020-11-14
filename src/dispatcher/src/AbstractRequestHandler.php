dd9cc57035fc6d103d4f1b7f37439b6edc6cfc60
zhangchengming@kkguan.com <b8h5zr2hj0>
2020-11-14 19:51:24 +0800

    Fixed Middleware does not work as expected when repeatedly configured

--- src/dispatcher/src/AbstractRequestHandler.php
+++ src/dispatcher/src/AbstractRequestHandler.php
@@ -45,7 +45,7 @@ abstract class AbstractRequestHandler
      */
     public function __construct(array $middlewares, $coreHandler, ContainerInterface $container)
     {
-        $this->middlewares = array_unique($middlewares);
+        $this->middlewares = array_values(array_unique($middlewares));
         $this->coreHandler = $coreHandler;
         $this->container = $container;
     }
