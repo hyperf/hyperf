2d413f4f4be26be36538ef578f3748eb0f91ac84
李铭昕 <715557344@qq.com>
2020-11-24 13:58:09 +0800

    Added HTTP_CLIENT_ESTATUS_SEND_FAILED

--- src/guzzle/src/CoroutineHandler.php
+++ src/guzzle/src/CoroutineHandler.php
@@ -255,6 +255,9 @@ class CoroutineHandler
                 return new RequestException(sprintf('Request timed out, errCode=%s', $errCode), $request, null, null, $ctx);
             case SWOOLE_HTTP_CLIENT_ESTATUS_SERVER_RESET:
                 return new RequestException('Server reset', $request, null, null, $ctx);
+            case -4:
+                // Added 2020-09-04
+                return new RequestException(sprintf('Send failed, errCode=%s', $errCode), $request, null, null, $ctx);
             default:
                 return new RequestException(sprintf('Unknown reason, errCode=%s', $errCode), $request, null, null, $ctx);
         }
