27b3d7a3eec6cf0f431524e0df85b34bce7783d5
武杰 <1224805548@qq.com>
2020-11-25 14:15:44 +0800

    encrypter

---
+++ src/encryption/src/Encrypter.php
@@ -0,0 +1,236 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace Hyperf\Encryption;
+
+use Hyperf\Contract\ConfigInterface;
+use Psr\Container\ContainerInterface;
+
+class Encrypter
+{
+    /**
+     * The encryption key.
+     *
+     * @var string
+     */
+    protected $key;
+
+    /**
+     * The algorithm used for encryption.
+     *
+     * @var string
+     */
+    protected $cipher;
+
+    /**
+     * @var []
+     */
+    private $configs;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $config = $container->get(ConfigInterface::class);
+        $this->configs = $config->get('encrypter', []);
+
+        if ($this->key = (string) ($this->configs['key'] ?? '') and ! $this->key) {
+            throw new \RuntimeException('Encrypter key Not Set');
+        }
+
+        if ($this->cipher = $this->configs['cipher'] ?? 'AES-128-CBC' and ! in_array(strtolower($this->cipher), openssl_get_cipher_methods())) {
+            throw new \RuntimeException('Encrypter ciphers Not supported');
+        }
+    }
+
+    /**
+     * Encrypt the given value.
+     *
+     * @param mixed $value
+     * @param bool $serialize
+     * @throws \Exception
+     * @return string
+     */
+    public function encrypt($value, $serialize = true)
+    {
+        $iv = random_bytes(openssl_cipher_iv_length($this->cipher));
+
+        // First we will encrypt the value using OpenSSL. After this is encrypted we
+        // will proceed to calculating a MAC for the encrypted value so that this
+        // value can be verified later as not having been changed by the users.
+        $value = \openssl_encrypt(
+            $serialize ? serialize($value) : $value,
+            $this->cipher,
+            $this->key,
+            0,
+            $iv
+        );
+
+        if ($value === false) {
+            throw new EncryptException('Could not encrypt the data.');
+        }
+
+        // Once we get the encrypted value we'll go ahead and base64_encode the input
+        // vector and create the MAC for the encrypted value so we can then verify
+        // its authenticity. Then, we'll JSON the data into the "payload" array.
+        $mac = $this->hash($iv = base64_encode($iv), $value);
+
+        $json = json_encode(compact('iv', 'value', 'mac'), JSON_UNESCAPED_SLASHES);
+
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new EncryptException('Could not encrypt the data.');
+        }
+
+        return base64_encode($json);
+    }
+
+    /**
+     * Encrypt a string without serialization.
+     *
+     * @param string $value
+     * @throws \Exception
+     * @return string
+     */
+    public function encryptString($value)
+    {
+        return $this->encrypt($value, false);
+    }
+
+    /**
+     * Decrypt the given value.
+     *
+     * @param string $payload
+     * @param bool $unserialize
+     * @return mixed
+     */
+    public function decrypt($payload, $unserialize = true)
+    {
+        $payload = $this->getJsonPayload($payload);
+
+        $iv = base64_decode($payload['iv']);
+
+        // Here we will decrypt the value. If we are able to successfully decrypt it
+        // we will then unserialize it and return it out to the caller. If we are
+        // unable to decrypt this value we will throw out an exception message.
+        $decrypted = \openssl_decrypt(
+            $payload['value'],
+            $this->cipher,
+            $this->key,
+            0,
+            $iv
+        );
+
+        if ($decrypted === false) {
+            throw new DecryptException('Could not decrypt the data.');
+        }
+
+        return $unserialize ? unserialize($decrypted) : $decrypted;
+    }
+
+    /**
+     * Decrypt the given string without unserialization.
+     *
+     * @param string $payload
+     * @return string
+     */
+    public function decryptString($payload)
+    {
+        return $this->decrypt($payload, false);
+    }
+
+    /**
+     * Get the encryption key.
+     *
+     * @return string
+     */
+    public function getKey()
+    {
+        return $this->key;
+    }
+
+    /**
+     * Create a MAC for the given value.
+     *
+     * @param string $iv
+     * @param mixed $value
+     * @return string
+     */
+    protected function hash($iv, $value)
+    {
+        return hash_hmac('sha256', $iv . $value, $this->key);
+    }
+
+    /**
+     * Get the JSON array from the given payload.
+     *
+     * @param string $payload
+     * @return array
+     */
+    protected function getJsonPayload($payload)
+    {
+        $payload = json_decode(base64_decode($payload), true);
+
+        // If the payload is not valid JSON or does not have the proper keys set we will
+        // assume it is invalid and bail out of the routine since we will not be able
+        // to decrypt the given value. We'll also check the MAC for this encryption.
+        if (! $this->validPayload($payload)) {
+            throw new DecryptException('The payload is invalid.');
+        }
+
+        if (! $this->validMac($payload)) {
+            throw new DecryptException('The MAC is invalid.');
+        }
+
+        return $payload;
+    }
+
+    /**
+     * Verify that the encryption payload is valid.
+     *
+     * @param mixed $payload
+     * @return bool
+     */
+    protected function validPayload($payload)
+    {
+        return is_array($payload) && isset($payload['iv'], $payload['value'], $payload['mac']) &&
+            strlen(base64_decode($payload['iv'], true)) === openssl_cipher_iv_length($this->cipher);
+    }
+
+    /**
+     * Determine if the MAC for the given payload is valid.
+     *
+     * @return bool
+     */
+    protected function validMac(array $payload)
+    {
+        $calculated = $this->calculateMac($payload, $bytes = random_bytes(16));
+
+        return hash_equals(
+            hash_hmac('sha256', $payload['mac'], $bytes, true),
+            $calculated
+        );
+    }
+
+    /**
+     * Calculate the hash of the given payload.
+     *
+     * @param array $payload
+     * @param string $bytes
+     * @return string
+     */
+    protected function calculateMac($payload, $bytes)
+    {
+        return hash_hmac(
+            'sha256',
+            $this->hash($payload['iv'], $payload['value']),
+            $bytes,
+            true
+        );
+    }
+}
