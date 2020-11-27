5eb6b59ce7dea7d04146f08e054f12178775891e
李铭昕 <715557344@qq.com>
2020-11-27 19:37:40 +0800

    Update

--- .travis/guzzle/upgrade.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-if [ ${GUZZLE_7} ]; then
-  composer remove overtrue/flysystem-cos --dev
-  composer remove endclothing/prometheus_client_php --dev
-  cp -rf .travis/guzzle/phpstan.neon phpstan.neon
-fi
