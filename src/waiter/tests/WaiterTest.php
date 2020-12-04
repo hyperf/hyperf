3881f5ac16d0ce635280161d34d639b04d228df8
李铭昕 <715557344@qq.com>
2020-12-04 16:08:18 +0800

    Added test cases.

--- src/waiter/tests/WaiterTest.php
+++ src/waiter/tests/WaiterTest.php
@@ -11,7 +11,11 @@ declare(strict_types=1);
  */
 namespace HyperfTest\Waiter;
 
+use Hyperf\Engine\Channel;
+use Hyperf\Utils\Coroutine;
+use Hyperf\Waiter\Exception\WaitTimeoutException;
 use PHPUnit\Framework\TestCase;
+use function Hyperf\Waiter\wait;
 
 /**
  * @internal
@@ -19,4 +23,79 @@ use PHPUnit\Framework\TestCase;
  */
 class WaiterTest extends TestCase
 {
+    public function testWait()
+    {
+        $id = uniqid();
+        $result = wait(function () use ($id) {
+            return $id;
+        });
+
+        $this->assertSame($id, $result);
+
+        $id = rand(0, 9999);
+        $result = wait(function () use ($id) {
+            return $id + 1;
+        });
+
+        $this->assertSame($id + 1, $result);
+    }
+
+    public function testWaitNone()
+    {
+        $callback = function () {
+        };
+        $result = wait($callback);
+        $this->assertSame($result, $callback());
+        $this->assertSame(null, $result);
+
+        $callback = function () {
+            return null;
+        };
+        $result = wait($callback);
+        $this->assertSame($result, $callback());
+        $this->assertSame(null, $result);
+    }
+
+    public function testWaitException()
+    {
+        $message = uniqid();
+        $callback = function () use ($message) {
+            throw new \RuntimeException($message);
+        };
+
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage($message);
+        wait($callback);
+    }
+
+    public function testWaitReturnException()
+    {
+        $message = uniqid();
+        $callback = function () use ($message) {
+            return new \RuntimeException($message);
+        };
+
+        $result = wait($callback);
+        $this->assertInstanceOf(\RuntimeException::class, $result);
+        $this->assertSame($message, $result->getMessage());
+    }
+
+    public function testPushTimeout()
+    {
+        $channel = new Channel(1);
+        $this->assertSame(true, $channel->push(1, 1));
+        $this->assertSame(false, $channel->push(1, 1));
+    }
+
+    public function testTimeout()
+    {
+        $callback = function () {
+            Coroutine::sleep(0.5);
+            return true;
+        };
+
+        $this->expectException(WaitTimeoutException::class);
+        $this->expectExceptionMessage('Channel wait failed, reason: Timed out for 0.001 s');
+        wait($callback, 0.001);
+    }
 }
