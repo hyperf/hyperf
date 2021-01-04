ece8629d8c7c4c4e93f324031edec29e41c35879
李铭昕 <715557344@qq.com>
2021-01-04 10:22:57 +0800

    Update ConnectionClosedException.php

--- src/socketio-server/src/Exception/ConnectionClosedException.php
+++ src/socketio-server/src/Exception/ConnectionClosedException.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  */
 namespace Hyperf\SocketIOServer\Exception;
 
-use Hyperf\DB\Exception\RuntimeException;
+use Hyperf\Server\Exception\RuntimeException;
 
 class ConnectionClosedException extends RuntimeException
 {
