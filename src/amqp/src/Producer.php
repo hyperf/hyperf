db523a6ea448c3b71c5b63d02359ed4a39ad221e
李铭昕 <715557344@qq.com>
2021-02-01 12:01:38 +0800

    Fixed connection exhausted, when connect amqp server failed.

--- src/amqp/src/Producer.php
+++ src/amqp/src/Producer.php
@@ -34,16 +34,15 @@ class Producer extends Builder
         $pool = $this->getConnectionPool($producerMessage->getPoolName());
         /** @var \Hyperf\Amqp\Connection $connection */
         $connection = $pool->get();
-        if ($confirm) {
-            $channel = $connection->getConfirmChannel();
-        } else {
-            $channel = $connection->getChannel();
-        }
-        $channel->set_ack_handler(function () use (&$result) {
-            $result = true;
-        });
-
         try {
+            if ($confirm) {
+                $channel = $connection->getConfirmChannel();
+            } else {
+                $channel = $connection->getChannel();
+            }
+            $channel->set_ack_handler(function () use (&$result) {
+                $result = true;
+            });
             $channel->basic_publish($message, $producerMessage->getExchange(), $producerMessage->getRoutingKey());
             $channel->wait_for_pending_acks_returns($timeout);
         } catch (\Throwable $exception) {
