7fcf0fb99f30697f29b36248d555a16cf21c28be
idx <sxj1394@gmail.com>
2021-03-17 15:14:03 +0800

    Support `$connection` and `$attempts` for `Transactional`. (#3376)
    * Added Transactional::$attempts
    
    * Added test cases
    
    Co-authored-by: 李铭昕 <715557344@qq.com>

--- src/db-connection/src/Aspect/TransactionAspect.php
+++ src/db-connection/src/Aspect/TransactionAspect.php
@@ -13,9 +13,11 @@ namespace Hyperf\DbConnection\Aspect;
 
 use Hyperf\DbConnection\Annotation\Transactional;
 use Hyperf\DbConnection\Db;
+use Hyperf\Di\Annotation\AnnotationCollector;
 use Hyperf\Di\Annotation\Aspect;
 use Hyperf\Di\Aop\AbstractAspect;
 use Hyperf\Di\Aop\ProceedingJoinPoint;
+use Hyperf\Di\Exception\AnnotationException;
 
 /**
  * @Aspect
@@ -28,10 +30,23 @@ class TransactionAspect extends AbstractAspect
 
     public function process(ProceedingJoinPoint $proceedingJoinPoint)
     {
-        return Db::transaction(
-            function () use ($proceedingJoinPoint) {
+        $transactional = $this->getTransactionalAnnotation($proceedingJoinPoint->className, $proceedingJoinPoint->methodName);
+
+        return Db::connection($transactional->connection)->transaction(
+            static function () use ($proceedingJoinPoint) {
                 return $proceedingJoinPoint->process();
-            }
+            },
+            $transactional->attempts
         );
+    }
+
+    protected function getTransactionalAnnotation(string $className, string $method): Transactional
+    {
+        $annotation = AnnotationCollector::getClassMethodAnnotation($className, $method)[Transactional::class] ?? null;
+        if (! $annotation instanceof Transactional) {
+            throw new AnnotationException("Annotation Transactional couldn't be collected successfully.");
+        }
+
+        return $annotation;
     }
 }
