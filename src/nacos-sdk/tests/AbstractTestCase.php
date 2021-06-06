52fcb3427392ae47a85117cd2e22e55c4508673c
pandaLIU <563883861@qq.com>
2021-06-06 13:42:42 +0800

    merge hyperf/nacos-sdk-incubator into hyperf/hyperf

---
+++ src/nacos-sdk/tests/AbstractTestCase.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace HyperfTest\NacosSdk;
+
+use Mockery;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class AbstractTestCase.
+ */
+abstract class AbstractTestCase extends TestCase
+{
+    /**
+     * @var bool
+     */
+    protected $isMock = true;
+
+    protected function tearDown(): void
+    {
+        Mockery::close();
+    }
+}
