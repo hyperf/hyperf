f525b211d6bba86c1876f3b8f94dcb50972d0771
huangzhhui <huangzhwork@gmail.com>
2021-06-28 03:05:02 +0800

    Add config file of service-governance

---
+++ src/service-governance/publish/services.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+return [
+    'enable' => [
+        'discovery' => true,
+        'register' => true,
+    ],
+    'consumers' => [],
+    'providers' => [],
+    'drivers' => [
+        'nacos' => [
+            // nacos server url like https://nacos.hyperf.io, Priority is higher than host:port
+            // 'url' => '',
+            // The nacos host info
+            'host' => '127.0.0.1',
+            'port' => 8848,
+            // The nacos account info
+            'username' => null,
+            'password' => null,
+            'guzzle' => [
+                'config' => [],
+            ],
+            'service_name' => 'hyperf',
+            'group_name' => 'api',
+            'namespace_id' => 'namespace_id',
+            'protect_threshold' => 0.5,
+            'metadata' => null,
+            'selector' => null,
+            'instance' => [
+                'cluster' => null,
+                'weight' => null,
+                'metadata' => null,
+                'ephemeral' => null,
+                'heartbeat' => 5,
+                'auto_removed' => false,
+            ],
+        ],
+    ],
+];
