38ad20c4cdeb1be4fca8a446e0b7ae6599250673
李铭昕 <715557344@qq.com>
2021-07-11 08:38:18 +0800

    Fixed bug that amqp consumer couldn't restart when rabbitmq stopped.

--- src/amqp/src/AMQPConnection.php
+++ src/amqp/src/AMQPConnection.php
@@ -16,6 +16,7 @@ use Hyperf\Utils\Channel\ChannelManager;
 use Hyperf\Utils\Coordinator\Constants;
 use Hyperf\Utils\Coordinator\CoordinatorManager;
 use Hyperf\Utils\Coroutine;
+use Hyperf\Utils\Exception\ChannelClosedException;
 use PhpAmqpLib\Channel\AMQPChannel;
 use PhpAmqpLib\Connection\AbstractConnection;
 use PhpAmqpLib\Exception\AMQPRuntimeException;
@@ -186,6 +187,12 @@ class AMQPConnection extends AbstractConnection
         }
     }
 
+    public function close($reply_code = 0, $reply_text = '', $method_sig = [0, 0])
+    {
+        $this->channelManager->flush();
+        return parent::close($reply_code, $reply_text, $method_sig);
+    }
+
     protected function makeChannelId(): int
     {
         for ($i = 0; $i < $this->channel_max; ++$i) {
@@ -232,6 +239,9 @@ class AMQPConnection extends AbstractConnection
         if ($data === false) {
             if ($chan->isTimeout()) {
                 throw new AMQPTimeoutException('Timeout waiting on channel');
+            }
+            if ($chan->isClosing()) {
+                throw new ChannelClosedException('Wait channel was closed.');
             }
         }
 
