7c2193d6670bcef491ef0702c9d6851330134d81
李铭昕 <715557344@qq.com>
2021-08-05 12:59:08 +0800

    Added test cases for AMQP

--- src/amqp/tests/Stub/ContainerStub.php
+++ src/amqp/tests/Stub/ContainerStub.php
@@ -18,6 +18,8 @@ use Hyperf\Config\Config;
 use Hyperf\Contract\ConfigInterface;
 use Hyperf\Contract\StdoutLoggerInterface;
 use Hyperf\Di\Container;
+use Hyperf\ExceptionHandler\Formatter\DefaultFormatter;
+use Hyperf\ExceptionHandler\Formatter\FormatterInterface;
 use Hyperf\Utils\ApplicationContext;
 use Mockery;
 use Psr\Container\ContainerInterface;
@@ -53,6 +55,7 @@ class ContainerStub
         $container->shouldReceive('get')->with(StdoutLoggerInterface::class)->andReturnUsing(function () {
             $logger = Mockery::mock(StdoutLoggerInterface::class);
             $logger->shouldReceive('debug')->andReturn(null);
+            $logger->shouldReceive('log')->andReturn(null);
             return $logger;
         });
         $container->shouldReceive('get')->with(EventDispatcherInterface::class)->andReturnUsing(function () {
@@ -64,6 +67,7 @@ class ContainerStub
         $container->shouldReceive('get')->with(Consumer::class)->andReturnUsing(function () use ($container) {
             return new Consumer($container, $container->get(ConnectionFactory::class), $container->get(StdoutLoggerInterface::class));
         });
+        $container->shouldReceive('get')->with(FormatterInterface::class)->andReturn(new DefaultFormatter());
 
         return $container;
     }
