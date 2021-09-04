705370fb1dcac2dcc00ccf1afb896b023c08bbb6
李铭昕 <l@hyperf.io>
2021-09-05 00:00:43 +0800

    Added test cases.

---
+++ src/validation/tests/Cases/Stub/BarSceneRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace HyperfTest\Validation\Cases\Stub;
+
+use Hyperf\Validation\Request\FormRequest;
+
+class BarSceneRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        $scene = $this->getScene();
+
+        switch ($scene) {
+            case 'required':
+                return [
+                    'name' => 'required',
+                ];
+            default:
+                return [
+                    'name' => 'required|integer',
+                ];
+        }
+    }
+}
