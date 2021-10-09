0d1a82bfa75bee110ffbb8546d1e4c159972ce1d
李铭昕 <715557344@qq.com>
2021-10-09 09:09:16 +0800

    Update FilesystemTest.php

--- src/utils/tests/FilesystemTest.php
+++ src/utils/tests/FilesystemTest.php
@@ -11,6 +11,7 @@ declare(strict_types=1);
  */
 namespace HyperfTest\Utils;
 
+use Hyperf\Utils\Filesystem\Filesystem;
 use Hyperf\Utils\Parallel;
 use PHPUnit\Framework\TestCase;
 use Swoole\Coroutine\Channel;
@@ -64,6 +65,15 @@ class FilesystemTest extends TestCase
 
             $this->assertSame([2, 1], $result);
         });
+    }
+
+    public function testMakeDirection()
+    {
+        $system = new Filesystem();
+        $system->makeDirectory(BASE_PATH . '/runtime/test');
+        $system->makeDirectory(BASE_PATH . '/runtime/test');
+
+        $this->assertTrue(true);
     }
 
     /**
