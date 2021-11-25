18a620f5089261424bae90126cfe2fddcc14ed0d
sy-records <52o@qq52o.cn>
2021-11-25 14:14:01 +0800

    update

--- src/utils/src/IPReader.php
+++ src/utils/src/IPReader.php
@@ -18,30 +18,10 @@ class IPReader implements IPReaderInterface
 {
     public function read(): string
     {
-        $ips = [];
-        if (function_exists('swoole_get_local_ip')) {
-            $ips = swoole_get_local_ip();
+        try {
+            return Network::ip();
+        } catch (\Throwable $throwable) {
+            throw new IPReadFailedException($throwable->getMessage());
         }
-        if (empty($ips) && function_exists('net_get_interfaces')) {
-            foreach (net_get_interfaces() ?: [] as $name => $value) {
-                foreach ($value['unicast'] as $item) {
-                    if (! isset($item['address'])) {
-                        continue;
-                    }
-                    if (! Str::contains($item['address'], '::') && $item['address'] !== '127.0.0.1') {
-                        $ips[$name] = $item['address'];
-                    }
-                }
-            }
-        }
-        if (is_array($ips) && ! empty($ips)) {
-            return current($ips);
-        }
-        /** @var mixed|string $ip */
-        $ip = gethostbyname(gethostname());
-        if (is_string($ip)) {
-            return $ip;
-        }
-        throw new IPReadFailedException('Can not get the internal IP.');
     }
 }
