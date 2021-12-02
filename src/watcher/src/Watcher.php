0a353eee018e2af5ac5edd07154b1a5345f465ac
沈唁 <52o@qq52o.cn>
2021-12-02 09:30:30 +0800

    Fix `collector-reload` file not found when running `server:watch` with absolute path (#4308)
    * Fix Could not open input file: vendor/hyperf/watcher/collector-reload.php
    
    * Update CHANGELOG-2.2.md

--- src/watcher/src/Watcher.php
+++ src/watcher/src/Watcher.php
@@ -124,7 +124,7 @@ class Watcher
                     $this->restart(false);
                 }
             } else {
-                $ret = System::exec($this->option->getBin() . ' vendor/hyperf/watcher/collector-reload.php ' . $file);
+                $ret = System::exec(sprintf('%s %s/vendor/hyperf/watcher/collector-reload.php %s', $this->option->getBin(), BASE_PATH, $file));
                 if ($ret['code'] === 0) {
                     $this->output->writeln('Class reload success.');
                 } else {
