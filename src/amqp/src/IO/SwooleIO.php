854387dd0610b6dcbeb9c49223c910e847d2a31a
李铭昕 <715557344@qq.com>
2021-12-15 15:42:31 +0800

    Optimized code for SwooleIO

--- src/amqp/src/IO/SwooleIO.php
+++ src/amqp/src/IO/SwooleIO.php
@@ -15,8 +15,7 @@ use PhpAmqpLib\Exception\AMQPConnectionClosedException;
 use PhpAmqpLib\Exception\AMQPRuntimeException;
 use PhpAmqpLib\Wire\AMQPWriter;
 use PhpAmqpLib\Wire\IO\AbstractIO;
-use Swoole\Coroutine\Client;
-use const SWOOLE_SOCK_TCP;
+use Swoole\Coroutine\Socket;
 
 class SwooleIO extends AbstractIO
 {
@@ -40,10 +39,15 @@ class SwooleIO extends AbstractIO
     /**
      * @var int
      */
+    protected $readWriteTimeout;
+
+    /**
+     * @var int
+     */
     protected $heartbeat;
 
     /**
-     * @var null|Client
+     * @var null|Socket
      */
     private $sock;
 
@@ -58,11 +62,13 @@ class SwooleIO extends AbstractIO
     public function __construct(
         string $host,
         int $port,
-        int $connectionTimeout
+        int $connectionTimeout,
+        int $readWriteTimeout = 3
     ) {
         $this->host = $host;
         $this->port = $port;
         $this->connectionTimeout = $connectionTimeout;
+        $this->readWriteTimeout = $readWriteTimeout;
     }
 
     /**
@@ -77,34 +83,20 @@ class SwooleIO extends AbstractIO
 
     public function read($len)
     {
-        while (true) {
-            if ($len <= strlen($this->buffer)) {
-                $data = substr($this->buffer, 0, $len);
-                $this->buffer = substr($this->buffer, $len);
-
-                return $data;
-            }
-
-            if (! $this->sock->isConnected()) {
-                throw new AMQPConnectionClosedException('Broken pipe or closed connection. ' . $this->sock->errMsg);
-            }
-
-            $buffer = $this->sock->recv(-1);
-
-            if ($buffer === '') {
-                throw new AMQPConnectionClosedException('Connection is closed. The reason is ' . $this->sock->errMsg);
-            }
-
-            $this->buffer .= $buffer;
+        $data = $this->sock->recvAll($len, $this->readWriteTimeout);
+        if ($data === false || strlen($data) !== $len) {
+            throw new AMQPConnectionClosedException('Read data failed, The reason is ' . $this->sock->errMsg);
         }
+
+        return $data;
     }
 
     public function write($data)
     {
-        $buffer = $this->sock->send($data);
+        $len = $this->sock->sendAll($data, $this->readWriteTimeout);
 
-        if ($buffer === false) {
-            throw new AMQPConnectionClosedException('Error sending data');
+        if ($data === false || strlen($data) !== $len) {
+            throw new AMQPConnectionClosedException('Send data failed, The reason is ' . $this->sock->errMsg);
         }
     }
 
@@ -134,7 +126,7 @@ class SwooleIO extends AbstractIO
 
     protected function makeClient()
     {
-        $sock = new Client(SWOOLE_SOCK_TCP);
+        $sock = new Socket(AF_INET, SOCK_STREAM, 0);
         if (! $sock->connect($this->host, $this->port, $this->connectionTimeout)) {
             throw new AMQPRuntimeException(
                 sprintf('Error Connecting to server: %s ', $sock->errMsg),
