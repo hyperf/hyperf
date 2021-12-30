57eea2e1d9adf0e0b0c5126e32f0f6e79d8997aa
李铭昕 <715557344@qq.com>
2021-12-30 10:57:56 +0800

    Update

--- src/tracer/src/Aspect/DbAspect.php
+++ src/tracer/src/Aspect/DbAspect.php
@@ -5,16 +5,15 @@ declare(strict_types=1);
  * This file is part of Hyperf.
  *
  * @link     https://www.hyperf.io
- * @document https://doc.hyperf.io
+ * @document https://hyperf.wiki
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
 namespace Hyperf\Tracer\Aspect;
 
-use Hyperf\Di\Annotation\Aspect;
+use Hyperf\DB\DB;
 use Hyperf\Di\Aop\AbstractAspect;
 use Hyperf\Di\Aop\ProceedingJoinPoint;
-use Hyperf\DB\DB;
 use Hyperf\Tracer\SpanStarter;
 use Hyperf\Tracer\SpanTagManager;
 use Hyperf\Tracer\SwitchManager;
