b583d52f5ee97d37274c84ac113e1dab73c52e45
李铭昕 <715557344@qq.com>
2022-02-01 12:53:26 +0800

    Added `Hyperf\Coordinator\Timer`.

---
+++ src/coordinator/src/Timer.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace Hyperf\Coordinator;
+
+use Closure;
+use Hyperf\Contract\StdoutLoggerInterface;
+
+class Timer
+{
+    public const STOP = 'stop';
+
+    private array $closures = [];
+
+    private int $id = 0;
+
+    public function __construct(private StdoutLoggerInterface $logger)
+    {
+    }
+
+    public function after(float $timeout, Closure $closure, string $identifier = Constants::WORKER_EXIT): int
+    {
+        $id = ++$this->id;
+        $this->closures[$id] = true;
+        go(static function () use ($timeout, $closure, $identifier, $id) {
+            try {
+                $isClosing = CoordinatorManager::until($identifier)->yield($timeout);
+                if (isset($this->closures[$id])) {
+                    $closure($isClosing);
+                }
+            } finally {
+                unset($this->closures[$id]);
+            }
+        });
+        return $id;
+    }
+
+    public function tick(float $timeout, Closure $closure, string $identifier = Constants::WORKER_EXIT)
+    {
+        $id = ++$this->id;
+        $this->closures[$id] = true;
+        go(static function () use ($timeout, $closure, $identifier, $id) {
+            try {
+                while (true) {
+                    $isClosing = CoordinatorManager::until($identifier)->yield($timeout);
+                    if (! isset($this->closures[$id])) {
+                        break;
+                    }
+
+                    try {
+                        $result = $closure($isClosing);
+                        if ($result === self::STOP) {
+                            break;
+                        }
+                    } catch (\Throwable $exception) {
+                        $this->logger->error((string) $exception);
+                    }
+                }
+            } finally {
+                unset($this->closures[$id]);
+            }
+        });
+        return $id;
+    }
+
+    public function clear(int $id)
+    {
+        unset($this->closures[$id]);
+    }
+
+    public function clearAll()
+    {
+        $this->closures = [];
+    }
+}
