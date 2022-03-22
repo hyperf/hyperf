9fdefb011afd64808cb784472cd4a21154dd7cc9
D.J.Hwang <huangdijia@gmail.com>
2022-03-22 09:06:40 +0800

    Add monolog hook for is_resource()

---
+++ src/logger/src/Adapter/UdpSocketHook.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace Monolog\Handler\SyslogUdp;
+
+    function is_resource(mixed $value): bool
+    {
+        if (\is_resource($value)) {
+            return true;
+        }
+
+        if ($value instanceof \Swoole\Coroutine\Socket) {
+            return true;
+        }
+
+        return false;
+    }
