bc399dcabc5f9ce9d2ecc12ccce6c0cbcd4af7af
D.J.Hwang <huangdijia@gmail.com>
2022-03-22 09:59:55 +0800

    Add tests

---
+++ src/logger/tests/HookTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace HyperfTest\Logger;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+class HookTest extends TestCase
+{
+    public function testUdpSocketHook()
+    {
+        $socket = socket_create(AF_UNIX, SOCK_DGRAM, 0);
+
+        $this->assertTrue(\Monolog\Handler\SyslogUdp\is_resource($socket));
+    }
+}
