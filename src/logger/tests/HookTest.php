b5dd0e30799ec58ae24b3d2a1a8c3bc92afe37d5
D.J.Hwang <huangdijia@gmail.com>
2022-03-23 14:50:48 +0800

    recover tests

---
+++ src/logger/tests/HookTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace ;
+
+class HookTest
+{
+    
+}@@ -1,39 +0,0 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace HyperfTest\Logger;
+
+use Mockery;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+class HookTest extends TestCase
+{
+    /**
+     * @group NonCoroutine
+     * @covers \Monolog\Handler\SyslogUdp\is_resource
+     */
+    public function testUdpSocketHook()
+    {
+        run(function () {
+            // $socket = Mockery::mock(\Swoole\Coroutine\Socket::class);
+            $socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+
+            $this->assertTrue(\Monolog\Handler\SyslogUdp\is_resource($socket));
+            $this->assertFalse(\is_resource($socket));
+
+            socket_close($socket);
+        });
+    }
+}
