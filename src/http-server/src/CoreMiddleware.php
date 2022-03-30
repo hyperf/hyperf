2648d160d964ddceadf6dce42f794a051c2565cd
李铭昕 <715557344@qq.com>
2022-03-30 13:25:40 +0800

    Update CoreMiddleware.php

--- src/http-server/src/CoreMiddleware.php
+++ src/http-server/src/CoreMiddleware.php
@@ -18,6 +18,7 @@ use Hyperf\Contract\Arrayable;
 use Hyperf\Contract\NormalizerInterface;
 use Hyperf\Di\ClosureDefinitionCollectorInterface;
 use Hyperf\Di\MethodDefinitionCollectorInterface;
+use Hyperf\Di\ReflectionType;
 use Hyperf\HttpMessage\Exception\MethodNotAllowedHttpException;
 use Hyperf\HttpMessage\Exception\NotFoundHttpException;
 use Hyperf\HttpMessage\Exception\ServerErrorHttpException;
@@ -230,7 +231,7 @@ class CoreMiddleware implements CoreMiddlewareInterface
     }
 
     /**
-     * @param \Hyperf\Di\ReflectionType[] $definitions
+     * @param ReflectionType[] $definitions
      */
     private function getInjections(array $definitions, string $callableName, array $arguments): array
     {
