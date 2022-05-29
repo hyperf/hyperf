3ef67af77415ef9472f4aedbfa638f7dcf779860
李铭昕 <715557344@qq.com>
2022-05-29 16:35:53 +0800

    Fixed

--- src/event/src/ListenerProvider.php
+++ src/event/src/ListenerProvider.php
@@ -23,7 +23,7 @@ class ListenerProvider implements ListenerProviderInterface
 
     /**
      * @param object $event An event for which to return the relevant listeners
-     * @return iterable[callable] An iterable (array, iterator, or generator) of callables.  Each
+     * @return iterable<callable> An iterable (array, iterator, or generator) of callables.  Each
      *                            callable MUST be type-compatible with $event.
      */
     public function getListenersForEvent($event): iterable
