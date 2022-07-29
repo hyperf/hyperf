9304ce11a3e64963b2361cb3357a65e4a2bd0d7a
李铭昕 <715557344@qq.com>
2022-07-29 12:01:52 +0800

    Update DatabaseMigratorIntegrationTest.php

--- src/database/tests/DatabaseMigratorIntegrationTest.php
+++ src/database/tests/DatabaseMigratorIntegrationTest.php
@@ -103,18 +103,17 @@ class DatabaseMigratorIntegrationTest extends TestCase
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Users Table'",
             "CREATE TABLE `users` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `remember_token` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb3_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_email_unique` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci COMMENT='Users Table'",
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci COMMENT='Users Table'",
         ];
 
-        echo $sql;
         $this->assertTrue(in_array($sql, $asserts, true));
 
         $res = (array) $schema->connection()->selectOne('SHOW CREATE TABLE password_resets;');
