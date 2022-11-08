9a606e22ea73f3727e15d96c9b1fba9fd99a841b
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2022-11-09 07:49:40 +0800

    Fix cs-fixer on PHP8.2

--- .travis/run.test.sh
+++ .travis/run.test.sh
@@ -3,4 +3,4 @@ composer analyse src
 php -dswoole.use_shortname='Off' bin/co-phpunit --exclude-group NonCoroutine
 php -dswoole.use_shortname='Off' vendor/bin/phpunit --group NonCoroutine
 php -dswoole.use_shortname='Off' vendor/bin/phpunit src/filesystem --group NonCoroutine
-vendor/bin/php-cs-fixer fix src --dry-run
+PHP_CS_FIXER_IGNORE_ENV=1 vendor/bin/php-cs-fixer fix src --dry-run
