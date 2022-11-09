87500f7772c93737b912c985a3ad7458d6ee1413
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2022-11-09 08:06:15 +0800

    Sort actions

--- .travis/run.test.sh
+++ .travis/run.test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-composer analyse src
+# composer analyse src
 php -dswoole.use_shortname='Off' bin/co-phpunit --exclude-group NonCoroutine
 php -dswoole.use_shortname='Off' vendor/bin/phpunit --group NonCoroutine
 php -dswoole.use_shortname='Off' vendor/bin/phpunit src/filesystem --group NonCoroutine
-PHP_CS_FIXER_IGNORE_ENV=1 vendor/bin/php-cs-fixer fix src --dry-run
+# vendor/bin/php-cs-fixer fix src --dry-run
