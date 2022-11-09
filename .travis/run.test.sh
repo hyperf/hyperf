b22fcd1cccc7dc1d1a065b2ba18c9f1ec4acd0fc
Deeka Wong <huangdijia@gmail.com>
2022-11-09 10:01:40 +0800

    Optimize the order of github actions (#5184)
    Co-authored-by: 李铭昕 <715557344@qq.com>

--- .travis/run.test.sh
+++ .travis/run.test.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-composer analyse src
 php -dswoole.use_shortname='Off' bin/co-phpunit --exclude-group NonCoroutine
 php -dswoole.use_shortname='Off' vendor/bin/phpunit --group NonCoroutine
 php -dswoole.use_shortname='Off' vendor/bin/phpunit src/filesystem --group NonCoroutine
-vendor/bin/php-cs-fixer fix src --dry-run
