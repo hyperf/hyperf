73714b1ae08fa2c00a61295aeb24e405c2edcb49
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2022-11-11 10:02:03 +0800

    Optimize

--- src/watcher/watcher.php
+++ src/watcher/watcher.php
@@ -15,14 +15,14 @@ $dir = dirname(__DIR__, 2);
 $cwd = getcwd();
 
 switch (true) {
+    case file_exists($cwd . '/bin/hyperf.php'):
+        require_once $cwd . '/bin/hyperf.php';
+        break;
     case file_exists($dir . '/bin/hyperf.php'):
         require_once $dir . '/bin/hyperf.php';
         break;
     case file_exists($dir . '/../bin/hyperf.php'):
         require_once $dir . '/../bin/hyperf.php';
-        break;
-    case file_exists($cwd . '/bin/hyperf.php'):
-        require_once $cwd . '/bin/hyperf.php';
         break;
     default:
         require_once 'bin/hyperf.php';
