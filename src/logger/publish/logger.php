ad0af2383bab1c272d898ead994b0f63e70a6001
luxin <luxin88@users.noreply.github.com>
2022-11-23 09:01:57 +0800

    Logger::DEBUG is deprecated, use Level::Debug

--- src/logger/publish/logger.php
+++ src/logger/publish/logger.php
@@ -16,7 +16,7 @@ return [
                 'class' => Monolog\Handler\StreamHandler::class,
                 'constructor' => [
                     'stream' => BASE_PATH . '/runtime/logs/hyperf.log',
-                    'level' => Monolog\Logger::DEBUG,
+                    'level' => Monolog\Level::Debug,
                 ],
                 'formatter' => [
                     'class' => Monolog\Formatter\LineFormatter::class,
