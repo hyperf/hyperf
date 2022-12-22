3c52356428b08273584e839b13b711167da01605
李铭昕 <715557344@qq.com>
2022-12-22 16:09:08 +0800

    Added test cases.

---
+++ src/rpc-client/tests/AstTest.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace HyperfTest\RpcClient;
+
+use Hyperf\RpcClient\Proxy\Ast;
+use HyperfTest\RpcClient\Stub\FooInterface;
+use HyperfTest\RpcClient\Stub\ParInterface;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+class AstTest extends TestCase
+{
+    protected $license = '<?php
+
+declare (strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */';
+
+    public function testAstGenerate()
+    {
+        $ast = new Ast();
+
+        $code = $ast->proxy(ParInterface::class, 'ParClient');
+
+        $this->assertSame($this->license . '
+namespace HyperfTest\RpcClient\Stub;
+
+class ParClient extends \Hyperf\RpcClient\Proxy\AbstractProxyService implements ParInterface
+{
+    public function getName() : string
+    {
+        return $this->client->__call(__FUNCTION__, func_get_args());
+    }
+}', $code);
+
+        $code = $ast->proxy(FooInterface::class, 'FooClient');
+
+        $this->assertSame($this->license . '
+namespace HyperfTest\RpcClient\Stub;
+
+class FooClient extends \Hyperf\RpcClient\Proxy\AbstractProxyService implements FooInterface
+{
+    public function getId() : int
+    {
+        return $this->client->__call(__FUNCTION__, func_get_args());
+    }
+    public function getName() : string
+    {
+        return $this->client->__call(__FUNCTION__, func_get_args());
+    }
+}', $code);
+    }
+}
