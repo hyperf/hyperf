33895c366d203654a8c2b93438bb0195d153e095
李铭昕 <715557344@qq.com>
2023-01-01 13:09:19 +0800

    Update AstTest.php

--- src/di/tests/AstTest.php
+++ src/di/tests/AstTest.php
@@ -148,6 +148,31 @@ abstract class Abs
     }
     public abstract function absabs() : string;
 }", $code);
+
+        $code = $ast->proxy(Chi::class);
+        $this->assertSame($this->license .'
+namespace HyperfTest\Di\Stub\Ast;
+
+class Chi extends Abs
+{
+    use \Hyperf\Di\Aop\ProxyTrait;
+    use \Hyperf\Di\Aop\PropertyHandlerTrait;
+    function __construct()
+    {
+        if (method_exists(parent::class, \'__construct\')) {
+            parent::__construct(...func_get_args());
+        }
+        $this->__handlePropertyHandler(__CLASS__);
+    }
+    public function absabs() : string
+    {
+        $__function__ = __FUNCTION__;
+        $__method__ = __METHOD__;
+        return self::__proxyCall(__CLASS__, __FUNCTION__, self::__getParamsMap(__CLASS__, __FUNCTION__, func_get_args()), function () use($__function__, $__method__) {
+            return \'chi\';
+        });
+    }
+}', $code);
     }
 
     public function testParentMethods()
