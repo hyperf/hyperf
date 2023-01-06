68648c70a07fe7c2e9e0b942edf173ca4523516d
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-01-06 11:39:47 +0800

    Create .phpstorm.meta.php

---
+++ .phpstorm.meta.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace PHPSTORM_META {
+    // Reflect
+    override(\Psr\Container\ContainerInterface::get(0), map(['' => '@']));
+    override(\Hyperf\Context\Context::get(0), map(['' => '@']));
+    override(\make(0), map(['' => '@']));
+    override(\di(0), map(['' => '@']));
+    override(\optional(0), type(0));
+    override(\tap(0), type(0));
+}
