1e616b656fa7e256aaa9be0993b124ec007b812c
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-01-06 11:56:48 +0800

    Update .phpstorm.meta.php

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -1,11 +1,20 @@
 <?php
 
-namespace PHPSTORM_META {
-    // Reflect
-    override(\Psr\Container\ContainerInterface::get(0), map(['' => '@']));
-    override(\Hyperf\Context\Context::get(0), map(['' => '@']));
-    override(\make(0), map(['' => '@']));
-    override(\di(0), map(['' => '@']));
-    override(\optional(0), type(0));
-    override(\tap(0), type(0));
-}
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace PHPSTORM_META;
+
+// Reflect
+override(\Psr\Container\ContainerInterface::get(0), map(['' => '@']));
+override(\Hyperf\Context\Context::get(0), map(['' => '@']));
+override(\make(0), map(['' => '@']));
+override(\di(0), map(['' => '@']));
+override(\optional(0), type(0));
+override(\tap(0), type(0));
