9ba31deccf7f99ab196ba5982bfb431170991dd2
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-01-10 08:21:50 +0800

    Optimize phpdoc

--- src/metric/src/Listener/DBPoolWatcher.php
+++ src/metric/src/Listener/DBPoolWatcher.php
@@ -29,6 +29,7 @@ class DBPoolWatcher extends PoolWatcher implements ListenerInterface
 
     /**
      * Periodically scan metrics.
+     * {@inheritdoc}
      */
     public function process(object $event): void
     {
