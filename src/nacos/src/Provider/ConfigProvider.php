ce4c9200899a7ad7a23bf950ec690acd80f4c452
李铭昕 <715557344@qq.com>
2023-01-12 22:20:45 +0800

    Update ConfigProvider.php

--- src/nacos/src/Provider/ConfigProvider.php
+++ src/nacos/src/Provider/ConfigProvider.php
@@ -17,6 +17,10 @@ use Psr\Http\Message\ResponseInterface;
 
 class ConfigProvider extends AbstractProvider
 {
+    public const WORD_SEPARATOR = "\x02";
+
+    public const LINE_SEPARATOR = "\x01";
+
     public function get(string $dataId, string $group, ?string $tenant = null): ResponseInterface
     {
         return $this->request('GET', 'nacos/v1/cs/configs', [
@@ -51,35 +55,28 @@ class ConfigProvider extends AbstractProvider
             ]),
         ]);
     }
-    
-    public const WORD_SEPARATOR = "\x02";
-
-    public const LINE_SEPARATOR = "\x01";
 
     /**
-     * @param array $options =  ['dataId' => $dataId,
-     *                          'group' => $group,
-     *                          'contentMD5' => md5(file_get_contents($configPath)),
-     *                          'tenant' => $tenant]
-     * @return ResponseInterface
-     * @Time 2023/1/12 11:27
-     * @author sunsgne
+     * @param $options = [
+     *     'dataId' => '',
+     *     'group' => '',
+     *     'contentMD5' => md5(file_get_contents($configPath)),
+     *     'tenant' => '',
+     * ]
      */
     public function listener(array $options = []): ResponseInterface
     {
-
-        $ListeningConfigs = ($options['dataId'] ?? null) . self::WORD_SEPARATOR .
+        $config = ($options['dataId'] ?? null) . self::WORD_SEPARATOR .
             ($options['group'] ?? null) . self::WORD_SEPARATOR .
             ($options['contentMD5'] ?? null) . self::WORD_SEPARATOR .
             ($options['tenant'] ?? null) . self::LINE_SEPARATOR;
         return $this->request('POST', 'nacos/v1/cs/configs/listener', [
-            RequestOptions::QUERY   => [
-                'Listening-Configs' => $ListeningConfigs,
+            RequestOptions::QUERY => [
+                'Listening-Configs' => $config,
             ],
             RequestOptions::HEADERS => [
                 'Long-Pulling-Timeout' => 30,
             ],
         ]);
-
     }
 }
