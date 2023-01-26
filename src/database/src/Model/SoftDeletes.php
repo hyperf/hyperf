3bdec6237504b8b39d71cb247f458df83e5a2cde
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-01-26 17:57:43 +0800

    Sort

--- src/database/src/Model/SoftDeletes.php
+++ src/database/src/Model/SoftDeletes.php
@@ -14,8 +14,8 @@ namespace Hyperf\Database\Model;
 use Psr\EventDispatcher\StoppableEventInterface;
 
 /**
- * @method static static|\Hyperf\Database\Model\Builder|\Hyperf\Database\Query\Builder withTrashed(bool $withTrashed = true)
  * @method static static|\Hyperf\Database\Model\Builder|\Hyperf\Database\Query\Builder restoreOrCreate(\Hyperf\Database\Query\Builder $builder, array $attributes = [], array $values = [])
+ * @method static static|\Hyperf\Database\Model\Builder|\Hyperf\Database\Query\Builder withTrashed(bool $withTrashed = true)
  * @method static static|\Hyperf\Database\Model\Builder|\Hyperf\Database\Query\Builder onlyTrashed()
  * @method static static|\Hyperf\Database\Model\Builder|\Hyperf\Database\Query\Builder withoutTrashed()
  */
