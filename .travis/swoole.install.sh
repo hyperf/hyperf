26fc2c69228c047062f306b4e8b19827a96fbd4f
李铭昕 <715557344@qq.com>
2023-02-13 20:23:22 +0800

    Update swoole.install.sh

--- .travis/swoole.install.sh
+++ .travis/swoole.install.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 sudo apt-get update
-sudo apt-get install libcurl4-openssl-dev
+sudo apt-get install libcurl4-openssl-dev libpq-dev
 wget https://github.com/swoole/swoole-src/archive/${SW_VERSION}.tar.gz -O swoole.tar.gz
 mkdir -p swoole
 tar -xf swoole.tar.gz -C swoole --strip-components=1
 rm swoole.tar.gz
 cd swoole
 phpize
-./configure --enable-openssl --enable-http2 --enable-swoole-curl --enable-swoole-json
+./configure --enable-openssl --enable-http2 --enable-swoole-curl --enable-swoole-json --enable-swoole-pgsql
 make -j$(nproc)
 sudo make install
 sudo sh -c "echo extension=swoole > /etc/php/${PHP_VERSION}/cli/conf.d/swoole.ini"
