aeaf500517ab37e8766d7f9a7954c9a8b1f62423
她和她的猫 <hxhsoft@foxmail.com>
2023-02-16 13:58:21 +0800

    Fixed bug that database-pgsql does not support migration. (#5417)

---
+++ src/database-pgsql/tests/migrations/one/2016_01_01_100000_create_password_resets_for_pgsql_table.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+use Hyperf\Database\Migrations\Migration;
+use Hyperf\Database\Schema\Blueprint;
+use Hyperf\Database\Schema\Schema;
+
+class CreatePasswordResetsForPgsqlTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('password_resets_for_pgsql', function (Blueprint $table) {
+            $table->string('email')->index();
+            $table->string('token')->index();
+            $table->timestamp('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::dropIfExists('password_resets_for_pgsql');
+    }
+}
