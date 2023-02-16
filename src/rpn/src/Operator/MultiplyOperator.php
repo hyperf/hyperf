55b3c4677c15a25e88affbe37def4b06d1e09752
李铭昕 <715557344@qq.com>
2023-02-16 12:34:22 +0800

    Optimized code for rpn. (#5419)

--- src/rpn/src/Operator/MultiplyOperator.php
+++ src/rpn/src/Operator/MultiplyOperator.php
@@ -18,8 +18,9 @@ class MultiplyOperator extends Operator
         return '*';
     }
 
-    public function execute(array $parameters, int $scale): string
+    public function execute(array $parameters, int $scale, array $bindings = []): string
     {
+        $parameters = $this->fromBindings($parameters, $bindings);
         $parameters[] = $scale;
         return bcmul(...$parameters);
     }
