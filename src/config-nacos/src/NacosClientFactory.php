bff8c053a15f157e824cf29d4b79fd296835ed79
Yi <uyijin@gmail.com>
2023-02-18 06:07:42 +0800

    Added `access_key` and `access_secret` which used to connect aliyun nacos. (#5429)

--- src/config-nacos/src/NacosClientFactory.php
+++ src/config-nacos/src/NacosClientFactory.php
@@ -35,6 +35,8 @@ class NacosClientFactory
             'base_uri' => $baseUri,
             'username' => $config['username'] ?? null,
             'password' => $config['password'] ?? null,
+            'access_key' => $config['access_key'] ?? null,
+            'access_secret' => $config['access_secret'] ?? null,
             'guzzle_config' => $config['guzzle']['config'] ?? null,
         ]));
     }
