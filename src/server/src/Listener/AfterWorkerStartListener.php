12681954e594d4fcabe21c47863f1feb1ec60426
李铭昕 <715557344@qq.com>
2023-02-21 13:52:36 +0800

    Update

--- src/server/src/Listener/AfterWorkerStartListener.php
+++ src/server/src/Listener/AfterWorkerStartListener.php
@@ -20,16 +20,6 @@ use Hyperf\Server\ServerManager;
 use Psr\Log\LoggerInterface;
 use Swoole\Server\Port;
 
-define('SWOOLE_SOCK_TCP', 1);
-define('SWOOLE_SOCK_TCP6', 3);
-define('SWOOLE_SOCK_UDP', 2);
-define('SWOOLE_SOCK_UDP6', 4);
-
-define('SWOW_SOCK_TCP', 16777233);
-define('SWOW_SOCK_TCP6', 16777297);
-define('SWOW_SOCK_UDP', 134217746);
-define('SWOW_SOCK_UDP6', 134217810);
-
 class AfterWorkerStartListener implements ListenerInterface
 {
     private LoggerInterface $logger;
@@ -67,10 +57,10 @@ class AfterWorkerStartListener implements ListenerInterface
 
                         case ServerInterface::SERVER_BASE:
                             $sockType = $server->type;
-                            if (in_array($sockType, [SWOOLE_SOCK_TCP, SWOOLE_SOCK_TCP6, SWOW_SOCK_TCP, SWOW_SOCK_TCP6])) {
+                            if (in_array($sockType, [SWOOLE_SOCK_TCP, SWOOLE_SOCK_TCP6])) {
                                 return 'TCP';
                             }
-                            if (in_array($sockType, [SWOOLE_SOCK_UDP, SWOOLE_SOCK_UDP6, SWOW_SOCK_UDP, SWOW_SOCK_UDP6])) { 
+                            if (in_array($sockType, [SWOOLE_SOCK_UDP, SWOOLE_SOCK_UDP6])) {
                                 return 'UDP';
                             }
 
