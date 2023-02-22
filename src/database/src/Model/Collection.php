14b8147c55a60643c8c2e08f72d605bd882b3009
李铭昕 <715557344@qq.com>
2023-02-23 06:41:11 +0800

    Update

--- src/database/src/Model/Collection.php
+++ src/database/src/Model/Collection.php
@@ -422,11 +422,11 @@ class Collection extends BaseCollection implements CompressInterface
     {
         return $this->each->makeVisible($attributes);
     }
-    
+
     /**
      * Append an attribute across the entire collection.
      *
-     * @param  array|string  $attributes
+     * @param array|string $attributes
      * @return $this
      */
     public function append($attributes)
