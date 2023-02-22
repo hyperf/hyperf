0ac1d5e807956eefbdced8a19902f134fb932752
李铭昕 <715557344@qq.com>
2023-02-22 10:10:58 +0800

    Fixed ci (#5440)

--- src/devtool/src/Describe/RoutesCommand.php
+++ src/devtool/src/Describe/RoutesCommand.php
@@ -84,10 +84,8 @@ class RoutesCommand extends HyperfCommand
             $action = $handler->callback[0] . '::' . $handler->callback[1];
         } elseif (is_string($handler->callback)) {
             $action = $handler->callback;
-        } elseif (is_callable($handler->callback)) {
-            $action = 'Closure';
         } else {
-            $action = (string) $handler->callback;
+            $action = 'Closure';
         }
         $unique = "{$serverName}|{$uri}|{$action}";
         if (isset($data[$unique])) {
