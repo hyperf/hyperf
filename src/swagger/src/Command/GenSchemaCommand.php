fceb2707578d90ce90b29e226fad6be60b264116
李铭昕 <715557344@qq.com>
2023-02-22 17:37:36 +0800

    Support generate swagger schemas by models.

--- src/swagger/src/Command/GenSchemaCommand.php
+++ src/swagger/src/Command/GenSchemaCommand.php
@@ -12,7 +12,14 @@ declare(strict_types=1);
 namespace Hyperf\Swagger\Command;
 
 use Hyperf\Command\Command as HyperfCommand;
+use Hyperf\Database\Model\Model;
+use Hyperf\Swagger\Command\Ast\ModelSchemaVisitor;
+use PhpParser\Lexer\Emulative;
+use PhpParser\NodeTraverser;
+use PhpParser\ParserFactory;
+use PhpParser\PrettyPrinter\Standard;
 use Psr\Container\ContainerInterface;
+use ReflectionClass;
 use Symfony\Component\Console\Input\InputOption;
 
 class GenSchemaCommand extends HyperfCommand
@@ -28,16 +35,37 @@ class GenSchemaCommand extends HyperfCommand
         $this->setDescription('Generate swagger schemas.');
         $this->addOption('name', 'N', InputOption::VALUE_OPTIONAL, 'The schema name.');
         $this->addOption('force', 'f', InputOption::VALUE_NONE, 'Whether to force generate the schema.');
+        $this->addOption('model', 'M', InputOption::VALUE_OPTIONAL, 'The model which used to generate schemas.');
     }
 
     public function handle()
     {
         $name = $this->input->getOption('name');
         $force = $this->input->getOption('force');
+        $model = $this->input->getOption('model');
+
+        if ($model) {
+            if (! class_exists($model)) {
+                $this->output->error(sprintf('The model %s is not exists.', $model));
+                return;
+            }
+            $ref = new ReflectionClass($model);
+            /** @var Model $model */
+            $model = new $model();
+        }
+
+        if (! $name) {
+            if (! $model) {
+                $this->output->error('The one of name or model must be exists.');
+                return;
+            }
+
+            $name = $ref->getShortName() . 'Schema';
+        }
 
         $path = BASE_PATH . '/app/Schema/' . $name . '.php';
         if (file_exists($path) && ! $force) {
-            $this->output->error('The file is exists.');
+            $this->output->error(sprintf('The path of schema %s is exists.', $path));
             return;
         }
 
@@ -45,6 +73,27 @@ class GenSchemaCommand extends HyperfCommand
 
         $code = str_replace('%NAME', $name, $stub);
 
-        file_put_contents($path, $code);
+        if (! $model) {
+            file_put_contents($path, $code);
+            return;
+        }
+
+        $lexer = new Emulative([
+            'usedAttributes' => [
+                'comments',
+                'startLine', 'endLine',
+                'startTokenPos', 'endTokenPos',
+            ],
+        ]);
+        $parser = (new ParserFactory())->create(ParserFactory::ONLY_PHP7, $lexer);
+        $printer = new Standard();
+
+        $traverser = new NodeTraverser();
+
+        $traverser->addVisitor(new ModelSchemaVisitor($ref, $model));
+
+        $stmts = $traverser->traverse($parser->parse($code));
+
+        file_put_contents($path, '<?php' . PHP_EOL . $printer->prettyPrint($stmts));
     }
 }
