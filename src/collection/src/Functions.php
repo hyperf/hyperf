fba0dfb0b7c8b6e1abca72cbbf00aba7b71d8817
李铭昕 <715557344@qq.com>
2023-03-28 18:38:22 +0800

    change license.

--- src/collection/src/Functions.php
+++ src/collection/src/Functions.php
@@ -177,11 +177,10 @@ function last($array)
 /**
  * Return the default value of the given value.
  *
- * @param mixed $value
  * @param mixed ...$args
  * @return mixed
  */
-function value($value, ...$args)
+function value(mixed $value, ...$args)
 {
     return $value instanceof Closure ? $value(...$args) : $value;
 }
