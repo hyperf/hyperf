4950f209033fbefc0f91de16479a90654773bd7c
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-04-11 07:43:25 +0800

    Adds helper functions

---
+++ src/coroutinue/src/Functions.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Hyperf\Coroutine;
+
+use Closure;
+use Hyperf\Context\ApplicationContext;
+
+/**
+ * @param callable[] $callables
+ * @param int $concurrent if $concurrent is equal to 0, that means unlimited
+ */
+function parallel(array $callables, int $concurrent = 0)
+{
+    $parallel = new Parallel($concurrent);
+    foreach ($callables as $key => $callable) {
+        $parallel->add($callable, $key);
+    }
+    return $parallel->wait();
+}
+
+function wait(Closure $closure, ?float $timeout = null)
+{
+    if (ApplicationContext::hasContainer()) {
+        $waiter = ApplicationContext::getContainer()->get(Waiter::class);
+        return $waiter->wait($closure, $timeout);
+    }
+    return (new Waiter())->wait($closure, $timeout);
+}
+
+/**
+ * @return bool|int
+ */
+function co(callable $callable)
+{
+    $id = Coroutine::create($callable);
+    return $id > 0 ? $id : false;
+}
+
+function defer(callable $callable): void
+{
+    Coroutine::defer($callable);
+}
+
+/**
+ * @return bool|int
+ */
+function go(callable $callable)
+{
+    $id = Coroutine::create($callable);
+    return $id > 0 ? $id : false;
+}
