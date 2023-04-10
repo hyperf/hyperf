25b85ac35f97448b9bf143caf1f965c9c13c831c
李铭昕 <715557344@qq.com>
2023-04-10 10:12:10 +0800

    Revert "Removed the default header `Server: Hyperf` from response. (#5614)"
    This reverts commit 90b70be1d8fc651fa71d9bba2a1c43a226117267.

--- src/http-server/src/CoreMiddleware.php
+++ src/http-server/src/CoreMiddleware.php
@@ -96,7 +96,7 @@ class CoreMiddleware implements CoreMiddlewareInterface
         if (! $response instanceof ResponseInterface) {
             $response = $this->transferToResponse($response, $request);
         }
-        return $response;
+        return $response->withAddedHeader('Server', 'Hyperf');
     }
 
     public function getMethodDefinitionCollector(): MethodDefinitionCollectorInterface
