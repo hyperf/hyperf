83b1efb41f4ae78f64078c978d2ec28712e797c9
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-04-12 07:41:07 +0800

    ⚙️chore(tests) add CoroutineProxyTest for Context\Traits

---
+++ src/context/tests/Traits/CoroutineProxyTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace HyperfTest\Context\Traits;
+
+use Hyperf\Context\Context;
+use Hyperf\Context\Traits\CoroutineProxy;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+class CoroutineProxyTest extends TestCase
+{
+    public function testCoroutineProxy()
+    {
+        Context::set('bar', new Bar());
+        $foo = new Foo();
+        $this->assertSame('bar', $foo->callBar());
+        $this->assertSame('bar', $foo->bar);
+        $foo->bar = 'foo';
+        $this->assertSame('foo', $foo->bar);
+    }
+}
+
+class Bar
+{
+    public $bar = 'bar';
+
+    public function callBar()
+    {
+        return 'bar';
+    }
+}
+
+class Foo
+{
+    use CoroutineProxy;
+
+    protected $proxyKey = 'bar';
+}
