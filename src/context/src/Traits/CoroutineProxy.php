7e46b65adf46548e543350ced04dd8e57eaf8e39
Deeka Wong <huangdijia@gmail.com>
2023-04-12 09:10:55 +0800

    Split `CoroutineProxy` from `hyperf/utils` to `hyperf/context` (#5622)

---
+++ src/context/src/Traits/CoroutineProxy.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace Hyperf\Context\Traits;
+
+use Hyperf\Context\Context;
+use RuntimeException;
+
+trait CoroutineProxy
+{
+    public function __call($name, $arguments)
+    {
+        $target = $this->getTargetObject();
+        return $target->{$name}(...$arguments);
+    }
+
+    public function __get($name)
+    {
+        $target = $this->getTargetObject();
+        return $target->{$name};
+    }
+
+    public function __set($name, $value)
+    {
+        $target = $this->getTargetObject();
+        return $target->{$name} = $value;
+    }
+
+    protected function getTargetObject()
+    {
+        if (! isset($this->proxyKey)) {
+            throw new RuntimeException(sprintf('Missing $proxyKey property in %s.', $this::class));
+        }
+        return Context::get($this->proxyKey);
+    }
+}
