1612d360da52bbbad594c954235f4f9d6d6e58b8
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-04-24 08:14:32 +0800

    🛠️refactor(utils) deprecate Base62 in favor of Hyperf\Codec\Base62

--- src/utils/src/Codec/Base62.php
+++ src/utils/src/Codec/Base62.php
@@ -11,29 +11,9 @@ declare(strict_types=1);
  */
 namespace Hyperf\Utils\Codec;
 
-class Base62
+/**
+ * @deprecated since 3.1, use Hyperf\Codec\Base62 instead.
+ */
+class Base62 extends \Hyperf\Codec\Base62
 {
-    public const CHARS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-
-    public const BASE = 62;
-
-    public static function encode(int $number): string
-    {
-        $chars = [];
-        while ($number > 0) {
-            $remain = $number % self::BASE;
-            $chars[] = self::CHARS[$remain];
-            $number = ($number - $remain) / self::BASE;
-        }
-        return implode('', array_reverse($chars));
-    }
-
-    public static function decode(string $data): int
-    {
-        return array_reduce(array_map(function ($character) {
-            return strpos(self::CHARS, $character);
-        }, str_split($data)), function ($result, $remain) {
-            return $result * self::BASE + $remain;
-        });
-    }
 }
