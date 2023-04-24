faa2e3c86719a7525e5d7293f0a0efddd0784b7f
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-04-24 08:14:23 +0800

    ⚙️chore(utils): deprecate Json class in favor of Hyperf\Codec\Json

--- src/utils/src/Codec/Json.php
+++ src/utils/src/Codec/Json.php
@@ -11,46 +11,9 @@ declare(strict_types=1);
  */
 namespace Hyperf\Utils\Codec;
 
-use Hyperf\Contract\Arrayable;
-use Hyperf\Contract\Jsonable;
-use Hyperf\Utils\Exception\InvalidArgumentException;
-use Throwable;
-
-class Json
+/**
+ * @deprecated since 3.1, use Hyperf\Codec\Json instead.
+ */
+class Json extends \Hyperf\Codec\Json
 {
-    /**
-     * @throws InvalidArgumentException
-     */
-    public static function encode(mixed $data, int $flags = JSON_UNESCAPED_UNICODE, int $depth = 512): string
-    {
-        if ($data instanceof Jsonable) {
-            return (string) $data;
-        }
-
-        if ($data instanceof Arrayable) {
-            $data = $data->toArray();
-        }
-
-        try {
-            $json = json_encode($data, $flags | JSON_THROW_ON_ERROR, $depth);
-        } catch (Throwable $exception) {
-            throw new InvalidArgumentException($exception->getMessage(), (int) $exception->getCode(), $exception);
-        }
-
-        return $json;
-    }
-
-    /**
-     * @throws InvalidArgumentException
-     */
-    public static function decode(string $json, bool $assoc = true, int $depth = 512, int $flags = 0): mixed
-    {
-        try {
-            $decode = json_decode($json, $assoc, $depth, $flags | JSON_THROW_ON_ERROR);
-        } catch (Throwable $exception) {
-            throw new InvalidArgumentException($exception->getMessage(), (int) $exception->getCode(), $exception);
-        }
-
-        return $decode;
-    }
 }
