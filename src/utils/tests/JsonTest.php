2280b3cee119095ed422c375fbf1e17437454f66
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-04-24 09:08:47 +0800

    🛠️refactor(utils) update JsonTest to use Hyperf\Codec\Exception\InvalidArgumentException

--- src/utils/tests/JsonTest.php
+++ src/utils/tests/JsonTest.php
@@ -12,7 +12,7 @@ declare(strict_types=1);
 namespace HyperfTest\Utils;
 
 use Hyperf\Utils\Codec\Json;
-use Hyperf\Utils\Exception\InvalidArgumentException;
+use Hyperf\Codec\Exception\InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 
 use function Hyperf\Coroutine\go;
