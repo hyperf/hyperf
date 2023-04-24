b11847b9686ed32de11a5930dcc6cdda528484df
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-04-24 13:22:48 +0800

    Create ConfigProvider.php

---
+++ src/serializer/src/ConfigProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace Hyperf\Serializer;
+
+use Hyperf\Contract\NormalizerInterface;
+use Hyperf\Serializer\SerializerFactory;
+use Hyperf\Serializer\SimpleNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+class ConfigProvider
+{
+    public function __invoke()
+    {
+        return [
+            'dependencies' => [
+                Serializer::class => SerializerFactory::class,
+                NormalizerInterface::class => SimpleNormalizer::class,
+            ],
+        ];
+    }
+}
