557ef86ada4894466f70543be697a84ea5be8d4f
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-04-25 15:08:30 +0800

    Extends

--- src/utils/src/Backoff.php
+++ src/utils/src/Backoff.php
@@ -11,69 +11,9 @@ declare(strict_types=1);
  */
 namespace Hyperf\Utils;
 
-use InvalidArgumentException;
-
-class Backoff
+/**
+ * @deprecated since 3.1, use \Hyperf\Support\Backoff instead.
+ */
+class Backoff extends \Hyperf\Support\Backoff
 {
-    /**
-     * Max backoff.
-     */
-    private const CAP = 60 * 1000; // 1 minute
-
-    /**
-     * Backoff interval.
-     */
-    private int $currentMs;
-
-    /**
-     * @param int the first backoff in milliseconds
-     */
-    public function __construct(private int $firstMs = 0)
-    {
-        if ($firstMs < 0) {
-            throw new InvalidArgumentException(
-                'first backoff interval must be greater or equal than 0'
-            );
-        }
-
-        if ($firstMs > Backoff::CAP) {
-            throw new InvalidArgumentException(
-                sprintf(
-                    'first backoff interval must be less or equal than %d milliseconds',
-                    self::CAP
-                )
-            );
-        }
-
-        $this->currentMs = $firstMs;
-    }
-
-    /**
-     * Sleep until the next execution.
-     */
-    public function sleep(): void
-    {
-        if ($this->currentMs === 0) {
-            return;
-        }
-
-        usleep($this->currentMs * 1000);
-
-        // update backoff using Decorrelated Jitter
-        // see: https://aws.amazon.com/blogs/architecture/exponential-backoff-and-jitter/
-        $this->currentMs = rand($this->firstMs, $this->currentMs * 3);
-
-        if ($this->currentMs > self::CAP) {
-            $this->currentMs = self::CAP;
-        }
-    }
-
-    /**
-     * Get the next backoff for logging, etc.
-     * @return int next backoff
-     */
-    public function nextBackoff(): int
-    {
-        return $this->currentMs;
-    }
 }
