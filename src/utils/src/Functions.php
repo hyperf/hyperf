08b0d4d4f7448a61dfdb3aa74883626209cf2704
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-04-26 10:05:15 +0800

    🚀feat(utils) add class alias for FileNotFoundException

--- src/utils/src/Functions.php
+++ src/utils/src/Functions.php
@@ -532,3 +532,4 @@ if (! function_exists('wait')) {
 
 // TODO: Removed when deleting `\Hyperf\Utils\Collection`.
 class_exists(\Hyperf\Utils\Collection::class);
+class_exists(\Hyperf\Utils\Filesystem\FileNotFoundException::class);
