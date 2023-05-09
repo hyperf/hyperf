c65de33c1a6df458c481311ae50afc12fbf694e0
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-05-09 08:34:43 +0800

    Adds `Hyperf\Config\config()`

--- src/config/src/Functions.php
+++ src/config/src/Functions.php
@@ -9,19 +9,38 @@ declare(strict_types=1);
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
-use Hyperf\Context\ApplicationContext;
-use Hyperf\Contract\ConfigInterface;
+namespace Hyperf\Config {
+    use Hyperf\Context\ApplicationContext;
+    use Hyperf\Contract\ConfigInterface;
 
-if (! function_exists('config')) {
+    /**
+     * @param mixed $default
+     */
     function config(string $key, $default = null)
     {
         if (! ApplicationContext::hasContainer()) {
             throw new \RuntimeException('The application context lacks the container.');
         }
+
         $container = ApplicationContext::getContainer();
+
         if (! $container->has(ConfigInterface::class)) {
             throw new \RuntimeException('ConfigInterface is missing in container.');
         }
+
         return $container->get(ConfigInterface::class)->get($key, $default);
+    }
+}
+
+namespace {
+    if (! function_exists('config')) {
+        /**
+         * @deprecated since v3.1, use `Hyperf\Config\config()` instead.
+         * @param null|mixed $default
+         */
+        function config(string $key, $default = null)
+        {
+            \Hyperf\Config\config($key, $default);
+        }
     }
 }
