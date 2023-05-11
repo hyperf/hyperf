73654148e639b07006757832772a166cbaf816ce
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-05-11 14:53:01 +0800

    Add EnableRequestLifecycleListener

---
+++ src/http-server/src/Listener/EnableRequestLifecycleListener.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace Hyperf\HttpServer\Listener;
+
+use Hyperf\Event\Contract\ListenerInterface;
+use Hyperf\Framework\Event\BootApplication;
+use Hyperf\HttpServer\Server;
+use Psr\Container\ContainerInterface;
+use Psr\EventDispatcher\EventDispatcherInterface;
+
+class EnableRequestLifecycleListener implements ListenerInterface
+{
+    public function __construct(protected ContainerInterface $container)
+    {
+    }
+
+    public function listen(): array
+    {
+        return [
+            BootApplication::class,
+        ];
+    }
+
+    public function process(object $event): void
+    {
+        if ($this->container->has(EventDispatcherInterface::class)) {
+            $this->container->get(Server::class)->setEventDispatcher(
+                $this->container->get(EventDispatcherInterface::class)
+            );
+        }
+    }
+}
