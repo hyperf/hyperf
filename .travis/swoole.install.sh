dbbe01c367b7574ea2fe547d5641d7a33bd537db
李铭昕 <715557344@qq.com>
2023-05-16 20:54:57 +0800

    Update swoole.install.sh

--- .travis/swoole.install.sh
+++ .travis/swoole.install.sh
@@ -15,6 +15,5 @@ sudo make install
 sudo sh -c "echo extension=swoole > /etc/php/${PHP_VERSION}/cli/conf.d/swoole.ini"
 sudo sh -c "echo swoole.use_shortname='Off' >> /etc/php/${PHP_VERSION}/cli/conf.d/swoole.ini"
 php -v
+php -m
 php --ri swoole
-
-sudo rm -rf /etc/php/${PHP_VERSION}/cli/conf.d/20-xdebug.ini
