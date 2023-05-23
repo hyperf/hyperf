041cdd608058efe71b536dd30ab1e58184a66808
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-05-23 15:12:17 +0800

    Format

--- src/nacos/tests/Cases/Sign/NacosNamingSignTest.php
+++ src/nacos/tests/Cases/Sign/NacosNamingSignTest.php
@@ -1,10 +1,14 @@
 <?php
+
 declare(strict_types=1);
 /**
- * @author   crayxn <https://github.com/crayxn>
- * @contact  crayxn@qq.com
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
-
 namespace HyperfTest\Nacos\Cases\Sign;
 
 use GuzzleHttp\Psr7\Response;
@@ -13,13 +17,17 @@ use Hyperf\Nacos\Config;
 use HyperfTest\Nacos\AbstractTestCase;
 use Psr\Http\Message\RequestInterface;
 
+/**
+ * @internal
+ * @coversNothing
+ */
 class NacosNamingSignTest extends AbstractTestCase
 {
     public function testNacosNamingSign()
     {
-        $accessKey = "ak";
-        $accessSecret = "as";
-        $serverName = "example.rpc";
+        $accessKey = 'ak';
+        $accessSecret = 'as';
+        $serverName = 'example.rpc';
         $headers = [];
         $application = new Application(new Config([
             'access_key' => $accessKey,
@@ -28,16 +36,16 @@ class NacosNamingSignTest extends AbstractTestCase
                 'handler' => function (RequestInterface $request) use (&$headers) {
                     $headers = $request->getHeaders();
                     return new Response();
-                }
+                },
             ],
         ]));
         $application->service->detail($serverName);
-        [, $name] = explode("@@", $headers['data'][0] ?? '');
-        //check accessKey
+        [, $name] = explode('@@', $headers['data'][0] ?? '');
+        // check accessKey
         $this->assertSame($headers['ak'][0] ?? '', $accessKey);
-        //check serverName
+        // check serverName
         $this->assertSame($serverName, $name);
-        //check signature
+        // check signature
         $this->assertSame(base64_encode(hash_hmac('sha1', $headers['data'][0] ?? '', $accessSecret, true)), $headers['signature'][0] ?? '');
     }
 }
