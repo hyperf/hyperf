5009fa18e2fb7bbdbf5b9265763572d1670592cd
李铭昕 <715557344@qq.com>
2023-06-23 20:20:40 +0800

    Update RegisterInstanceException.php

--- src/service-governance/src/Exception/RegisterInstanceException.php
+++ src/service-governance/src/Exception/RegisterInstanceException.php
@@ -1,8 +1,18 @@
 <?php
 
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
 namespace Hyperf\ServiceGovernance\Exception;
 
-class RegisterInstanceException extends \RuntimeException
-{
+use RuntimeException;
 
+class RegisterInstanceException extends RuntimeException
+{
 }
