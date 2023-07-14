d15cf83e59665f8a78bd41ecc5f025b1079878e1
李铭昕 <715557344@qq.com>
2023-07-14 23:01:55 +0800

    Added confirm mode for `nsq::publish()`. (#5946)

--- src/nsq/tests/NsqTest.php
+++ src/nsq/tests/NsqTest.php
@@ -11,6 +11,11 @@ declare(strict_types=1);
  */
 namespace HyperfTest\Nsq;
 
+use Hyperf\Coroutine\Coroutine;
+use Hyperf\Engine\Channel;
+use Hyperf\Nsq\Message;
+use Hyperf\Nsq\Nsq;
+use HyperfTest\Nsq\Stub\ContainerStub;
 use PHPUnit\Framework\Attributes\CoversNothing;
 use PHPUnit\Framework\TestCase;
 
@@ -21,8 +26,24 @@ use PHPUnit\Framework\TestCase;
 #[CoversNothing]
 class NsqTest extends TestCase
 {
-    public function testExample()
+    public function testPublishAndSubscriber()
     {
-        $this->assertTrue(true);
+        $nsq = new Nsq(ContainerStub::getContainer());
+        try {
+            $body = uniqid();
+            $confirm = new Channel(10);
+            Coroutine::create(function () use ($nsq, $confirm) {
+                $nsq->subscribe('test', 'test', function (Message $message) use ($confirm) {
+                    $confirm->push($message->getBody());
+                }, true);
+            });
+
+            $res = $nsq->publish('test', $body, confirm: true);
+
+            $this->assertTrue($res);
+            $this->assertSame($body, $confirm->pop(5));
+        } finally {
+            $nsq->stopSubscribe();
+        }
     }
 }
