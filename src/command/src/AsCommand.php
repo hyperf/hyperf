80708f7907900324f8b06d168e3acb766cf8984f
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-07-14 08:07:53 +0800

    Optimize the code for setting input and output

--- src/command/src/AsCommand.php
+++ src/command/src/AsCommand.php
@@ -36,13 +36,12 @@ final class AsCommand extends Command
     {
         $inputs = array_merge($this->input->getArguments(), $this->input->getOptions());
         $parameters = $this->parameterParser->parseMethodParameters($this->class, $this->method, $inputs);
+        /** @var InteractsWithIO $instance */
         $instance = $this->container->get($this->class);
 
         if (in_array(InteractsWithIO::class, class_uses_recursive($this->class))) {
-            (function ($input, $output) {
-                $this->input = $input;
-                $this->output = $output;
-            })->call($instance, $this->input, $this->output);
+            $instance->setInput($this->input);
+            $instance->setOutput($this->output);
         }
 
         return (fn ($method) => $this->{$method}(...$parameters))->call($instance, $this->method);
