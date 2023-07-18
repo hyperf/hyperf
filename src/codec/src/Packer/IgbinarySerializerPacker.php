82de60b0f2f4b4ed3f03d85a9857f7c7dcf6c92a
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2023-07-18 18:38:47 +0930

    Add IgbinarySerializerPacker

---
+++ src/codec/src/Packer/IgbinarySerializerPacker.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace Hyperf\Codec\Packer;
+
+use Hyperf\Contract\PackerInterface;
+
+class IgbinarySerializerPacker implements PackerInterface
+{
+    public function pack($data): string
+    {
+        return igbinary_serialize($data);
+    }
+
+    public function unpack(string $data)
+    {
+        return igbinary_unserialize($data);
+    }
+}
