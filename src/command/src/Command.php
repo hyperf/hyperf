913f70ed11732c8ed2969ac8e3f805fcf7607051
szutoutou <szutoutou@qq.com>
2023-08-08 11:21:53 +0800

    fix(command):exception exitCode default -1

--- src/command/src/Command.php
+++ src/command/src/Command.php
@@ -439,7 +439,7 @@ abstract class Command extends SymfonyCommand
                 $this->output && $this->error($exception->getMessage());
 
                 $this->eventDispatcher->dispatch(new Event\FailToHandle($this, $exception));
-                return $this->exitCode = (int) $exception->getCode();
+                return $this->exitCode = (int) $exception->getCode() ?: -1;
             } finally {
                 $this->eventDispatcher?->dispatch(new Event\AfterExecute($this));
             }
