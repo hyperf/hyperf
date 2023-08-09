a6e1014f70113c2ede2da1c3406d01d9bdd7790a
gaichao168 <1432681790@qq.com>
2023-08-09 09:55:43 +0800

    Optimized the code for pgsql.

--- src/db/src/PgSQL/PgSQLConnection.php
+++ src/db/src/PgSQL/PgSQLConnection.php
@@ -22,10 +22,7 @@ use Swoole\Coroutine\PostgreSQLStatement;
 
 class PgSQLConnection extends AbstractConnection
 {
-    /**
-     * @var PostgreSQL
-     */
-    protected $connection;
+    protected PostgreSQL $connection;
 
     protected array $config = [
         'driver' => 'pgsql',
@@ -104,7 +101,7 @@ class PgSQLConnection extends AbstractConnection
 
     public function exec(string $sql): int
     {
-        return $this->execute($sql, []);
+        return $this->execute($sql);
     }
 
     public function query(string $query, array $bindings = []): array
@@ -128,16 +125,12 @@ class PgSQLConnection extends AbstractConnection
 
     public function call(string $method, array $argument = [])
     {
-        switch ($method) {
-            case 'beginTransaction':
-                return $this->connection->query('BEGIN');
-            case 'rollBack':
-                return $this->connection->query('ROLLBACK');
-            case 'commit':
-                return $this->connection->query('COMMIT');
-        }
-
-        return $this->connection->{$method}(...$argument);
+        return match ($method) {
+            'beginTransaction' => $this->connection->query('BEGIN'),
+            'rollBack' => $this->connection->query('ROLLBACK'),
+            'commit' => $this->connection->query('COMMIT'),
+            default => $this->connection->{$method}(...$argument),
+        };
     }
 
     public function run(Closure $closure)
@@ -145,7 +138,18 @@ class PgSQLConnection extends AbstractConnection
         return $closure->call($this, $this->connection);
     }
 
-    public function str_replace_once($needle, $replace, $haystack)
+    /**
+     * @param string $needle
+     * @param string $replace
+     * @param string $haystack
+     * @deprecated ,using `strReplaceOnce` instead
+     */
+    public function str_replace_once($needle, $replace, $haystack): array|string
+    {
+        return $this->strReplaceOnce($needle, $replace, $haystack);
+    }
+
+    public function strReplaceOnce(string $needle, string $replace, string $haystack): array|string
     {
         // Looks for the first occurence of $needle in $haystack
         // and replaces it with $replace.
@@ -162,7 +166,7 @@ class PgSQLConnection extends AbstractConnection
     {
         $num = 1;
         while (strpos($query, '?')) {
-            $query = $this->str_replace_once('?', '$' . $num++, $query);
+            $query = $this->strReplaceOnce('?', '$' . $num++, $query);
         }
 
         $statement = $this->connection->prepare($query);
