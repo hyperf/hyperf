160db2c3f8287749a2255f0e70606a025f6a6686
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-08-15 15:43:03 +0800

    Allow using the tracer instance from context

--- src/tracer/src/SpanStarter.php
+++ src/tracer/src/SpanStarter.php
@@ -15,6 +15,7 @@ use Hyperf\Context\ApplicationContext;
 use Hyperf\Context\Context;
 use Hyperf\Rpc;
 use OpenTracing\Span;
+use OpenTracing\Tracer;
 use Psr\Http\Message\ServerRequestInterface;
 
 use const OpenTracing\Formats\TEXT_MAP;
@@ -32,14 +33,16 @@ trait SpanStarter
         string $kind = SPAN_KIND_RPC_SERVER
     ): Span {
         $root = Context::get('tracer.root');
+        /** @var Tracer $tracer */
+        $tracer = Context::get('tracer.tracer') ?: $this->tracer;
         if (! $root instanceof Span) {
             $container = ApplicationContext::getContainer();
             /** @var ServerRequestInterface $request */
             $request = Context::get(ServerRequestInterface::class);
             if (! $request instanceof ServerRequestInterface) {
-                // If the request object is absent, we are probably in a commandline context.
+                // If the request object is absent, we are probably in a commandLine context.
                 // Throwing an exception is unnecessary.
-                $root = $this->tracer->startSpan($name, $option);
+                $root = $tracer->startSpan($name, $option);
                 $root->setTag(SPAN_KIND, $kind);
                 Context::set('tracer.root', $root);
                 return $root;
@@ -54,17 +57,17 @@ trait SpanStarter
                 }
             }
             // Extracts the context from the HTTP headers.
-            $spanContext = $this->tracer->extract(TEXT_MAP, $carrier);
+            $spanContext = $tracer->extract(TEXT_MAP, $carrier);
             if ($spanContext) {
                 $option['child_of'] = $spanContext;
             }
-            $root = $this->tracer->startSpan($name, $option);
+            $root = $tracer->startSpan($name, $option);
             $root->setTag(SPAN_KIND, $kind);
             Context::set('tracer.root', $root);
             return $root;
         }
         $option['child_of'] = $root->getContext();
-        $child = $this->tracer->startSpan($name, $option);
+        $child = $tracer->startSpan($name, $option);
         $child->setTag(SPAN_KIND, $kind);
         return $child;
     }
