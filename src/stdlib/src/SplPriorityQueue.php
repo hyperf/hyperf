5d2d69a80bb6984829478318d7f4d573908e6d61
Deeka Wong <huangdijia@gmail.com>
2023-08-24 22:55:47 -0500

    Adds `hyperf/stdlib` component (#6082)
    Co-authored-by: 李铭昕 <715557344@qq.com>

---
+++ src/stdlib/src/SplPriorityQueue.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace Hyperf\Stdlib;
+
+use const PHP_INT_MAX;
+
+/**
+ * Serializable version of SplPriorityQueue.
+ *
+ * Also, provides predictable heap order for datums added with the same priority
+ * (i.e., they will be emitted in the same order they are enqueued).
+ *
+ * @template TValue
+ * @template TPriority
+ * @extends \SplPriorityQueue<TPriority, TValue>
+ */
+class SplPriorityQueue extends \SplPriorityQueue
+{
+    /**
+     * Seed used to ensure queue order for items of the same priority.
+     */
+    protected int $serial = PHP_INT_MAX;
+
+    /**
+     * Insert a value with a given priority.
+     *
+     * Utilizes {@var to $serial} ensure that values of equal priority are
+     * emitted in the same order in which they are inserted.
+     *
+     * @param TValue $value
+     * @param TPriority $priority
+     */
+    public function insert($value, $priority)
+    {
+        $priority = [$priority, $this->serial--];
+        return parent::insert($value, $priority);
+    }
+
+    /**
+     * Serialize to an array.
+     *
+     * Array will be priority => data pairs
+     *
+     * @return list<TValue>
+     */
+    public function toArray()
+    {
+        $array = [];
+        foreach (clone $this as $item) {
+            $array[] = $item;
+        }
+        return $array;
+    }
+}
