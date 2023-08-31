4e23cb6f5cafa7b85d2318d367d571d40460bc5a
Vinchan <ademo@vip.qq.com>
2023-08-31 15:49:41 +0800

    feat:tests

--- src/validation/tests/Cases/ValidateAttributesTest.php
+++ src/validation/tests/Cases/ValidateAttributesTest.php
@@ -25,43 +25,43 @@ class ValidateAttributesTest extends TestCase
     public function testValidateAlpha()
     {
         $validator = new ValidatesAttributesStub();
-        $this->assertTrue($validator->validateAlpha('', 'xxx'));
-        $this->assertTrue($validator->validateAlpha('', '你好'));
+        $this->assertTrue($validator->validateAlpha('', 'xxx', []));
+        $this->assertTrue($validator->validateAlpha('', '你好', []));
 
-        $this->assertFalse($validator->validateAlpha('', '123'));
-        $this->assertFalse($validator->validateAlpha('', '123f1'));
-        $this->assertFalse($validator->validateAlpha('', 123));
-        $this->assertFalse($validator->validateAlpha('', 123.1));
-        $this->assertFalse($validator->validateAlpha('', '123_f1'));
-        $this->assertFalse($validator->validateAlpha('', 'xxx_yy'));
+        $this->assertFalse($validator->validateAlpha('', '123', []));
+        $this->assertFalse($validator->validateAlpha('', '123f1', []));
+        $this->assertFalse($validator->validateAlpha('', 123, []));
+        $this->assertFalse($validator->validateAlpha('', 123.1, []));
+        $this->assertFalse($validator->validateAlpha('', '123_f1', []));
+        $this->assertFalse($validator->validateAlpha('', 'xxx_yy', []));
     }
 
     public function testValidateAlphaDash()
     {
         $validator = new ValidatesAttributesStub();
-        $this->assertTrue($validator->validateAlphaDash('', 'xxx'));
-        $this->assertTrue($validator->validateAlphaDash('', 'xxx_yy'));
-        $this->assertTrue($validator->validateAlphaDash('', '你好'));
-        $this->assertTrue($validator->validateAlphaDash('', '123'));
-        $this->assertTrue($validator->validateAlphaDash('', '123f1'));
-        $this->assertTrue($validator->validateAlphaDash('', 123));
-        $this->assertTrue($validator->validateAlphaDash('', '123_f1'));
+        $this->assertTrue($validator->validateAlphaDash('', 'xxx', []));
+        $this->assertTrue($validator->validateAlphaDash('', 'xxx_yy', []));
+        $this->assertTrue($validator->validateAlphaDash('', '你好', []));
+        $this->assertTrue($validator->validateAlphaDash('', '123', []));
+        $this->assertTrue($validator->validateAlphaDash('', '123f1', []));
+        $this->assertTrue($validator->validateAlphaDash('', 123, []));
+        $this->assertTrue($validator->validateAlphaDash('', '123_f1', []));
 
-        $this->assertFalse($validator->validateAlphaDash('', 123.1));
+        $this->assertFalse($validator->validateAlphaDash('', 123.1, []));
     }
 
     public function testValidateAlphaNum()
     {
         $validator = new ValidatesAttributesStub();
-        $this->assertTrue($validator->validateAlphaNum('', 'xxx'));
-        $this->assertTrue($validator->validateAlphaNum('', '123'));
-        $this->assertTrue($validator->validateAlphaNum('', '123f1'));
-        $this->assertTrue($validator->validateAlphaNum('', 123));
-        $this->assertTrue($validator->validateAlphaNum('', '你好'));
+        $this->assertTrue($validator->validateAlphaNum('', 'xxx', []));
+        $this->assertTrue($validator->validateAlphaNum('', '123', []));
+        $this->assertTrue($validator->validateAlphaNum('', '123f1', []));
+        $this->assertTrue($validator->validateAlphaNum('', 123, []));
+        $this->assertTrue($validator->validateAlphaNum('', '你好', []));
 
-        $this->assertFalse($validator->validateAlphaNum('', 123.1));
-        $this->assertFalse($validator->validateAlphaNum('', '123_f1'));
-        $this->assertFalse($validator->validateAlphaNum('', 'xxx_yy'));
+        $this->assertFalse($validator->validateAlphaNum('', 123.1, []));
+        $this->assertFalse($validator->validateAlphaNum('', '123_f1', []));
+        $this->assertFalse($validator->validateAlphaNum('', 'xxx_yy', []));
     }
 
     public function testValidateDate()
