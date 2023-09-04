4d0861151df202ee49262d15603f13b9b57fb774
李铭昕 <715557344@qq.com>
2023-09-04 21:01:44 +0800

    Update RewriteConfigFactoryVisitor.php

--- src/phar/src/Ast/Visitor/RewriteConfigFactoryVisitor.php
+++ src/phar/src/Ast/Visitor/RewriteConfigFactoryVisitor.php
@@ -30,7 +30,7 @@ class ConfigFactory{
                 str_replace('/', '.', \$file->getRelativePath()),
                 \$file->getBasename('.php'),
             ]));
-            Arr::set(\$config, \$key, require \$file->getRealPath());
+            \\Hyperf\\Collection\\Arr::set(\$config, \$key, require \$file->getRealPath());
             \$configs[] = \$config;
         }
         return \$configs;
