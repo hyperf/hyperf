2fb9bbea70a33e7b1a29d4e6510c5b64313d5e74
李铭昕 <715557344@qq.com>
2023-09-14 15:57:06 +0800

    Update RedisTest.php

--- src/redis/tests/RedisTest.php
+++ src/redis/tests/RedisTest.php
@@ -66,7 +66,11 @@ class RedisTest extends TestCase
         $this->assertSame('host', $host->getName());
         $this->assertSame('port', $port->getName());
         $this->assertSame('timeout', $timeout->getName());
-        $this->assertSame('retry_interval', $retryInterval->getName());
+        if (version_compare(phpversion('redis'), '6.0', '>=')) {
+            $this->assertSame('persistent_id', $retryInterval->getName());
+        } else {
+            $this->assertSame('retry_interval', $retryInterval->getName());
+        }
 
         $this->assertTrue($redis->connect('127.0.0.1', 6379, 0.0, null, 0, 0));
     }
@@ -159,14 +163,31 @@ class RedisTest extends TestCase
         $ref = new ReflectionClass(RedisCluster::class);
         $method = $ref->getMethod('__construct');
         $names = [
-            'name', 'seeds', 'timeout', 'read_timeout', 'persistent', 'auth',
+            ['name', 'string'],
+            ['seeds', 'array'],
+            ['timeout', ['int', 'float']],
+            ['read_timeout', ['int', 'float']],
+            ['persistent', 'bool'],
+            ['auth', 'mixed'],
+            ['context', 'array'],
         ];
         foreach ($method->getParameters() as $parameter) {
-            $this->assertSame(array_shift($names), $parameter->getName());
+            [$name, $type] = array_shift($names);
+            $this->assertSame($name, $parameter->getName());
             if ($parameter->getName() === 'seeds') {
                 $this->assertSame('array', $parameter->getType()->getName());
             } else {
-                $this->assertNull($parameter->getType());
+                if (version_compare(phpversion('redis'), '6.0', '>=')) {
+                    if (is_array($type)) {
+                        foreach ($parameter->getType()->getTypes() as $namedType) {
+                            $this->assertTrue(in_array($namedType->getName(), $type));
+                        }
+                    } else {
+                        $this->assertSame($type, $parameter->getType()->getName());
+                    }
+                } else {
+                    $this->assertNull($parameter->getType());
+                }
             }
         }
     }
@@ -202,11 +223,16 @@ class RedisTest extends TestCase
         $rel = new ReflectionClass(RedisSentinel::class);
         $method = $rel->getMethod('__construct');
         $count = count($method->getParameters());
-        if ($count === 6) {
-            $this->markTestIncomplete('RedisSentinel don\'t support auth.');
-        }
 
-        $this->assertSame(7, $count);
+        if (version_compare(phpversion('redis'), '6.0', '>=')) {
+            $this->assertSame(1, $count);
+            $this->assertSame('options', $method->getParameters()[0]->getName());
+        } else {
+            if ($count === 6) {
+                $this->markTestIncomplete('RedisSentinel don\'t support auth.');
+            }
+            $this->assertSame(7, $count);
+        }
     }
 
     private function getRedis()
