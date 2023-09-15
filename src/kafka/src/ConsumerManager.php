a80f9d618a7c406d0a4d2ad88ca87cde505da7e7
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-09-15 10:52:54 +0800

    Update ConsumerManager.php

--- src/kafka/src/ConsumerManager.php
+++ src/kafka/src/ConsumerManager.php
@@ -151,7 +151,7 @@ class ConsumerManager
                         $longLangConsumer->start();
                     } catch (Throwable $exception) {
                         $this->stdoutLogger->warning((string) $exception);
-                        $this->dispatcher?->dispatch(new FailToConsume($consumer, [], $exception));
+                        $this->dispatcher?->dispatch(new FailToConsume($this->consumer, [], $exception));
                     }
 
                     if (CoordinatorManager::until(Constants::WORKER_EXIT)->yield(10)) {
