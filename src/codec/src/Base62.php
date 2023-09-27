f2db9c85b8221dc80657b1e3829592b631e36519
张城铭 <z@hyperf.io>
2023-09-27 11:18:47 +0800

    Update Base62.php
    以便子类重写CHARS 常量顺序

--- src/codec/src/Base62.php
+++ src/codec/src/Base62.php
@@ -21,9 +21,9 @@ class Base62
     {
         $chars = [];
         while ($number > 0) {
-            $remain = $number % self::BASE;
-            $chars[] = self::CHARS[$remain];
-            $number = ($number - $remain) / self::BASE;
+            $remain = $number % static::BASE;
+            $chars[] = static::CHARS[$remain];
+            $number = ($number - $remain) / static::BASE;
         }
         return implode('', array_reverse($chars));
     }
@@ -31,9 +31,9 @@ class Base62
     public static function decode(string $data): int
     {
         return array_reduce(array_map(function ($character) {
-            return strpos(self::CHARS, $character);
+            return strpos(static::CHARS, $character);
         }, str_split($data)), function ($result, $remain) {
-            return $result * self::BASE + $remain;
+            return $result * static::BASE + $remain;
         });
     }
 }
