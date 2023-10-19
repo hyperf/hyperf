07c2c949f7b3bad7b04cddfef8555ffba2e454a9
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-10-19 20:44:28 +0800

    Format

--- src/http-factory/src/ConfigProvider.php
+++ src/http-factory/src/ConfigProvider.php
@@ -1,7 +1,16 @@
 <?php
 
 declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
 namespace Hyperf\HttpMessage\Factory;
+
 use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ServerRequestFactoryInterface;
@@ -19,7 +28,7 @@ class ConfigProvider
                 ResponseFactoryInterface::class => ResponseFactory::class,
                 ServerRequestFactoryInterface::class => ServerRequestFactory::class,
                 StreamFactoryInterface::class => StreamFactory::class,
-//                UploadedFileFactoryInterface::class => UploadedFileFactory::class,
+                // UploadedFileFactoryInterface::class => UploadedFileFactory::class,
                 UriFactoryInterface::class => UriFactory::class,
             ],
         ];
