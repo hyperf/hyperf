ebf08d669824778d2edde65b5b73c410ce346de5
宣言就是Siam <59419979@qq.com>
2023-10-21 15:21:01 +0800

    Adds charset param and html method for Response. (#6224)

--- src/http-server/tests/ResponseTest.php
+++ src/http-server/tests/ResponseTest.php
@@ -179,6 +179,23 @@ class ResponseTest extends TestCase
         $this->assertSame('{"kstring":"string","kint1":1,"kint0":0,"kfloat":0.12345,"kfalse":false,"ktrue":true,"karray":{"kstring":"string","kint1":1,"kint0":0,"kfloat":0.12345,"kfalse":false,"ktrue":true}}', (string) $json->getBody());
     }
 
+    public function testHtml()
+    {
+        $container = Mockery::mock(ContainerInterface::class);
+        ApplicationContext::setContainer($container);
+
+        $psrResponse = new \Hyperf\HttpMessage\Base\Response();
+        Context::set(PsrResponseInterface::class, $psrResponse);
+
+        $response = new Response();
+        $html = $response->html('<h1>hello world</h1>');
+        $this->assertSame('<h1>hello world</h1>', (string) $html->getBody());
+
+        $html = $response->html('<h1>hello world</h1>', 'GBK');
+        $this->assertSame('<h1>hello world</h1>', (string) $html->getBody());
+        $this->assertSame('text/html; charset=GBK', $html->getHeaderLine('content-type'));
+    }
+
     public function testObjectToJson()
     {
         $container = Mockery::mock(ContainerInterface::class);
