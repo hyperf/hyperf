0fc239be1511b6892f2c9b19ff6da8afea240020
Deeka Wong <huangdijia@gmail.com>
2023-10-24 10:09:30 +0800

    Make rules method in FormRequest optional (#6228)
    Co-authored-by: 李铭昕 <715557344@qq.com>

--- src/validation/src/Request/FormRequest.php
+++ src/validation/src/Request/FormRequest.php
@@ -104,6 +104,11 @@ class FormRequest extends Request implements ValidatesWhenResolved
         return $this;
     }
 
+    public function rules(): array
+    {
+        return [];
+    }
+
     /**
      * Get the validator instance for the request.
      */
@@ -198,7 +203,7 @@ class FormRequest extends Request implements ValidatesWhenResolved
      */
     protected function getRules(): array
     {
-        $rules = call_user_func_array([$this, 'rules'], []);
+        $rules = $this->rules();
         $scene = $this->getScene();
         if ($scene && isset($this->scenes[$scene]) && is_array($this->scenes[$scene])) {
             return Arr::only($rules, $this->scenes[$scene]);
