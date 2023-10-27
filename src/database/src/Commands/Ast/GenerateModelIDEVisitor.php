074e83301012d937b336c9cc7d3974823ffe4462
Siam <59419979@qq.com>
2023-10-27 09:21:22 +0000

    Support unionType param for GenerateModelIDEVisitor.

--- src/database/src/Commands/Ast/GenerateModelIDEVisitor.php
+++ src/database/src/Commands/Ast/GenerateModelIDEVisitor.php
@@ -19,8 +19,10 @@ use Hyperf\Stringable\Str;
 use PhpParser\BuilderFactory;
 use PhpParser\Comment\Doc;
 use PhpParser\Node;
+use PhpParser\Node\Identifier;
 use ReflectionClass;
 use ReflectionParameter;
+use ReflectionUnionType;
 
 class GenerateModelIDEVisitor extends AbstractVisitor
 {
@@ -109,7 +111,16 @@ class GenerateModelIDEVisitor extends AbstractVisitor
                     if ($argument->getType()->allowsNull()) {
                         $argType = new Node\NullableType($argument->getType()->getName());
                     } else {
-                        $argType = $argument->getType()->getName();
+                        $argumentType = $argument->getType();
+                        if ($argumentType instanceof ReflectionUnionType) {
+                            $unionTypeIdentifier = [];
+                            foreach ($argumentType->getTypes() as $type) {
+                                $unionTypeIdentifier[] = new Identifier($type->getName());
+                            }
+                            $argType = new Node\UnionType($unionTypeIdentifier);
+                        } else {
+                            $argType = $argumentType->getName();
+                        }
                     }
                 }
                 if ($argument->isDefaultValueAvailable()) {
