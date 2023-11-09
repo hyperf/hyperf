35fd2e14123b528c51ec47417973d0de6e3bdcf7
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-11-09 15:43:47 +0800

    Adds `metric. enable_command_metric` switch

--- src/metric/publish/metric.php
+++ src/metric/publish/metric.php
@@ -18,6 +18,7 @@ return [
     'default' => env('METRIC_DRIVER', 'prometheus'),
     'use_standalone_process' => env('METRIC_USE_STANDALONE_PROCESS', true),
     'enable_default_metric' => env('METRIC_ENABLE_DEFAULT_METRIC', true),
+    'enable_command_metric' => env('METRIC_ENABLE_COMMAND_METRIC', true),
     'default_metric_interval' => env('DEFAULT_METRIC_INTERVAL', 5),
     'metric' => [
         'prometheus' => [
