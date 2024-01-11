f8abca09ad80e1cf34d277ce2fbf835274fdc4da
pandaLIU <563883861@qq.com>
2024-01-11 09:45:15 +0800

    优化 socketIO Decoder

--- src/socketio-server/src/Parser/Decoder.php
+++ src/socketio-server/src/Parser/Decoder.php
@@ -11,49 +11,80 @@ declare(strict_types=1);
  */
 namespace Hyperf\SocketIOServer\Parser;
 
+use InvalidArgumentException;
+use Throwable;
+
 class Decoder
 {
-    public function decode($payload): Packet
+    /**
+     * @param string $payload such as `2/ws?foo=xxx,2["event","hellohyperf"]`
+     */
+    public function decode(string $payload): Packet
     {
-        // type
-        $i = 0;
-        $type = $payload[$i];
+        if (! $payload) {
+            throw new InvalidArgumentException('Empty packet');
+        }
+
+        $length = strlen($payload);
+        $type = $payload[0];
+        if (! in_array($type, [Packet::OPEN, Packet::CLOSE, Packet::EVENT, Packet::ACK], true)) {
+            throw new InvalidArgumentException('Unknown packet type ' . $type);
+        }
+
+        if ($length === 1) {
+            return $this->makePacket($type);
+        }
+
         $nsp = '/';
         $query = [];
-        ++$i;
 
-        // TODO: Support attachment
+        $payload = substr($payload, 1);
+        if ($payload[0] === '/') {
+            // parse url
+            $exploded = explode(',', $payload, 2);
+            $parsedUrl = parse_url($exploded[0]);
+            $nsp = $parsedUrl['path'];
+            if (! empty($parsedUrl['query'])) {
+                parse_str($parsedUrl['query'], $query);
+            }
 
-        // namespace
-        if (isset($payload[$i]) && $payload[$i] === '/') {
-            ++$i;
-            while ($payload[$i] !== ',' && $payload[$i] !== '?') {
-                $nsp .= $payload[$i];
-                ++$i;
-            }
-            if ($payload[$i] === '?') {
-                ++$i;
-                $query = '';
-                while ($payload[$i] !== ',') {
-                    $query .= $payload[$i];
-                    ++$i;
-                }
-                $result = [];
-                parse_str($query, $result);
-                $query = $result;
-            }
-            ++$i;
+            $payload = $exploded[1] ?? null;
         }
 
-        // id
-        $id = '';
-        while (mb_strlen($payload) > $i && filter_var($payload[$i], FILTER_VALIDATE_INT) !== false) {
-            $id .= $payload[$i];
-            ++$i;
+        if (! $payload) {
+            return $this->makePacket($type, $nsp, $query);
         }
 
-        // data
-        $data = json_decode(mb_substr($payload, $i), true) ?? [];
+        $offset = 0;
+        while (true) {
+            $char = $payload[$offset] ?? null;
+            if ($char === null) {
+                break;
+            }
+
+            if (is_numeric($char)) {
+                ++$offset;
+            } else {
+                break;
+            }
+        }
+
+        $id = substr($payload, 0, $offset);
+        $payload = substr($payload, $offset);
+        $data = [];
+        if ($payload) {
+            try {
+                $data = json_decode($payload, true, flags: JSON_THROW_ON_ERROR);
+            } catch (Throwable $exception) {
+                throw new InvalidArgumentException('Invalid data', (int) $exception->getCode(), $exception);
+            }
+        }
+
+        return $this->makePacket($type, $nsp, $query, $id, $data);
+    }
+
+    public function makePacket(string $type, string $nsp = '/', array $query = [], string $id = '', array $data = []): Packet
+    {
         return Packet::create([
             'type' => $type,
             'nsp' => $nsp,
