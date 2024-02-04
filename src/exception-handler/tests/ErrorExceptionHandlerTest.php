3e2b2c21da7d005eaf7974ff2043a38e9fa9b65d
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2024-02-05 07:40:22 +0800

    Update ErrorExceptionHandlerTest.php

--- src/exception-handler/tests/ErrorExceptionHandlerTest.php
+++ src/exception-handler/tests/ErrorExceptionHandlerTest.php
@@ -15,7 +15,6 @@ use ErrorException;
 use Hyperf\ExceptionHandler\Listener\ErrorExceptionHandler;
 use Mockery;
 use PHPUnit\Framework\Attributes\CoversNothing;
-use PHPUnit\Framework\Attributes\WithoutErrorHandler;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -30,7 +29,6 @@ class ErrorExceptionHandlerTest extends TestCase
         Mockery::close();
     }
 
-    #[WithoutErrorHandler]
     public function testHandleError()
     {
         $container = Mockery::mock(\Psr\Container\ContainerInterface::class);
