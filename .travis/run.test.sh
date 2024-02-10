81d59c6f87b8c992167ee04778835e88ce6ffaa0
李铭昕 <715557344@qq.com>
2024-02-10 18:09:27 +0800

    Update run.test.sh

--- .travis/run.test.sh
+++ .travis/run.test.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+php -dswoole.use_shortname='Off' bin/co-phpunit src/phar
 php -dswoole.use_shortname='Off' bin/co-phpunit --exclude-group NonCoroutine
 php -dswoole.use_shortname='Off' vendor/bin/phpunit --group NonCoroutine
 php -dswoole.use_shortname='Off' vendor/bin/phpunit src/filesystem --group NonCoroutine
