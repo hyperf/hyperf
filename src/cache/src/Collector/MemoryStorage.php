5c2a890194b1c36ff580197885fda410d50d941e
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2024-02-19 11:57:38 +0800

    Added `MemoryDriver` for `hyperf/cache`

---
+++ src/cache/src/Collector/MemoryStorage.php
@@ -0,0 +1,111 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace Hyperf\Cache\Collector;
+
+use Carbon\Carbon;
+use Hyperf\Coordinator\Constants;
+use Hyperf\Coordinator\CoordinatorManager;
+use Hyperf\Coroutine\Coroutine;
+use Hyperf\Stringable\Str;
+
+final class MemoryStorage
+{
+    /**
+     * @var array<string, null|Carbon>
+     */
+    private array $keys = [];
+
+    /**
+     * @var array<string, mixed>
+     */
+    private array $values = [];
+
+    private ?int $loopCid = null;
+
+    private ?int $waitCloseCid = null;
+
+    private bool $workerExit = false;
+
+    public static function instance(): static
+    {
+        static $instance;
+
+        return $instance ??= new self();
+    }
+
+    public function has(string $key): bool
+    {
+        return isset($this->values[$key]);
+    }
+
+    public function get(string $key, mixed $default = null): mixed
+    {
+        return $this->values[$key] ?? $default;
+    }
+
+    public function set(string $key, mixed $value, ?Carbon $ttl = null): bool
+    {
+        $this->loop();
+        $this->keys[$key] = $ttl;
+        $this->values[$key] = $value;
+
+        return true;
+    }
+
+    public function delete(string $key): bool
+    {
+        unset($this->keys[$key], $this->values[$key]);
+
+        return true;
+    }
+
+    public function clear(): bool
+    {
+        $this->keys = [];
+        $this->values = [];
+
+        return true;
+    }
+
+    public function clearPrefix(string $prefix): bool
+    {
+        foreach ($this->keys as $key => $ttl) {
+            if (Str::startsWith($key, $prefix)) {
+                $this->delete($key);
+            }
+        }
+
+        return true;
+    }
+
+    private function loop(): void
+    {
+        $this->loopCid ??= Coroutine::create(function () {
+            while (! $this->workerExit) {
+                foreach ($this->keys as $key => $ttl) {
+                    if ($ttl instanceof Carbon && Carbon::now()->gt($ttl)) {
+                        $this->delete($key);
+                    }
+                }
+                sleep(1);
+            }
+            $this->loopCid = null;
+        });
+
+        $this->waitCloseCid ??= Coroutine::create(function () {
+            CoordinatorManager::until(Constants::WORKER_EXIT)->yield();
+            $this->workerExit = true;
+            $this->clear();
+            $this->waitCloseCid = null;
+        });
+    }
+}
---
+++ src/cache/src/Driver/MemoryDriver.php
@@ -0,0 +1,118 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace Hyperf\Cache\Driver;
+
+use Carbon\Carbon;
+use DateInterval;
+use Hyperf\Cache\Collector\MemoryStorage;
+use Hyperf\Cache\Exception\InvalidArgumentException;
+use Psr\Container\ContainerInterface;
+
+class MemoryDriver extends Driver implements DriverInterface
+{
+    public function __construct(ContainerInterface $container, array $config)
+    {
+        parent::__construct($container, $config);
+    }
+
+    /**
+     * @return array<bool, mixed>
+     */
+    public function fetch(string $key, mixed $default = null): array
+    {
+        return [
+            $this->has($key),
+            $this->get($key, $default),
+        ];
+    }
+
+    public function has(string $key): bool
+    {
+        return $this->getStore()->has($this->getCacheKey($key));
+    }
+
+    public function get(string $key, mixed $default = null): mixed
+    {
+        return $this->getStore()->get(
+            $this->getCacheKey($key),
+            $default
+        );
+    }
+
+    public function set(string $key, mixed $value, DateInterval|int|null $ttl = null): bool
+    {
+        $seconds = $this->secondsUntil($ttl);
+        return $this->getStore()->set(
+            $this->getCacheKey($key),
+            $value,
+            $ttl <= 0 ? null : Carbon::now()->addSeconds($seconds)
+        );
+    }
+
+    public function delete(string $key): bool
+    {
+        return $this->getStore()->delete($this->getCacheKey($key));
+    }
+
+    public function clear(): bool
+    {
+        return $this->getStore()->clear();
+    }
+
+    public function getMultiple(iterable $keys, mixed $default = null): iterable
+    {
+        $result = [];
+
+        foreach ($keys as $key) {
+            $result[$key] = $this->get($key, $default);
+        }
+
+        return $result;
+    }
+
+    public function setMultiple(iterable $values, DateInterval|int|null $ttl = null): bool
+    {
+        if (! is_array($values)) {
+            throw new InvalidArgumentException('The values is invalid!');
+        }
+
+        foreach ($values as $key => $value) {
+            $this->set($key, $value, $ttl);
+        }
+
+        return true;
+    }
+
+    public function deleteMultiple($keys): bool
+    {
+        foreach ($keys as $key) {
+            $this->delete($key);
+        }
+
+        return true;
+    }
+
+    public function clearPrefix(string $prefix): bool
+    {
+        return $this->getStore()->clearPrefix($this->getCacheKey($prefix));
+    }
+
+    public function getConnection(): mixed
+    {
+        return $this;
+    }
+
+    protected function getStore(): MemoryStorage
+    {
+        return MemoryStorage::instance();
+    }
+}
