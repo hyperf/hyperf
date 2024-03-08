be0b046c5f5c43b2cb3d1b4280a863ce6ebc441c
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2024-03-08 15:34:47 +0800

    Refactor setUpFoo method to accept InputInterface and OutputInterface as parameters

--- src/command/tests/Command/Traits/Foo.php
+++ src/command/tests/Command/Traits/Foo.php
@@ -11,11 +11,14 @@ declare(strict_types=1);
  */
 namespace HyperfTest\Command\Command\Traits;
 
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
 trait Foo
 {
     private ?string $propertyFoo = null;
 
-    protected function setUpFoo()
+    protected function setUpFoo(?InputInterface $input, ?OutputInterface $output)
     {
         $this->propertyFoo = 'foo';
     }
