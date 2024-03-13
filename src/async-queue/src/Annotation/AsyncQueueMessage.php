e2b0a9209164c58e16a569b5b9e4d401bf6737cd
李铭昕 <715557344@qq.com>
2024-03-13 18:13:10 +0800

    Update AsyncQueueMessage.php

--- src/async-queue/src/Annotation/AsyncQueueMessage.php
+++ src/async-queue/src/Annotation/AsyncQueueMessage.php
@@ -17,7 +17,7 @@ use Hyperf\Di\Annotation\AbstractAnnotation;
 /**
  * Don't call the methods with this annotation in async queue environment.
  * Because the execution or delivery of a message depends on whether it is currently in an async queue environment,
- * re delivery in an async queue environment will be treated as a direct execution of the message
+ * re delivery in an async queue environment will be treated as a direct execution of the message.
  */
 #[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_METHOD)]
 class AsyncQueueMessage extends AbstractAnnotation
