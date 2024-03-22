e05fd9b27d0a649d79b1f49a82ca94170dc09b64
李铭昕 <715557344@qq.com>
2024-03-22 13:50:49 +0800

    Update ConnectionTest.php

--- src/pool/tests/ConnectionTest.php
+++ src/pool/tests/ConnectionTest.php
@@ -12,7 +12,9 @@ declare(strict_types=1);
 namespace HyperfTest\Pool;
 
 use Hyperf\Contract\StdoutLoggerInterface;
+use Hyperf\Pool\Event\ReleaseConnection;
 use Hyperf\Pool\Pool;
+use Hyperf\Pool\PoolOption;
 use HyperfTest\Pool\Stub\ActiveConnectionStub;
 use Mockery;
 use PHPUnit\Framework\Attributes\CoversNothing;
@@ -43,5 +45,41 @@ class ConnectionTest extends TestCase
 
         $connection = new ActiveConnectionStub($container, Mockery::mock(Pool::class));
         $this->assertEquals($connection, $connection->getConnection());
+    }
+
+    public function testReleaseConnectionEvent()
+    {
+        $assert = 0;
+        $container = Mockery::mock(ContainerInterface::class);
+        $container->shouldReceive('has')->with(StdoutLoggerInterface::class)->once()->andReturnFalse();
+        $container->shouldReceive('has')->with(EventDispatcherInterface::class)->andReturnTrue();
+        $container->shouldReceive('get')->with(EventDispatcherInterface::class)->andReturn($dispatcher = Mockery::mock(EventDispatcherInterface::class));
+        $dispatcher->shouldReceive('dispatch')->once()->with(ReleaseConnection::class)->andReturnUsing(function (ReleaseConnection $event) use (&$assert) {
+            $assert = $event->connection->getLastReleaseTime();
+        });
+
+        $connection = new ActiveConnectionStub($container, $pool = Mockery::mock(Pool::class));
+        $pool->shouldReceive('release')->withAnyArgs()->andReturnNull();
+        $pool->shouldReceive('getOption')->andReturn(new PoolOption(events: [ReleaseConnection::class]));
+
+        $connection->release();
+        $this->assertTrue($assert > 0);
+    }
+
+    public function testDontHaveEvents()
+    {
+        $container = Mockery::mock(ContainerInterface::class);
+        $container->shouldReceive('has')->with(StdoutLoggerInterface::class)->once()->andReturnFalse();
+        $container->shouldReceive('has')->with(EventDispatcherInterface::class)->andReturnTrue();
+        $container->shouldReceive('get')->with(EventDispatcherInterface::class)->andReturn($dispatcher = Mockery::mock(EventDispatcherInterface::class));
+        $dispatcher->shouldReceive('dispatch')->never()->with(ReleaseConnection::class)->andReturnNull();
+
+        $connection = new ActiveConnectionStub($container, $pool = Mockery::mock(Pool::class));
+        $pool->shouldReceive('release')->withAnyArgs()->andReturnNull();
+        $pool->shouldReceive('getOption')->andReturn(new PoolOption(events: []));
+
+        $connection->release();
+
+        $this->assertTrue(true);
     }
 }
