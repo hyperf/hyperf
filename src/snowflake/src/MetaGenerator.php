8b51296d23d7ac25333176999b4f8e66657b44fd
李铭昕 <715557344@qq.com>
2024-03-23 18:43:22 +0800

    f

--- src/snowflake/src/MetaGenerator.php
+++ src/snowflake/src/MetaGenerator.php
@@ -9,6 +9,7 @@ declare(strict_types=1);
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
+
 namespace Hyperf\Snowflake;
 
 use Hyperf\Snowflake\Exception\SnowflakeException;
