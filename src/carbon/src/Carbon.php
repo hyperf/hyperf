517518a02ab3ed7d5819c1930a3ad0ee62bacd74
李铭昕 <715557344@qq.com>
2024-03-23 17:37:09 +0800

    Format carbon

--- src/carbon/src/Carbon.php
+++ src/carbon/src/Carbon.php
@@ -9,6 +9,7 @@ declare(strict_types=1);
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
+
 namespace Hyperf\Carbon;
 
 use DateTime;
@@ -20,7 +21,7 @@ class Carbon extends \Carbon\Carbon
     /**
      * Create a Carbon instance from a given ordered UUID or ULID.
      */
-    public static function createFromId(Uuid|Ulid|string $id): static
+    public static function createFromId(string|Ulid|Uuid $id): static
     {
         if (is_string($id)) {
             $id = Ulid::isValid($id) ? Ulid::fromString($id) : Uuid::fromString($id);
