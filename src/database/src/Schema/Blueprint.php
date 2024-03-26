ccb4599256f41c97017fd51e49dfd3a9d3a63726
Chance <chance.fyii@gmail.com>
2024-03-26 17:21:33 +0800

    Adding the `computed` method to `Blueprint`

--- src/database/src/Schema/Blueprint.php
+++ src/database/src/Schema/Blueprint.php
@@ -1355,6 +1355,18 @@ class Blueprint
     }
 
     /**
+     * Create a new generated, computed column on the table.
+     *
+     * @param string $column
+     * @param string $expression
+     * @return \hyperf\Database\Schema\ColumnDefinition
+     */
+    public function computed($column, $expression)
+    {
+        return $this->addColumn('computed', $column, compact('expression'));
+    }
+
+    /**
      * Ensure the commands on the blueprint are valid for the connection type.
      *
      * @throws BadMethodCallException
