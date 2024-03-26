33b4065342a875a4de6bb47a61daf61d5487a436
Chance <chance.fyii@gmail.com>
2024-03-26 08:56:10 +0800

    fix testInsertGetIdWithEmptyValues

--- src/database-sqlserver/src/Query/Grammars/SqlServerGrammar.php
+++ src/database-sqlserver/src/Query/Grammars/SqlServerGrammar.php
@@ -221,6 +221,36 @@ class SqlServerGrammar extends Grammar
     }
 
     /**
+     * Compile an insert statement into SQL.
+     */
+    public function compileInsert(Builder $query, array $values): string
+    {
+        // Essentially we will force every insert to be treated as a batch insert which
+        // simply makes creating the SQL easier for us since we can utilize the same
+        // basic routine regardless of an amount of records given to us to insert.
+        $table = $this->wrapTable($query->from);
+
+        if (empty($values)) {
+            return "insert into {$table} default values";
+        }
+
+        if (! is_array(reset($values))) {
+            $values = [$values];
+        }
+
+        $columns = $this->columnize(array_keys(reset($values)));
+
+        // We need to build a list of parameter place-holders of values that are bound
+        // to the query. Each insert should have the exact same number of parameter
+        // bindings so we will loop through the record and parameterize them all.
+        $parameters = collect($values)->map(function ($record) {
+            return '(' . $this->parameterize($record) . ')';
+        })->implode(', ');
+
+        return "insert into {$table} ({$columns}) values {$parameters}";
+    }
+
+    /**
      * Compile the "select *" portion of the query.
      *
      * @param array $columns
