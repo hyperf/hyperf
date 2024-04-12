bb492c9fd0e7a204ce2d8766d182485bb48e0a9c
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2024-04-12 13:30:44 +0800

    Refactor `Hyperf\Coordinator` helper functions and add `Hyperf\Coordinator\resume` and `Hyperf\Coordinator\clear` helper functions

--- src/coordinator/src/Functions.php
+++ src/coordinator/src/Functions.php
@@ -12,16 +12,25 @@ declare(strict_types=1);
 
 namespace Hyperf\Coordinator;
 
+/**
+ * Block the current coroutine until the specified identifier is resumed.
+ */
 function block(float $timeout = -1, string $identifier = Constants::WORKER_EXIT): bool
 {
     return CoordinatorManager::until($identifier)->yield($timeout);
 }
 
+/**
+ * Resume the coroutine that is blocked by the specified identifier.
+ */
 function resume(string $identifier = Constants::WORKER_EXIT): void
 {
     CoordinatorManager::until($identifier)->resume();
 }
 
+/**
+ * Clear the coroutine that is blocked by the specified identifier.
+ */
 function clear(string $identifier = Constants::WORKER_EXIT): void
 {
     CoordinatorManager::clear($identifier);
