a9af8154ec79211f0f714f4d94070b2a7df6ec4e
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2024-04-12 13:14:36 +0800

    Added `Hyperf\Coordinator\block` helper function

---
+++ src/coordinator/src/Functions.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+
+namespace Hyperf\Coordinator;
+
+/**
+ * @param null|callable(bool $isWorkerExited) $callback
+ */
+function block(float $timeout = -1, ?callable $callback = null, string $identifier = Constants::WORKER_EXIT): void
+{
+    $isWorkerExited = CoordinatorManager::until($identifier)->yield($timeout);
+
+    if ($callback) {
+        $callback($isWorkerExited);
+    }
+}
