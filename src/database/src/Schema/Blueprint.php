9ce68e4248ea1c3c23c6dafc314941cfcfc6fa6a
Morrios <morrios@163.com>
2024-05-16 10:50:16 +0800

    Update the deleted_at field type to datetime.

--- src/database/src/Schema/Blueprint.php
+++ src/database/src/Schema/Blueprint.php
@@ -934,7 +934,7 @@ class Blueprint
     }
 
     /**
-     * Add a "deleted at" timestamp for the table.
+     * Add a "deleted at" dateTime for the table.
      *
      * @param string $column
      * @param int $precision
@@ -942,11 +942,11 @@ class Blueprint
      */
     public function softDeletes($column = 'deleted_at', $precision = 0)
     {
-        return $this->timestamp($column, $precision)->nullable();
+        return $this->dateTime($column, $precision)->nullable();
     }
 
     /**
-     * Add a "deleted at" timestampTz for the table.
+     * Add a "deleted at" dateTimeTz for the table.
      *
      * @param string $column
      * @param int $precision
@@ -954,7 +954,7 @@ class Blueprint
      */
     public function softDeletesTz($column = 'deleted_at', $precision = 0)
     {
-        return $this->timestampTz($column, $precision)->nullable();
+        return $this->dateTimeTz($column, $precision)->nullable();
     }
 
     /**
