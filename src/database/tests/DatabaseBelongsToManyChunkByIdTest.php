f5e0dd65c3ee450a0c26756c54657264329d7a24
Zds <49744633+zds-s@users.noreply.github.com>
2024-06-06 21:37:07 +0800

    Added methods BuildsQueries::[chunkById|chunkByIdDesc]

---
+++ src/database/tests/DatabaseBelongsToManyChunkByIdTest.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace HyperfTest\Database;
+
+use Hyperf\Collection\Collection;
+use Hyperf\Context\ApplicationContext;
+use Hyperf\Contract\StdoutLoggerInterface;
+use Hyperf\Database\Connection;
+use Hyperf\Database\ConnectionResolver;
+use Hyperf\Database\ConnectionResolverInterface;
+use Hyperf\Database\Connectors\ConnectionFactory;
+use Hyperf\Database\Connectors\MySqlConnector;
+use Hyperf\Database\Model\Model;
+use Hyperf\Database\Model\Register;
+use Hyperf\Database\Schema\Builder;
+use Hyperf\DbConnection\Db;
+use Hyperf\Di\Container;
+use HyperfTest\Database\Stubs\ContainerStub;
+use Mockery;
+use PHPUnit\Framework\TestCase;
+use Psr\EventDispatcher\EventDispatcherInterface;
+
+class DatabaseBelongsToManyChunkByIdTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        $container = Mockery::mock(Container::class);
+        ApplicationContext::setContainer($container);
+
+        $container->allows('has')->andReturns(true);
+        $container->allows('has')->with(StdoutLoggerInterface::class)->andReturnFalse();
+        $container->allows('has')->with(EventDispatcherInterface::class)->andReturnFalse();
+        $container->allows('get')->with('db.connector.mysql')->andReturns(new MySqlConnector());
+        $connector = new ConnectionFactory($container);
+
+        $dbConfig = [
+            'driver' => 'mysql',
+            'host' => '127.0.0.1',
+            'database' => 'hyperf',
+            'username' => 'root',
+            'password' => '123456',
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => '',
+        ];
+
+        $connection = $connector->make($dbConfig, 'default');
+
+        $resolver = new ConnectionResolver(['default' => $connection]);
+
+        $container->allows('get')->with(ConnectionResolverInterface::class)->andReturns($resolver);
+        $container = ContainerStub::getContainer();
+        $db = new Db($container);
+        $container->allows('get')->with(Db::class)->andReturns($db);
+        Register::setConnectionResolver($resolver);
+        $this->createSchema();
+    }
+
+    protected function connection($connection = 'default'): Connection
+    {
+        return Register::getConnectionResolver()->connection($connection);
+    }
+
+    protected function schema($connection = 'default'): Builder
+    {
+        return $this->connection($connection)->getSchemaBuilder();
+    }
+
+    private function createSchema(): void
+    {
+        $this->schema()->create('users', function ($table) {
+            $table->increments('id');
+            $table->string('email')->unique();
+        });
+
+        $this->schema()->create('articles', function ($table) {
+            $table->increments('id');
+            $table->string('title');
+        });
+
+        $this->schema()->create('article_user', function ($table) {
+            $table->increments('id');
+            $table->integer('article_id')->unsigned();
+            $table->foreign('article_id')->references('id')->on('articles');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    protected function tearDown(): void
+    {
+        $this->schema()->drop('article_user');
+        $this->schema()->drop('articles');
+        $this->schema()->drop('users');
+    }
+
+    protected function seedData()
+    {
+        $user = BelongsToManyChunkByIdTestTestUser::create(['id' => 1, 'email' => 'taylorotwell@gmail.com']);
+        $data = [
+            ['id' => 1, 'title' => 'Another title'],
+            ['id' => 2, 'title' => 'Another title'],
+            ['id' => 3, 'title' => 'Another title'],
+        ];
+        foreach ($data as $item) {
+            $user->articles()->create($item);
+        }
+    }
+
+    public function testBelongsToChunkById(): void
+    {
+        $this->seedData();
+
+        $user = BelongsToManyChunkByIdTestTestUser::query()->first();
+        $i = 0;
+
+        $user->articles()->chunkById(1, function (Collection $collection) use (&$i) {
+            $i++;
+            $this->assertEquals($i, $collection->first()->id);
+        },'articles.id','id');
+
+        $this->assertSame(3, $i);
+    }
+
+    public function testBelongsToChunkByIdDesc(): void
+    {
+        $this->seedData();
+
+        $user = BelongsToManyChunkByIdTestTestUser::query()->first();
+        $i = 0;
+
+        $user->articles()->chunkByIdDesc(1, function (Collection $collection) use (&$i) {
+            $this->assertEquals(3 - $i, $collection->first()->id);
+            $i++;
+        },'articles.id','id');
+
+        $this->assertSame(3, $i);
+    }
+
+}
+
+class BelongsToManyChunkByIdTestTestUser extends Model
+{
+    protected ?string $table = 'users';
+    protected array $fillable = ['id', 'email'];
+    public bool $timestamps = false;
+
+    public function articles()
+    {
+        return $this->belongsToMany(BelongsToManyChunkByIdTestTestArticle::class, 'article_user', 'user_id', 'article_id');
+    }
+}
+
+class BelongsToManyChunkByIdTestTestArticle extends Model
+{
+    protected ?string $table = 'articles';
+    protected string $keyType = 'string';
+    public bool $incrementing = false;
+    public bool $timestamps = false;
+    protected array $fillable = ['id', 'title'];
+}
