7a0a831f5422371b0ac3254ab63667e0d4395df5
ChenGuanQun <99912250@qq.com>
2024-06-18 10:07:41 +0800

    update

--- src/cache/src/Driver/RedisDriver.php
+++ src/cache/src/Driver/RedisDriver.php
@@ -9,151 +9,153 @@ declare(strict_types=1);
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
+
 namespace Hyperf\Cache\Driver;
 
 use Hyperf\Cache\Exception\InvalidArgumentException;
 use Hyperf\Redis\Redis;
+use Hyperf\Redis\RedisFactory;
 use Psr\Container\ContainerInterface;
 
 class RedisDriver extends Driver implements KeyCollectorInterface
 {
-    protected Redis $redis;
+  protected Redis $redis;
 
-    public function __construct(ContainerInterface $container, array $config)
-    {
-        parent::__construct($container, $config);
+  public function __construct(ContainerInterface $container, array $config)
+  {
+    parent::__construct($container, $config);
 
-        $this->redis = $container->get(Redis::class);
+    $this->redis = $container->get(RedisFactory::class)->get($config['options']['poolName']  ?? 'default');
+  }
+
+  public function get($key, $default = null): mixed
+  {
+    $res = $this->redis->get($this->getCacheKey($key));
+    if ($res === false) {
+      return $default;
     }
 
-    public function get($key, $default = null): mixed
-    {
-        $res = $this->redis->get($this->getCacheKey($key));
-        if ($res === false) {
-            return $default;
-        }
+    return $this->packer->unpack($res);
+  }
 
-        return $this->packer->unpack($res);
+  public function fetch(string $key, $default = null): array
+  {
+    $res = $this->redis->get($this->getCacheKey($key));
+    if ($res === false) {
+      return [false, $default];
     }
 
-    public function fetch(string $key, $default = null): array
-    {
-        $res = $this->redis->get($this->getCacheKey($key));
-        if ($res === false) {
-            return [false, $default];
-        }
+    return [true, $this->packer->unpack($res)];
+  }
 
-        return [true, $this->packer->unpack($res)];
+  public function set($key, $value, $ttl = null): bool
+  {
+    $seconds = $this->secondsUntil($ttl);
+    $res = $this->packer->pack($value);
+    if ($seconds > 0) {
+      return $this->redis->set($this->getCacheKey($key), $res, $seconds);
     }
 
-    public function set($key, $value, $ttl = null): bool
-    {
-        $seconds = $this->secondsUntil($ttl);
-        $res = $this->packer->pack($value);
-        if ($seconds > 0) {
-            return $this->redis->set($this->getCacheKey($key), $res, $seconds);
-        }
+    return $this->redis->set($this->getCacheKey($key), $res);
+  }
 
-        return $this->redis->set($this->getCacheKey($key), $res);
+  public function delete($key): bool
+  {
+    return (bool) $this->redis->del($this->getCacheKey($key));
+  }
+
+  public function clear(): bool
+  {
+    return $this->clearPrefix('');
+  }
+
+  public function getMultiple($keys, $default = null): iterable
+  {
+    $cacheKeys = array_map(function ($key) {
+      return $this->getCacheKey($key);
+    }, $keys);
+
+    $values = $this->redis->mget($cacheKeys);
+    $result = [];
+    foreach ($keys as $i => $key) {
+      $result[$key] = $values[$i] === false ? $default : $this->packer->unpack($values[$i]);
     }
 
-    public function delete($key): bool
-    {
-        return (bool) $this->redis->del($this->getCacheKey($key));
+    return $result;
+  }
+
+  public function setMultiple($values, $ttl = null): bool
+  {
+    if (!is_array($values)) {
+      throw new InvalidArgumentException('The values is invalid!');
     }
 
-    public function clear(): bool
-    {
-        return $this->clearPrefix('');
+    $cacheKeys = [];
+    foreach ($values as $key => $value) {
+      $cacheKeys[$this->getCacheKey($key)] = $this->packer->pack($value);
     }
 
-    public function getMultiple($keys, $default = null): iterable
-    {
-        $cacheKeys = array_map(function ($key) {
-            return $this->getCacheKey($key);
-        }, $keys);
+    $seconds = $this->secondsUntil($ttl);
+    if ($seconds > 0) {
+      foreach ($cacheKeys as $key => $value) {
+        $this->redis->set($key, $value, $seconds);
+      }
 
-        $values = $this->redis->mget($cacheKeys);
-        $result = [];
-        foreach ($keys as $i => $key) {
-            $result[$key] = $values[$i] === false ? $default : $this->packer->unpack($values[$i]);
-        }
-
-        return $result;
+      return true;
     }
 
-    public function setMultiple($values, $ttl = null): bool
-    {
-        if (! is_array($values)) {
-            throw new InvalidArgumentException('The values is invalid!');
-        }
+    return $this->redis->mset($cacheKeys);
+  }
 
-        $cacheKeys = [];
-        foreach ($values as $key => $value) {
-            $cacheKeys[$this->getCacheKey($key)] = $this->packer->pack($value);
-        }
+  public function deleteMultiple($keys): bool
+  {
+    $cacheKeys = array_map(function ($key) {
+      return $this->getCacheKey($key);
+    }, $keys);
 
-        $seconds = $this->secondsUntil($ttl);
-        if ($seconds > 0) {
-            foreach ($cacheKeys as $key => $value) {
-                $this->redis->set($key, $value, $seconds);
-            }
+    return (bool) $this->redis->del(...$cacheKeys);
+  }
 
-            return true;
-        }
+  public function has($key): bool
+  {
+    return (bool) $this->redis->exists($this->getCacheKey($key));
+  }
 
-        return $this->redis->mset($cacheKeys);
+  public function clearPrefix(string $prefix): bool
+  {
+    $iterator = null;
+    $key = $prefix . '*';
+    while (true) {
+      $keys = $this->redis->scan($iterator, $this->getCacheKey($key), 10000);
+      if (!empty($keys)) {
+        $this->redis->del(...$keys);
+      }
+
+      if (empty($iterator)) {
+        break;
+      }
     }
 
-    public function deleteMultiple($keys): bool
-    {
-        $cacheKeys = array_map(function ($key) {
-            return $this->getCacheKey($key);
-        }, $keys);
+    return true;
+  }
 
-        return (bool) $this->redis->del(...$cacheKeys);
-    }
+  public function addKey(string $collector, string $key): bool
+  {
+    return (bool) $this->redis->sAdd($this->getCacheKey($collector), $key);
+  }
 
-    public function has($key): bool
-    {
-        return (bool) $this->redis->exists($this->getCacheKey($key));
-    }
+  public function keys(string $collector): array
+  {
+    return $this->redis->sMembers($this->getCacheKey($collector));
+  }
 
-    public function clearPrefix(string $prefix): bool
-    {
-        $iterator = null;
-        $key = $prefix . '*';
-        while (true) {
-            $keys = $this->redis->scan($iterator, $this->getCacheKey($key), 10000);
-            if (! empty($keys)) {
-                $this->redis->del(...$keys);
-            }
+  public function delKey(string $collector, string ...$key): bool
+  {
+    return (bool) $this->redis->sRem($this->getCacheKey($collector), ...$key);
+  }
 
-            if (empty($iterator)) {
-                break;
-            }
-        }
-
-        return true;
-    }
-
-    public function addKey(string $collector, string $key): bool
-    {
-        return (bool) $this->redis->sAdd($this->getCacheKey($collector), $key);
-    }
-
-    public function keys(string $collector): array
-    {
-        return $this->redis->sMembers($this->getCacheKey($collector));
-    }
-
-    public function delKey(string $collector, string ...$key): bool
-    {
-        return (bool) $this->redis->sRem($this->getCacheKey($collector), ...$key);
-    }
-
-    public function getConnection(): mixed
-    {
-        return $this->redis;
-    }
+  public function getConnection(): mixed
+  {
+    return $this->redis;
+  }
 }
