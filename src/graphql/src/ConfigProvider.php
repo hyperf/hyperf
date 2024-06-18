0845225bbbebd7ef09dc6261f3dc71baba35de6b
李铭昕 <715557344@qq.com>
2024-06-18 17:49:44 +0800

    Removed useless code

--- src/graphql/src/ConfigProvider.php
+++ src/graphql/src/ConfigProvider.php
@@ -13,15 +13,6 @@ declare(strict_types=1);
 namespace Hyperf\GraphQL;
 
 use GraphQL\Type\Schema;
-use TheCodingMachine\GraphQLite\Mappers\RecursiveTypeMapperInterface;
-use TheCodingMachine\GraphQLite\Mappers\Root\RootTypeMapperInterface;
-use TheCodingMachine\GraphQLite\NamingStrategy;
-use TheCodingMachine\GraphQLite\NamingStrategyInterface;
-use TheCodingMachine\GraphQLite\QueryProviderInterface;
-use TheCodingMachine\GraphQLite\Security\AuthenticationServiceInterface;
-use TheCodingMachine\GraphQLite\Security\AuthorizationServiceInterface;
-use TheCodingMachine\GraphQLite\Security\FailAuthenticationService;
-use TheCodingMachine\GraphQLite\Security\FailAuthorizationService;
 
 class ConfigProvider
 {
@@ -30,20 +21,11 @@ class ConfigProvider
         return [
             'dependencies' => [
                 Schema::class => SchemaFactory::class,
-                // QueryProviderInterface::class => QueryProvider::class,
-                // RecursiveTypeMapperInterface::class => RecursiveTypeMapperFactory::class,
-                // AuthenticationServiceInterface::class => FailAuthenticationService::class,
-                // AuthorizationServiceInterface::class => FailAuthorizationService::class,
-                // NamingStrategyInterface::class => NamingStrategy::class,
-                // RootTypeMapperInterface::class => RootTypeMapperFactory::class
             ],
             'annotations' => [
                 'scan' => [
                     'paths' => [
                         __DIR__,
-                    ],
-                    'collectors' => [
-                        ClassCollector::class,
                     ],
                 ],
             ],
