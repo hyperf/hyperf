f6eaae6ba9f60bba4637d2d389e72da06cc4b03f
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2024-06-22 22:40:02 +0800

    chore: Update PHPStorm meta file to include SymfonyConsoleApplication mapping

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -9,7 +9,13 @@ declare(strict_types=1);
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
+
 namespace PHPSTORM_META;
+
+use Hyperf\Context\Context;
+use Hyperf\Contract\ApplicationInterface;
+use Psr\Container\ContainerInterface;
+use Symfony\Component\Console\Application as SymfonyConsoleApplication;
 
 use function di;
 use function make;
@@ -17,11 +23,17 @@ use function optional;
 use function tap;
 
 // Reflect
-override(\Psr\Container\ContainerInterface::get(0), map(['' => '@']));
-override(\Hyperf\Context\Context::get(0), map(['' => '@']));
+override(ContainerInterface::get(0), map([
+    '' => '@',
+    ApplicationInterface::class => SymfonyConsoleApplication::class,
+]));
+override(Context::get(0), map(['' => '@']));
 override(make(0), map(['' => '@']));
 override(\Hyperf\Support\make(0), map(['' => '@']));
-override(di(0), map(['' => '@']));
+override(di(0), map([
+    '' => '@',
+    ApplicationInterface::class => SymfonyConsoleApplication::class,
+]));
 override(optional(0), type(0));
 override(\Hyperf\Support\optional(0), type(0));
 override(tap(0), type(0));
