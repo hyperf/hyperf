62f40bd7ab8ca20f83bbb7e643d5c8e2b0131fa4
李铭昕 <715557344@qq.com>
2024-07-08 10:54:43 +0800

    Create ClassLoaderTest.php

---
+++ src/di/tests/ClassLoaderTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+
+namespace HyperfTest\Di;
+
+use Hyperf\Di\ClassLoader;
+use PHPUnit\Framework\Attributes\CoversNothing;
+use PHPUnit\Framework\TestCase;
+use ReflectionClass;
+
+use function Hyperf\Support\env;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+#[CoversNothing]
+class ClassLoaderTest extends TestCase
+{
+    public function testDotEnv()
+    {
+        $class = new class() extends ClassLoader {
+            public function __construct()
+            {
+            }
+        };
+
+        $ref = new ReflectionClass($class);
+        $method = $ref->getMethod('loadDotenv');
+        $method->invoke($class);
+
+        $this->assertNotEquals('0.0.0', env('SW_VERSION'));
+        $this->assertSame('Hyperf', env('PHP_FRAMEWORK'));
+    }
+}
