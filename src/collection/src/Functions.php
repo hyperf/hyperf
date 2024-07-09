551554e9040161c93aa3c1cf2faa074e32c757ec
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2024-07-09 18:58:18 +0800

    Update collect function to include return type hint

--- src/collection/src/Functions.php
+++ src/collection/src/Functions.php
@@ -24,7 +24,7 @@ use Hyperf\Contract\Arrayable;
  * @param null|Arrayable<TKey, TValue>|iterable<TKey, TValue> $value
  * @return Collection<TKey, TValue>
  */
-function collect($value = [])
+function collect($value = []): Collection
 {
     return new Collection($value);
 }
