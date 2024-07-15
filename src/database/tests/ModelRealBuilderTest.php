783f507f5bd113ef8dd9b7370e3262f30cad54cf
@长久同学^v^ <littlezov@qq.com>
2024-07-16 02:59:36 +0800

    Added a query by with test case

--- src/database/tests/ModelRealBuilderTest.php
+++ src/database/tests/ModelRealBuilderTest.php
@@ -126,6 +126,17 @@ class ModelRealBuilderTest extends TestCase
         $this->assertTrue($hit);
     }
 
+    public function testQueryByWith()
+    {
+        $this->getContainer();
+
+        $sql = User::query()->orderByWith('book', 'created_at', 'asc')->toSql();
+        $this->assertSame('select * from `user` order by (select `group` from `created_at` where `book`.`user_id` = `user`.`id`) asc', $sql);
+
+        $desc = User::query()->orderByWithDesc('book', 'created_at')->toSql();
+        $this->assertSame('select * from `user` order by (select `group` from `created_at` where `book`.`user_id` = `user`.`id`) desc', $desc);
+    }
+
     public function testModelEnum()
     {
         $this->getContainer();
