a570a95edeca266fb859c43b0533a56e0bdcbe56
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2024-07-21 15:10:15 +0800

    improve: use Symfony's renderThrowable function to render command exceptions

--- src/command/src/Command.php
+++ src/command/src/Command.php
@@ -12,8 +12,6 @@ declare(strict_types=1);
 
 namespace Hyperf\Command;
 
-use Hyperf\Context\ApplicationContext;
-use Hyperf\Contract\ApplicationInterface;
 use Hyperf\Coroutine\Coroutine;
 use Psr\EventDispatcher\EventDispatcherInterface;
 use Swoole\ExitException;
@@ -232,8 +230,6 @@ abstract class Command extends SymfonyCommand
      */
     protected function renderThrowable(Throwable $exception, OutputInterface $output): void
     {
-        ApplicationContext::getContainer()
-            ->get(ApplicationInterface::class)
-            ->renderThrowable($exception, $output);
+        $this->getApplication()?->renderThrowable($exception, $output);
     }
 }
