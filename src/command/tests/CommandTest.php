e8eb4b13406c0e98aa888dd4637ddd02ce6a4396
Albert Chen <albert.cht@gmail.com>
2024-07-21 16:33:11 +0800

    feat: add renderThrowable assertions to unit test

--- src/command/tests/CommandTest.php
+++ src/command/tests/CommandTest.php
@@ -27,8 +27,10 @@ use PHPUnit\Framework\Attributes\Group;
 use PHPUnit\Framework\TestCase;
 use Psr\Container\ContainerInterface;
 use Psr\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\Console\Application as ConsoleApplication;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Throwable;
 
 /**
  * @internal
@@ -57,27 +59,40 @@ class CommandTest extends TestCase
         ApplicationContext::setContainer($container = Mockery::mock(ContainerInterface::class));
         $container->shouldReceive('has')->with(EventDispatcherInterface::class)->andReturnFalse();
 
+        $output = Mockery::mock(OutputInterface::class);
+        $application = Mockery::mock(ConsoleApplication::class);
+        $application->shouldReceive('renderThrowable')
+            ->with(Mockery::type(Throwable::class), $output)
+            ->times(1);
+        $application->shouldReceive('getHelperSet');
+
         /** @var FooExceptionCommand $command */
         $command = new ClassInvoker(new FooExceptionCommand('foo'));
+        $command->setApplication($application);
+        $command->setOutput($output);
         $input = Mockery::mock(InputInterface::class);
         $input->shouldReceive('getOption')->andReturnFalse();
-        $output = Mockery::mock(OutputInterface::class);
-        $output->shouldReceive('writeln')->withAnyArgs()->andReturnNull();
 
         $exitCode = $command->execute($input, $output);
         $this->assertSame(1, $exitCode);
 
         /** @var FooExitCommand $command */
         $command = new ClassInvoker(new FooExitCommand());
+        $command->setApplication($application);
+        $command->setOutput($output);
         $exitCode = $command->execute($input, $output);
         $this->assertSame(11, $exitCode);
 
         /** @var FooCommand $command */
         $command = new ClassInvoker(new FooCommand());
+        $command->setApplication($application);
+        $command->setOutput($output);
         $exitCode = $command->execute($input, $output);
         $this->assertSame(0, $exitCode);
 
         $command = new FooTraitCommand();
+        $command->setApplication($application);
+        $command->setOutput($output);
         $this->assertArrayHasKey(Foo::class, (fn () => $this->setUpTraits($input, $output))->call($command));
         $this->assertSame('foo', (fn () => $this->propertyFoo)->call($command));
     }
