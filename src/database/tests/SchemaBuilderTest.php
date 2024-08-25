45ab81c5ffaf93ed1e354345ebba833fe92ceb36
zds <49744633+zds-s@users.noreply.github.com>
2024-08-25 18:49:17 +0800

    Added methods whenTableHasColumn and whenTableDoesntHaveColumn to \Hyperf\Database\Schema\Schema

--- src/database/tests/SchemaBuilderTest.php
+++ src/database/tests/SchemaBuilderTest.php
@@ -62,6 +62,23 @@ class SchemaBuilderTest extends TestCase
         Schema::drop('baz');
     }
 
+    public function testWhenTableHasColumn(): void
+    {
+        Schema::create('foo', static function (Blueprint $table) {
+            $table->comment('This is a comment');
+            $table->increments('id');
+        });
+        Schema::whenTableDoesntHaveColumn('foo', 'name', static function (Blueprint $table) {
+            $table->string('name');
+        });
+        $this->assertTrue(Schema::hasColumn('foo', 'name'));
+        Schema::whenTableHasColumn('foo', 'name', static function (Blueprint $table) {
+            $table->dropColumn('name');
+        });
+        $this->assertFalse(Schema::hasColumn('foo', 'name'));
+        Schema::drop('foo');
+    }
+
     public function testViews(): void
     {
         Schema::create('view_1', static function (Blueprint $table) {
