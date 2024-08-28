ce54b4b3f3e55615902e775422034fcc1062e588
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2024-08-28 12:38:18 +0800

    Added `Hyperf\Framework\Logger\ConsoleLogger `

---
+++ src/framework/src/Logger/ConsoleLogger.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+
+namespace Hyperf\Framework\Logger;
+
+use Hyperf\Contract\StdoutLoggerInterface;
+use Symfony\Component\Console\Logger\ConsoleLogger as SymfonyConsoleLogger;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ConsoleLogger extends SymfonyConsoleLogger implements StdoutLoggerInterface
+{
+    public function __construct(?OutputInterface $output = null, array $verbosityLevelMap = [], array $formatLevelMap = [])
+    {
+        $verbosity = function (): int {
+            $argv = $_SERVER['argv'] ?? [];
+            $argv = is_string($argv) ? explode(' ', $argv) : $argv;
+
+            foreach ($argv as $arg) {
+                if ($arg === '-v') {
+                    return OutputInterface::VERBOSITY_VERBOSE;
+                }
+
+                if ($arg === '-vv') {
+                    return OutputInterface::VERBOSITY_VERY_VERBOSE;
+                }
+
+                if ($arg === '-vvv') {
+                    return OutputInterface::VERBOSITY_DEBUG;
+                }
+            }
+
+            return OutputInterface::VERBOSITY_NORMAL;
+        };
+        $output = $output ?? new ConsoleOutput($verbosity());
+
+        parent::__construct($output, $verbosityLevelMap, $formatLevelMap);
+    }
+}
