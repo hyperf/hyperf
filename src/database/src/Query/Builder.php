72c7d3d91071a4034fd0f830c864b1824e94a7bb
hexiangyu <hexiangyu@addcn.com>
2024-09-13 14:05:16 +0800

    Optimize database hyperf/collection dependent version.

--- src/database/src/Query/Builder.php
+++ src/database/src/Query/Builder.php
@@ -1818,7 +1818,7 @@ class Builder
     /**
      * Add a "group by" clause to the query.
      *
-     * @param array|string ...$groups
+     * @param array|string|Expression ...$groups
      * @return $this
      */
     public function groupBy(...$groups)
