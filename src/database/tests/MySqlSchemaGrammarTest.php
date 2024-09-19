15356e9a0c81edb4db97027a725650e84a4dc091
李铭昕 <715557344@qq.com>
2024-09-19 18:20:58 +0800

    Update MySqlSchemaGrammarTest.php

--- src/database/tests/MySqlSchemaGrammarTest.php
+++ src/database/tests/MySqlSchemaGrammarTest.php
@@ -66,26 +66,26 @@ class MySqlSchemaGrammarTest extends TestCase
         $this->assertSame('alter table `users` add `id` int unsigned not null auto_increment primary key, add `email` varchar(255) not null', $statements[0]);
     }
 
-    // public function testNullableMorphs(): void
-    // {
-    //     $blueprint = new Blueprint('users');
-    //     $blueprint->nullableMorphs('imageable');
-    //     $statements = $blueprint->toSql($this->getConnection(), $this->getGrammar());
-    //     $this->assertCount(2, $statements);
-    //     $this->assertSame('alter table `users` add `imageable_type` varchar(255) null, add `imageable_id` bigint unsigned null', $statements[0]);
-    //     $this->assertSame('alter table `users` add index `users_imageable_type_imageable_id_index`(`imageable_type`, `imageable_id`)', $statements[1]);
-    // }
-    //
-    // public function testNullableUuidMorphs(): void
-    // {
-    //     $blueprint = new Blueprint('users');
-    //     $blueprint->nullableUuidMorphs('imageable');
-    //     $statements = $blueprint->toSql($this->getConnection(), $this->getGrammar());
-    //     $this->assertCount(2, $statements);
-    //     $this->assertSame('alter table `users` add `imageable_type` varchar(255) null, add `imageable_id` char(36) null', $statements[0]);
-    //     $this->assertSame('alter table `users` add index `users_imageable_type_imageable_id_index`(`imageable_type`, `imageable_id`)', $statements[1]);
-    // }
-    //
+    public function testNullableMorphs(): void
+    {
+        $blueprint = new Blueprint('users');
+        $blueprint->nullableMorphs('imageable');
+        $statements = $blueprint->toSql($this->getConnection(), $this->getGrammar());
+        $this->assertCount(2, $statements);
+        $this->assertSame('alter table `users` add `imageable_type` varchar(255) null, add `imageable_id` bigint unsigned null', $statements[0]);
+        $this->assertSame('alter table `users` add index `users_imageable_type_imageable_id_index`(`imageable_type`, `imageable_id`)', $statements[1]);
+    }
+
+    public function testNullableUuidMorphs(): void
+    {
+        $blueprint = new Blueprint('users');
+        $blueprint->nullableUuidMorphs('imageable');
+        $statements = $blueprint->toSql($this->getConnection(), $this->getGrammar());
+        $this->assertCount(2, $statements);
+        $this->assertSame('alter table `users` add `imageable_type` varchar(255) null, add `imageable_id` char(36) null', $statements[0]);
+        $this->assertSame('alter table `users` add index `users_imageable_type_imageable_id_index`(`imageable_type`, `imageable_id`)', $statements[1]);
+    }
+
     // public function testUuidMorphs(): void
     // {
     //     $blueprint = new Blueprint('users');
