ef622d7e499168da1b33bae188d9b31b0e9f0c15
Albert Chen <albert.cht@gmail.com>
2024-09-21 16:36:58 +0800

    fix: fix compileUpdateColumns's arguments in compileUpdate

--- src/database/src/Query/Grammars/MySqlGrammar.php
+++ src/database/src/Query/Grammars/MySqlGrammar.php
@@ -102,7 +102,7 @@ class MySqlGrammar extends Grammar
         // Each one of the columns in the update statements needs to be wrapped in the
         // keyword identifiers, also a place-holder needs to be created for each of
         // the values in the list of bindings so we can make the sets statements.
-        $columns = $this->compileUpdateColumns($values);
+        $columns = $this->compileUpdateColumns($query, $values);
 
         // If the query has any "join" clauses, we will setup the joins on the builder
         // and compile them so we can attach them to this update, as update queries
