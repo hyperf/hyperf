a8f21f685edfc2197d4feb4efa8e251218f5bd65
Weslen Teche <weslen_teche@hotmail.com>
2024-10-11 05:58:16 -0300

    Added `Hyperf\Database\Schema\Blueprint::tinyText()`. (#7071)
    Co-authored-by: 李铭昕 <715557344@qq.com>

--- CHANGELOG-3.1.md
+++ CHANGELOG-3.1.md
@@ -1,5 +1,9 @@
 # v3.1.44 - TBD
 
+## Added
+
+- [#7071](https://github.com/hyperf/hyperf/pull/7071) Added `Hyperf\Database\Schema\Blueprint::tinyText()`.
+
 # v3.1.43 - 2024-10-10
 
 ## Fixed
--- src/db-connection/tests/Stubs/PDOStatementStubPHP8.php
+++ src/db-connection/tests/Stubs/PDOStatementStubPHP8.php
@@ -14,6 +14,7 @@ namespace HyperfTest\DbConnection\Stubs;
 
 use PDO;
 use PDOStatement;
+use ReturnTypeWillChange;
 
 class PDOStatementStubPHP8 extends PDOStatement
 {
@@ -24,98 +25,45 @@ class PDOStatementStubPHP8 extends PDOStatement
         $this->statement = $statement;
     }
 
-    public function execute($input_parameters = null): bool
+    #[ReturnTypeWillChange]
+    public function execute(?array $params = null): bool
     {
         return true;
     }
 
-    public function fetch($fetch_style = null, $cursor_orientation = PDO::FETCH_ORI_NEXT, $cursor_offset = 0): mixed
+    #[ReturnTypeWillChange]
+    public function fetch(int $mode = PDO::FETCH_DEFAULT, int $cursorOrientation = PDO::FETCH_ORI_NEXT, int $cursorOffset = 0): mixed
     {
-        return parent::fetch($fetch_style, $cursor_orientation, $cursor_offset);
+        return null;
     }
 
-    public function bindParam($parameter, &$variable, $data_type = PDO::PARAM_STR, $length = null, $driver_options = null): bool
+    #[ReturnTypeWillChange]
+    public function bindParam(int|string $param, mixed &$var, int $type = PDO::PARAM_STR, ?int $maxLength = 0, mixed $driverOptions = null): bool
     {
-        return parent::bindParam($parameter, $variable, $data_type, $length, $driver_options);
+        return true;
     }
 
-    public function bindColumn($column, &$param, $type = null, $maxlen = null, $driverdata = null): bool
+    #[ReturnTypeWillChange]
+    public function bindColumn(int|string $column, mixed &$var, int $type = PDO::PARAM_STR, int $maxLength = 0, mixed $driverOptions = null): bool
     {
-        return parent::bindColumn($column, $param, $type, $maxlen, $driverdata);
+        return true;
     }
 
-    public function bindValue($parameter, $value, $data_type = PDO::PARAM_STR): bool
+    #[ReturnTypeWillChange]
+    public function bindValue(int|string $param, mixed $value, int $type = PDO::PARAM_STR): bool
     {
-        return parent::bindValue($parameter, $value, $data_type);
+        return true;
     }
 
-    public function rowCount(): int
-    {
-        return parent::rowCount();
-    }
-
-    public function fetchColumn($column_number = 0): mixed
-    {
-        return parent::fetchColumn($column_number);
-    }
-
+    #[ReturnTypeWillChange]
     public function fetchAll(int $mode = PDO::FETCH_BOTH, mixed ...$args): array
     {
         return [];
     }
 
-    public function fetchObject($class_name = 'stdClass', $ctor_args = null): object
-    {
-        return parent::fetchObject($class_name, $ctor_args);
-    }
-
-    public function errorCode(): ?string
-    {
-        return parent::errorCode();
-    }
-
-    public function errorInfo(): array
-    {
-        return parent::errorInfo();
-    }
-
-    public function setAttribute($attribute, $value): bool
-    {
-        return parent::setAttribute($attribute, $value);
-    }
-
-    public function getAttribute($attribute): mixed
-    {
-        return parent::getAttribute($attribute);
-    }
-
-    public function columnCount(): int
-    {
-        return parent::columnCount();
-    }
-
-    public function getColumnMeta($column): array
-    {
-        return parent::getColumnMeta($column);
-    }
-
+    #[ReturnTypeWillChange]
     public function setFetchMode($mode, $className = null, ...$params)
     {
         return true;
-    }
-
-    public function nextRowset(): bool
-    {
-        return parent::nextRowset();
-    }
-
-    public function closeCursor(): bool
-    {
-        return parent::closeCursor();
-    }
-
-    public function debugDumpParams(): ?bool
-    {
-        return parent::debugDumpParams();
     }
 }
