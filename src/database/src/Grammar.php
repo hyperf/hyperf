757f2c9bcd6bc05bb76d2a29c8c9339f4fb3c0f2
李铭昕 <715557344@qq.com>
2024-10-11 16:36:54 +0800

    Update Grammar.php

--- src/database/src/Grammar.php
+++ src/database/src/Grammar.php
@@ -189,7 +189,6 @@ abstract class Grammar
      */
     protected function wrapSegments($segments): string
     {
-        var_dump($segments);
         return collect($segments)->map(function ($segment, $key) use ($segments) {
             return $key == 0 && count($segments) > 1
                 ? $this->wrapTable($segment)
