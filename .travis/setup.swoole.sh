c4a3d2fb34c83aa58f407809ad39112dcbeffd13
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2024-12-17 21:52:47 +0800

    fix

--- .travis/setup.swoole.sh
+++ .travis/setup.swoole.sh
@@ -10,13 +10,11 @@ if [ $? -eq 1 ]; then
     sudo mv pie.phar /usr/local/bin/pie
 
     # Upgrade dependencies
-    sudo apt-get clean
-    sudo apt-get update
-    sudo apt-get upgrade -f
+    sudo apt-get clean && sudo apt-get update && sudo apt-get upgrade -f
     sudo apt-get install libcurl4-openssl-dev libc-ares-dev libpq-dev
 
     # Install Swoole
-    pie install swoole/swoole:${SW_VERSION} --enable-openssl --enable-swoole-curl --enable-cares --enable-swoole-pgsql --enable-brotli
+    sudo pie install swoole/swoole:${SW_VERSION} --enable-openssl --enable-swoole-curl --enable-cares --enable-swoole-pgsql --enable-brotli
 
     # Add extension to php.ini
     sudo sh -c "echo extension=swoole > /etc/php/${PHP_VERSION}/cli/conf.d/swoole.ini"
