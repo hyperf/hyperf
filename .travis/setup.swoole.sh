8a15c78533a8d65fa72fbe5767d4b35827b93f22
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2024-12-17 21:33:54 +0800

    Remove useless comments

--- .travis/setup.swoole.sh
+++ .travis/setup.swoole.sh
@@ -16,16 +16,6 @@ if [ $? -eq 1 ]; then
     sudo apt-get install libcurl4-openssl-dev libc-ares-dev libpq-dev
 
     # Install Swoole
-    # wget https://github.com/swoole/swoole-src/archive/${SW_VERSION}.tar.gz -O swoole.tar.gz
-    # mkdir -p swoole
-    # tar -xf swoole.tar.gz -C swoole --strip-components=1
-    # rm swoole.tar.gz
-    # cd swoole
-    # phpize
-    # ./configure --enable-openssl --enable-swoole-curl --enable-cares --enable-swoole-pgsql --enable-brotli
-    # make -j$(nproc)
-    # sudo make install
-
     pie install swoole:${SW_VERSION} --enable-openssl --enable-swoole-curl --enable-cares --enable-swoole-pgsql --enable-brotli
 
     # Add extension to php.ini
