82d8d6a16ee3c24c59f67edc83500c0f342f8bce
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2024-12-26 15:15:06 +0800

    Added `Hyperf\AsyncQueue\dispatch_sync` helper.

--- src/async-queue/src/Functions.php
+++ src/async-queue/src/Functions.php
@@ -13,7 +13,14 @@ declare(strict_types=1);
 namespace Hyperf\AsyncQueue;
 
 use Hyperf\AsyncQueue\Driver\DriverFactory;
+use Hyperf\AsyncQueue\Event\AfterHandle;
+use Hyperf\AsyncQueue\Event\BeforeHandle;
+use Hyperf\AsyncQueue\Event\FailedHandle;
 use Hyperf\Context\ApplicationContext;
+use Hyperf\Contract\DispatcherInterface;
+use Throwable;
+
+use function Hyperf\Support\make;
 
 function dispatch(JobInterface $job, ?int $delay = null, ?int $maxAttempts = null, ?string $pool = null): bool
 {
@@ -25,4 +32,23 @@ function dispatch(JobInterface $job, ?int $delay = null, ?int $maxAttempts = nul
         ->get(DriverFactory::class)
         ->get($pool ?? 'default')
         ->push($job, $delay ?? 0);
+}
+
+function dispatch_sync(JobInterface $job): mixed
+{
+    $container = ApplicationContext::getContainer();
+    $dispatcher = $container->has(DispatcherInterface::class)
+        ? $container->get(DispatcherInterface::class)
+        : null;
+    $message = make(JobMessage::class, [$job]);
+    try {
+        $dispatcher?->dispatch(new BeforeHandle($message));
+        $result = $message->job()->handle();
+        $dispatcher?->dispatch(new AfterHandle($message));
+    } catch (Throwable $exception) {
+        $dispatcher?->dispatch(new FailedHandle($message, $exception));
+        return false;
+    }
+
+    return $result;
 }
