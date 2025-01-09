07da477ffd8d3331b0c0e3eafd7b43b5e97a26a7
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2025-01-09 18:20:39 +0800

    Adds `finally` method to pipeline helper

--- src/pipeline/tests/PipelineTest.php
+++ src/pipeline/tests/PipelineTest.php
@@ -241,6 +241,123 @@ class PipelineTestPipeOne
     {
         return $next(++$piped);
     }
+
+    public function testPipelineFinally()
+    {
+        $pipeTwo = function ($piped, $next) {
+            $_SERVER['__test.pipe.two'] = $piped;
+
+            $next($piped);
+        };
+
+        $result = (new Pipeline(new Container()))
+            ->send('foo')
+            ->through([PipelineTestPipeOne::class, $pipeTwo])
+            ->finally(function ($piped) {
+                $_SERVER['__test.pipe.finally'] = $piped;
+            })
+            ->then(function ($piped) {
+                return $piped;
+            });
+
+        $this->assertSame(null, $result);
+        $this->assertSame('foo', $_SERVER['__test.pipe.one']);
+        $this->assertSame('foo', $_SERVER['__test.pipe.two']);
+        $this->assertSame('foo', $_SERVER['__test.pipe.finally']);
+
+        unset($_SERVER['__test.pipe.one'], $_SERVER['__test.pipe.two'], $_SERVER['__test.pipe.finally']);
+    }
+
+    public function testPipelineFinallyMethodWhenChainIsStopped()
+    {
+        $pipeTwo = function ($piped) {
+            $_SERVER['__test.pipe.two'] = $piped;
+        };
+
+        $result = (new Pipeline(new Container()))
+            ->send('foo')
+            ->through([PipelineTestPipeOne::class, $pipeTwo])
+            ->finally(function ($piped) {
+                $_SERVER['__test.pipe.finally'] = $piped;
+            })
+            ->then(function ($piped) {
+                return $piped;
+            });
+
+        $this->assertSame(null, $result);
+        $this->assertSame('foo', $_SERVER['__test.pipe.one']);
+        $this->assertSame('foo', $_SERVER['__test.pipe.two']);
+        $this->assertSame('foo', $_SERVER['__test.pipe.finally']);
+
+        unset($_SERVER['__test.pipe.one'], $_SERVER['__test.pipe.two'], $_SERVER['__test.pipe.finally']);
+    }
+
+    public function testPipelineFinallyOrder()
+    {
+        $std = new stdClass();
+
+        $result = (new Pipeline(new Container()))
+            ->send($std)
+            ->through([
+                function ($std, $next) {
+                    $std->value = 1;
+
+                    return $next($std);
+                },
+                function ($std, $next) {
+                    ++$std->value;
+
+                    return $next($std);
+                },
+            ])->finally(function ($std) {
+                $this->assertSame(3, $std->value);
+
+                ++$std->value;
+            })->then(function ($std) {
+                ++$std->value;
+
+                return $std;
+            });
+
+        $this->assertSame(4, $std->value);
+        $this->assertSame(4, $result->value);
+    }
+
+    public function testPipelineFinallyWhenExceptionOccurs()
+    {
+        $std = new stdClass();
+
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('My Exception: 1');
+
+        try {
+            (new Pipeline(new Container()))
+                ->send($std)
+                ->through([
+                    function ($std, $next) {
+                        $std->value = 1;
+
+                        return $next($std);
+                    },
+                    function ($std) {
+                        throw new Exception('My Exception: ' . $std->value);
+                    },
+                ])->finally(function ($std) {
+                    $this->assertSame(1, $std->value);
+
+                    ++$std->value;
+                })->then(function ($std) {
+                    $std->value = 0;
+
+                    return $std;
+                });
+        } catch (\Exception $e) {
+            $this->assertSame('My Exception: 1', $e->getMessage());
+            $this->assertSame(2, $std->value);
+
+            throw $e;
+        }
+    }
 }
 
 class PipelineTestPipeTwo
