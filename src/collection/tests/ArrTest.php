cc9c117760f20857a5e473a3a3e22b682d66a8b8
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2025-03-14 22:20:52 +0800

    Adds tests

--- src/collection/tests/ArrTest.php
+++ src/collection/tests/ArrTest.php
@@ -22,6 +22,7 @@ use Monolog\Level;
 use PHPUnit\Framework\Attributes\CoversNothing;
 use PHPUnit\Framework\TestCase;
 use Random\Engine\Mt19937;
+use Random\Engine\Secure;
 use stdClass;
 
 /**
@@ -234,6 +235,11 @@ class ArrTest extends TestCase
         $this->assertSame(
             Arr::shuffle(range(0, 100, 10), new Mt19937(1234)),
             Arr::shuffle(range(0, 100, 10), new Mt19937(1234))
+        );
+
+        $this->assertNotSame(
+            Arr::shuffle(range(0, 100, 10), new Secure()),
+            Arr::shuffle(range(0, 100, 10), new Secure())
         );
     }
 
