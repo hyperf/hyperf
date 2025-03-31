10bfa8ce53059c3eac0bc7b600665c308371610d
M0Yi <moyi@mymoyi.cn>
2025-03-31 16:54:34 +0800

    移除冗余内容

--- src/filesystem/src/Adapter/S3AdapterFactory.php
+++ src/filesystem/src/Adapter/S3AdapterFactory.php
@@ -17,9 +17,7 @@ use Aws\S3\S3Client;
 use GuzzleHttp\Client;
 use GuzzleHttp\HandlerStack;
 use Hyperf\Filesystem\Contract\AdapterFactoryInterface;
-use Hyperf\Filesystem\Version;
 use Hyperf\Guzzle\CoroutineHandler;
-use League\Flysystem\AwsS3v3\AwsS3Adapter;
 use League\Flysystem\AwsS3V3\AwsS3V3Adapter;
 
 class S3AdapterFactory implements AdapterFactoryInterface
@@ -31,10 +29,6 @@ class S3AdapterFactory implements AdapterFactoryInterface
         ]));
         $options = array_merge($options, ['http_handler' => $handler]);
         $client = new S3Client($options);
-
-        if (Version::isV2()) {
-            return new AwsS3V3Adapter($client, $options['bucket_name'], '');
-        }
-        return new AwsS3Adapter($client, $options['bucket_name'], '', ['override_visibility_on_copy' => true]);
+        return new AwsS3V3Adapter($client, $options['bucket_name'], '');
     }
 }
