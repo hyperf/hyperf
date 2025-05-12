6a528393b260178ea5ee2c55ce9f9f4f9c1454ef
李铭昕 <715557344@qq.com>
2025-05-12 09:51:04 +0800

    Create PostgresPdoDriver.php

---
+++ src/database-pgsql/src/DBAL/PostgresPdoDriver.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Hyperf\Database\PgSQL\DBAL;
+
+class PostgresPdoDriver
+{
+
+}
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+
+namespace Hyperf\Database\PgSQL\DBAL;
+
+use Doctrine\DBAL\Driver\AbstractPostgreSQLDriver;
+use Hyperf\Database\DBAL\Concerns\ConnectsToDatabase;
+
+class PostgresPdoDriver extends AbstractPostgreSQLDriver
+{
+    use ConnectsToDatabase;
+}
