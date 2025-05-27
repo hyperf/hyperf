46ca196b76bfbd3843e8559e48116f693a315785
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2025-05-27 12:15:23 +0000

    Add context key setter and getter to RedisConnection

--- src/redis/src/RedisConnection.php
+++ src/redis/src/RedisConnection.php
@@ -39,6 +39,8 @@ class RedisConnection extends BaseConnection implements ConnectionInterface
 
     protected ?EventDispatcherInterface $eventDispatcher = null;
 
+    protected ?string $contextKey = null;
+
     protected array $config = [
         'host' => 'localhost',
         'port' => 6379,
@@ -328,5 +330,15 @@ class RedisConnection extends BaseConnection implements ConnectionInterface
         if ($this->container->has(StdoutLoggerInterface::class) && $logger = $this->container->get(StdoutLoggerInterface::class)) {
             $logger->log($level, $message);
         }
+    }
+
+    public function setContextKey(string $key): void
+    {
+        $this->contextKey = $key;
+    }
+
+    public function getContextKey(): ?string
+    {
+        return $this->contextKey;
     }
 }
