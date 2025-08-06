67c881bc3aaf1abe80750458055cc8dfce07f8a1
zds <49744633+zds-s@users.noreply.github.com>
2025-08-06 11:41:37 +0800

    Added `except` method to `Hyperf\Database\Query\Builder` to exclude specified models from query results

--- src/database/src/Query/Builder.php
+++ src/database/src/Query/Builder.php
@@ -28,6 +28,7 @@ use Hyperf\Database\Concerns\ExplainsQueries;
 use Hyperf\Database\ConnectionInterface;
 use Hyperf\Database\Exception\InvalidBindingException;
 use Hyperf\Database\Model\Builder as ModelBuilder;
+use Hyperf\Database\Model\Model;
 use Hyperf\Database\Model\Relations\Relation;
 use Hyperf\Database\Query\Grammars\Grammar;
 use Hyperf\Database\Query\Processors\Processor;
@@ -1914,6 +1915,21 @@ class Builder
         $this->addBinding($bindings, 'having');
 
         return $this;
+    }
+
+    /**
+     * Exclude the given models from the query results.
+     *
+     * @param  iterable|mixed  $models
+     * @return static
+     */
+    public function except(mixed $models): static
+    {
+        return $this->whereKeyNot(
+            $models instanceof Model
+                ? $models->getKey()
+                : Collection::wrap($models)->modelKeys()
+        );
     }
 
     /**
