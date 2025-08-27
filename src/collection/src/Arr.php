60272b961fa4f4f6d99e5e5104c950537b971809
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2025-08-27 18:41:35 +0800

    使用 array_first 和 array_last 替代 reset 和 end 函数，以增强数组处理的安全性

--- src/collection/src/Arr.php
+++ src/collection/src/Arr.php
@@ -790,7 +790,7 @@ class Arr
         }
 
         if (count($array) === 1) {
-            return end($array);
+            return array_last($array);
         }
 
         $finalItem = array_pop($array);
