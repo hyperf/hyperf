cf5152f8f55edc72954823214e418fc7c76495f1
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2025-10-16 08:50:49 +0800

    fix(logger): update log level to use Monolog Level enum

--- src/logger/src/LoggerFactory.php
+++ src/logger/src/LoggerFactory.php
@@ -20,6 +20,7 @@ use Monolog\Formatter\LineFormatter;
 use Monolog\Handler\FormattableHandlerInterface;
 use Monolog\Handler\HandlerInterface;
 use Monolog\Handler\StreamHandler;
+use Monolog\Level;
 use Psr\Container\ContainerInterface;
 use Psr\Log\LoggerInterface;
 
@@ -88,7 +89,7 @@ class LoggerFactory
         $handlerClass = Arr::get($config, 'handler.class', StreamHandler::class);
         $handlerConstructor = Arr::get($config, 'handler.constructor', [
             'stream' => BASE_PATH . '/runtime/logs/hyperf.log',
-            'level' => Logger::DEBUG,
+            'level' => Level::Debug,
         ]);
 
         return [
