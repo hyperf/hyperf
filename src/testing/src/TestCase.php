198a37bf38285499a77ff2fe725b2aeb7378c9ac
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2025-10-25 17:17:10 +0800

    移除 TestCase 类中的 RunTestsInCoroutine 特性

--- src/testing/src/TestCase.php
+++ src/testing/src/TestCase.php
@@ -26,7 +26,6 @@ abstract class TestCase extends \PHPUnit\Framework\TestCase
     use Concerns\InteractsWithContainer;
     use Concerns\InteractsWithModelFactory;
     use Concerns\MakesHttpRequests;
-    use Concerns\RunTestsInCoroutine;
     use Concerns\InteractsWithDatabase;
 
     /**
