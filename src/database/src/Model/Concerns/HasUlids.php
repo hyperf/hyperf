25c5724444df19f6c54bf44f28d94537f4b2bde4
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2025-11-01 21:07:45 +0800

    refactor(database): extract HasUniqueStringIds trait to reduce code duplication
    Extracted common methods from HasUuids and HasUlids traits into a new HasUniqueStringIds trait to improve code maintainability and reduce duplication. The new trait contains shared logic for:
    - uniqueIds() method to get columns receiving unique identifiers
    - getKeyType() method to determine the key type based on unique IDs
    - getIncrementing() method to determine if IDs are auto-incrementing
    
    This refactoring makes the codebase more DRY and easier to maintain.

--- src/database/src/Model/Concerns/HasUlids.php
+++ src/database/src/Model/Concerns/HasUlids.php
@@ -16,6 +16,8 @@ use Hyperf\Stringable\Str;
 
 trait HasUlids
 {
+    use HasUniqueStringIds;
+
     /**
      * Generate a new ULID for the model.
      *
@@ -24,43 +26,5 @@ trait HasUlids
     public function newUniqueId()
     {
         return strtolower((string) Str::ulid());
-    }
-
-    /**
-     * Get the columns that should receive a unique identifier.
-     *
-     * @return array
-     */
-    public function uniqueIds()
-    {
-        return [$this->getKeyName()];
-    }
-
-    /**
-     * Get the auto-incrementing key type.
-     *
-     * @return string
-     */
-    public function getKeyType()
-    {
-        if (in_array($this->getKeyName(), $this->uniqueIds())) {
-            return 'string';
-        }
-
-        return $this->keyType;
-    }
-
-    /**
-     * Get the value indicating whether the IDs are incrementing.
-     *
-     * @return bool
-     */
-    public function getIncrementing()
-    {
-        if (in_array($this->getKeyName(), $this->uniqueIds())) {
-            return false;
-        }
-
-        return $this->incrementing;
     }
 }
--- src/database/src/Model/Concerns/HasUuids.php
+++ src/database/src/Model/Concerns/HasUuids.php
@@ -16,6 +16,8 @@ use Hyperf\Stringable\Str;
 
 trait HasUuids
 {
+    use HasUniqueStringIds;
+
     /**
      * Generate a new UUID for the model.
      *
@@ -24,43 +26,5 @@ trait HasUuids
     public function newUniqueId()
     {
         return (string) Str::orderedUuid();
-    }
-
-    /**
-     * Get the columns that should receive a unique identifier.
-     *
-     * @return array
-     */
-    public function uniqueIds()
-    {
-        return [$this->getKeyName()];
-    }
-
-    /**
-     * Get the auto-incrementing key type.
-     *
-     * @return string
-     */
-    public function getKeyType()
-    {
-        if (in_array($this->getKeyName(), $this->uniqueIds())) {
-            return 'string';
-        }
-
-        return $this->keyType;
-    }
-
-    /**
-     * Get the value indicating whether the IDs are incrementing.
-     *
-     * @return bool
-     */
-    public function getIncrementing()
-    {
-        if (in_array($this->getKeyName(), $this->uniqueIds())) {
-            return false;
-        }
-
-        return $this->incrementing;
     }
 }
