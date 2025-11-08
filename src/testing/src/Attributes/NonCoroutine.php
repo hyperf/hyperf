d9a640d4d05ec8f3d412d7691944aaf4ec01a3af
Deeka Wong <huangdijia@gmail.com>
2025-11-08 11:24:22 +0800

    refactor(testing): add NonCoroutine attribute for flexible test execution control (#7605)

---
+++ src/testing/src/Attributes/NonCoroutine.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+
+namespace Hyperf\Testing\Attributes;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_METHOD | Attribute::TARGET_CLASS)]
+class NonCoroutine
+{
+}
