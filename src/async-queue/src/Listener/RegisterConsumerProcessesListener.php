a77b05ca679923acfc0f4266daf544c75b9e566f
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2025-11-12 13:29:42 +0800

    feat(async-queue): implement ConsumerManager to manage consumer processes

--- src/async-queue/src/Listener/RegisterConsumerProcessesListener.php
+++ src/async-queue/src/Listener/RegisterConsumerProcessesListener.php
@@ -12,12 +12,9 @@ declare(strict_types=1);
 
 namespace Hyperf\AsyncQueue\Listener;
 
-use Hyperf\AsyncQueue\Driver\DriverFactory;
-use Hyperf\AsyncQueue\Process\ConsumerProcess;
-use Hyperf\Contract\ConfigInterface;
+use Hyperf\AsyncQueue\ConsumerManager;
 use Hyperf\Event\Contract\ListenerInterface;
 use Hyperf\Framework\Event\BeforeMainServerStart;
-use Hyperf\Process\ProcessManager;
 use Psr\Container\ContainerInterface;
 
 class RegisterConsumerProcessesListener implements ListenerInterface
@@ -35,38 +32,6 @@ class RegisterConsumerProcessesListener implements ListenerInterface
 
     public function process(object $event): void
     {
-        $config = $this->container->get(ConfigInterface::class);
-        $pools = $config->get('async_queue', []);
-
-        foreach ($pools as $pool => $config) {
-            if (! ($config['enable'] ?? false)) {
-                continue;
-            }
-
-            $this->createProcess($pool, $config);
-        }
-    }
-
-    protected function createProcess(string $pool, array $config): void
-    {
-        $process = new class($this->container, $pool, $config) extends ConsumerProcess {
-            public function __construct(
-                protected ContainerInterface $container,
-                protected string $pool,
-                array $config
-            ) {
-                parent::__construct($container);
-                $this->name = "queue.{$pool}";
-                $this->nums = $config['processes'] ?? 1;
-            }
-
-            public function handle(): void
-            {
-                $driver = $this->container->get(DriverFactory::class)->get($this->pool);
-                $driver->consume();
-            }
-        };
-
-        ProcessManager::register($process);
+        $this->container->get(ConsumerManager::class)->run();
     }
 }
