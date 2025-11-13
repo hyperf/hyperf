8b3ad4b5388bd8149a2177adfe0a11ac60a5c323
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2025-11-13 12:07:52 +0800

    Refactor command registration in ApplicationFactory
    Replaces individual command addition with batch addCommands method, simplifying the registration process and improving compatibility with newer Symfony versions.

--- src/framework/src/ApplicationFactory.php
+++ src/framework/src/ApplicationFactory.php
@@ -52,15 +52,9 @@ class ApplicationFactory
             $application->setDispatcher(new SymfonyEventDispatcher($eventDispatcher));
         }
 
-        foreach ($commands as $command) {
-            $pendingCommand = $this->pendingCommand($container->get($command));
-            // Use addCommand if available (Symfony 7.4+), otherwise fall back to add()
-            if (method_exists($application, 'addCommand')) {
-                $application->addCommand($pendingCommand);
-            } else {
-                $application->add($pendingCommand);
-            }
-        }
+        $application->addCommands(
+            array_map(fn ($command) => $this->pendingCommand($command), $commands)
+        );
 
         return $application;
     }
