f4c0bf29521982ec3d5b05439e914740d655ddff
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2025-12-02 09:51:47 +0800

    refactor(grpc): replace response parsing in _simpleRequest with UnaryCall class

---
+++ src/grpc-client/src/UnaryCall.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+
+namespace Hyperf\GrpcClient;
+
+use Hyperf\Grpc\Parser;
+use stdClass;
+
+class UnaryCall
+{
+    protected stdClass $status;
+
+    protected mixed $message;
+
+    public function __construct(protected mixed $response, protected mixed $deserialize)
+    {
+        [$this->message, $this->status] = $this->parse($this->response, $this->deserialize);
+    }
+
+    public function wait(): array
+    {
+        return [$this->message, $this->status];
+    }
+
+    private function parse($response, $deserialize): array
+    {
+        $status = new stdClass();
+        $status->code = 0;
+        $status->details = 'OK';
+
+        if (! $response) {
+            $status->code = Parser::GRPC_ERROR_NO_RESPONSE;
+            $status->details = 'No response';
+
+            return [null, $status];
+        }
+
+        if (self::isInvalidStatus($response->statusCode)) {
+            $status->message = $response->headers['grpc-message'] ?? 'Http status Error';
+            $status->code = $response->headers['grpc-status'] ?? ($response->errCode ?: $response->statusCode);
+
+            return [null, $status];
+        }
+
+        $grpcStatus = (int) ($response->headers['grpc-status'] ?? 0);
+
+        if ($grpcStatus !== 0) {
+            $status->code = $grpcStatus;
+            $status->message = $response->headers['grpc-message'] ?? 'Unknown error';
+
+            return [null, $status];
+        }
+
+        $message = Parser::deserializeMessage($deserialize, $response->data ?? '');
+
+        return [$message, $status];
+    }
+
+    private static function isInvalidStatus(int $code): bool
+    {
+        return $code !== 0 && $code !== 200 && $code !== 400;
+    }
+}
