f65d89844a417718b2d4933b966be4b0d849ff8a
Gang Wu <gokure@gmail.com>
2025-12-16 17:16:12 +0800

    Fix methods `Hyperf\Database\Query\Builder::eachById` bug and optimize code.

--- src/database/src/Model/Builder.php
+++ src/database/src/Model/Builder.php
@@ -763,56 +763,6 @@ class Builder
     }
 
     /**
-     * Chunk the results of a query by comparing numeric IDs.
-     *
-     * @param int $count
-     * @param callable(ModelCollection<int, TModel>, int): mixed $callback
-     * @param null|string $column
-     * @param null|string $alias
-     * @return bool
-     */
-    public function chunkById($count, callable $callback, $column = null, $alias = null)
-    {
-        $column = $column ?? $this->getModel()->getKeyName();
-
-        $alias = $alias ?? $column;
-
-        $lastId = null;
-
-        do {
-            $clone = clone $this;
-
-            // We'll execute the query for the given page and get the results. If there are
-            // no results we can just break and return from here. When there are results
-            // we will call the callback with the current chunk of these results here.
-            $results = $clone->forPageAfterId($count, $lastId, $column)->get();
-
-            $countResults = $results->count();
-
-            if ($countResults == 0) {
-                break;
-            }
-
-            // On each chunk result set, we will pass them to the callback and then let the
-            // developer take care of everything within the callback, which allows us to
-            // keep the memory low for spinning through large result sets for working.
-            if ($callback($results) === false) {
-                return false;
-            }
-
-            $lastId = $results->last()->{$alias};
-
-            if ($lastId === null) {
-                throw new RuntimeException("The chunkById operation was aborted because the [{$alias}] column is not present in the query result.");
-            }
-
-            unset($results);
-        } while ($countResults == $count);
-
-        return true;
-    }
-
-    /**
      * Get an array with the values of a given column.
      *
      * @param string $column
