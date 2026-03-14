62655fa03d2abaed2083832b922a306e600f0989
Deeka Wong <huangdijia@gmail.com>
2026-03-14 22:43:41 +0800

    Optimized the `toCssClasses` and `toCssStyles` methods by directly using the input array to simplify the code logic. (#7431)

--- src/collection/src/Arr.php
+++ src/collection/src/Arr.php
@@ -751,11 +751,9 @@ class Arr
      */
     public static function toCssClasses(array $array): string
     {
-        $classList = static::wrap($array);
-
         $classes = [];
 
-        foreach ($classList as $class => $constraint) {
+        foreach ($array as $class => $constraint) {
             if (is_numeric($class)) {
                 $classes[] = $constraint;
             } elseif ($constraint) {
@@ -771,11 +769,9 @@ class Arr
      */
     public static function toCssStyles(array $array): string
     {
-        $styleList = static::wrap($array);
-
         $styles = [];
 
-        foreach ($styleList as $class => $constraint) {
+        foreach ($array as $class => $constraint) {
             if (is_numeric($class)) {
                 $styles[] = Str::finish($constraint, ';');
             } elseif ($constraint) {
